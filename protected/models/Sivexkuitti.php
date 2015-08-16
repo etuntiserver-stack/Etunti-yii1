06f40ea7c79a4ec1c3cec970800304ff05590638
laptopsr <laptopsr@gmail.com>
2015-08-16 08:33:19 +0300

    m36

--- protected/models/Sivexkuitti.php
+++ protected/models/Sivexkuitti.php
@@ -60,7 +60,6 @@ public $domain;
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('asiakas_num, imei', 'required'),
 			//array('asiakas_num, osoite, kohde_kannasta, kohdenID, aloitan, loppui, viesti, tekijan_nimi, tid, etaisyys, status, tietoja, hyvaksytty', 'required'),
 			array('requests, kohdenID, tid, status, admin', 'numerical', 'integerOnly'=>true),
 			array('asiakas_num, puh_numero, bluetooth_name, subscriber_id, tekijan_nimi', 'length', 'max'=>50),
