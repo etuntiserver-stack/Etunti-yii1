c74c1a0dfeb968efdce7b3fb63572b94667fba4a
laptopsr <laptopsr@gmail.com>
2015-08-17 10:03:41 +0300

    Javascripttin  Laskuri
    Joka laskee hintoja

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -73,6 +73,8 @@ for ($i = 1; $i <= 12; $i++) {
 </div>
 -->
 <br>
+
+
     <div class="row">
         <div class="col-md-4 col-md-offset-1">
             <div class="panel panel-success">
@@ -87,34 +89,47 @@ for ($i = 1; $i <= 12; $i++) {
                 <ul class="list-group list-group-flush text-center">
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Työajanseuranta liikuvalle työlle ja Mobiili sovellus'); ?>
-                        <strong class="pull-right"><input type="checkbox" checked vlaue="basic"></strong>
+                        <strong class="pull-right"><input type="checkbox" checked value="15" disabled></strong>
                     </li>
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Työvuorosuunnittelu ohjelma'); ?>
-                        <strong class="pull-right"><input type="checkbox"></strong>
+                        <strong class="pull-right"><input type="checkbox" value="10"></strong>
                     </li>
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Asiakkuuksien hallinta ohjelma'); ?>
-                        <strong class="pull-right"><input type="checkbox"></strong>
+                        <strong class="pull-right"><input type="checkbox" value="20"></strong>
                     </li>
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Työnjohdon kuntopuntari'); ?>
-                        <strong class="pull-right"><input type="checkbox"></strong>
+                        <strong class="pull-right"><input type="checkbox" value="10"></strong>
                     </li>
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Laskutus ohjelma'); ?>
-                        <strong class="pull-right"><input type="checkbox"></strong>
+                        <strong class="pull-right"><input type="checkbox" value="10"></strong>
                     </li>
                     <li class="list-group-item">
                         <?php echo Yii::t('main', 'Online varaus'); ?>
-                        <strong class="pull-right"><input type="checkbox"></strong>
+                        <strong class="pull-right"><input type="checkbox" value="10"></strong>
                     </li>
                 </ul>
                 <div class="panel-footer">
-                    <a class="btn btn-lg btn-block btn-success"><?php echo Yii::t('main', 'Tilaa heti!'); ?></a>
+                    <a class="btn btn-lg btn-block btn-success"><?php echo Yii::t('main', 'Tilaa heti!'); ?> <i id="res"></i></a>
                 </div>
             </div>
         </div>
+
+        <div class="col-md-6">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h4 class="text-center"><?php echo Yii::t('main', 'TIEDOT'); ?></h4>
+                </div>
+                <div class="panel-body text-center">
+                    <p class="lead">
+                        <strong><?php echo Yii::t('main', 'Tähään tulee tiedot paketista'); ?></strong>
+                    </p>
+                </div>
+            </div>
+	</div>
     </div>
 
 
@@ -124,6 +139,25 @@ for ($i = 1; $i <= 12; $i++) {
 
 
 
+<script type="text/javascript">
+$(document).ready(function(){
+
+   $(':checkbox').click(function() {
+      	$("#res").html(checkChecked()+" &euro;")
+   });
+
+   function checkChecked(){
+     var str = 0;
+      $(':checkbox').each(function() {
+         str += this.checked ? Number($(this).val()) : 0;
+      });
+     return str;
+   }
+
+      	$("#res").html(checkChecked()+" &euro;")
+
+});
+</script>
 
 
 
