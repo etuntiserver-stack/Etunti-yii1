b21b83ae93f890f0dc6556dfb4a7aac2c6381fd0
laptopsr <laptopsr@gmail.com>
2015-08-28 07:53:55 +0300

    m

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,19 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- blueprint CSS framework -->
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />-->
-	<!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />-->
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
 
-
-
-
-
-  <!--<link rel="stylesheet" type="text/css" href="css/navbar.css" />-->
-  <!--<link rel="stylesheet" href="css/etunti-bootstrap-theme.css">-->
 
   <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
   <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
