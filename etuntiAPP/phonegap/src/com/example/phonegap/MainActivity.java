05ad4c2e5ce1c339ab2622b18c25dbb95a013f82
laptopsr <laptopsr@gmail.com>
2015-08-29 20:15:35 +0300

    android app

---
+++ etuntiAPP/phonegap/src/com/example/phonegap/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.phonegap;
+
+//import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.telephony.TelephonyManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.location.Location;
+
+import org.apache.cordova.DroidGap;
+
+
+
+
+public class MainActivity extends DroidGap {
+	private static String imei;
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String IMEI;
+        String my_location;
+        MyLocationListener.SetUpLocationListener(this);
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        IMEI  = telephonyManager.getDeviceId();
+        //Phno =  telephonyManager.getPhoneType();
+        
+        if(MyLocationListener.imHere != null){
+        	 my_location =  MyLocationListener.imHere.getLatitude()+"/"+MyLocationListener.imHere.getLongitude();
+            } else {
+        	 my_location = "GPS disabled";
+            }
+        
+        super.loadUrl("file:///android_asset/www/index.html?imei="+IMEI+"&location="+my_location);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        /*
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        */
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
