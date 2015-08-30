10f92b59c8706ab7f56cb115d6590c51bd4294a5
laptopsr <laptopsr@gmail.com>
2015-08-30 10:56:10 +0300

    m

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -57,7 +57,7 @@ public function actionImei($dom)
 	    if($_POST['check'] == 'osoitevaihto'){
 	        $kohteet = Kohteet::model()->findAll(" osoite like '%".$_POST['thisKey']."%' ");
 
-		$sel = '<select id="list">';
+		$sel = '<select id="list" class="form-control">';
 		foreach($kohteet as $val)
 		$sel .= '<option value="'.$val->id.'">'.$val->osoite.'</option>';
 		$sel .= '</select>';
