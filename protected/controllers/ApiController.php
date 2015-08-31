24f54e3325d3743a4302565bfc12e1b40de0e730
laptopsr <laptopsr@gmail.com>
2015-08-31 13:41:33 +0300

    m

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -54,10 +54,10 @@ public function actionImei($dom)
 	    if(isset($_POST['check'])){
 
 	        if($_POST['check'] == 'viestinta'){
-	            $kohteet = Viestinta::model()->findAll(" tekija = '".$ttekija->id."' ");
+	            $viestinta = Viestinta::model()->findAll(" tekija = '".$ttekija->id."' ");
 
 		    $sel = '<div class="">';
-		    foreach($kohteet as $val)
+		    foreach($viestinta as $val)
 		      $sel .= '<div class="row">
 				  '.$val->admin.'<br>
 				  '.$val->pvm.'<br>
