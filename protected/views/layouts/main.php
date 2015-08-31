96565b21d6bb318b6e05d2183eb51d7859f67bef
laptopsr <laptopsr@gmail.com>
2015-08-31 11:54:39 +0300

    m

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,8 +1,8 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 2:      "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<meta name="language" content="en" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
