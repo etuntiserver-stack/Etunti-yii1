d17da13a29676093ab75e2689860ee4549c88082
laptopsr <laptopsr@gmail.com>
2015-08-31 09:20:57 +0300

    m

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::app()->charset ?>" />
 	<meta name="language" content="en" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
