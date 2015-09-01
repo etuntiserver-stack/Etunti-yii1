aa7ab1091b72da072c36ada41b871733ac328780
laptopsr <laptopsr@gmail.com>
2015-09-01 11:09:29 +0300

    m3

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -144,7 +144,7 @@ public function actionImei($dom)
 				  '.$val->pvm.'<hr>
 				  <span class="text">'.$val->viesti.'</span><br>';
 
-				  if(isset($exAdmin[0]) and empty($exAdmin[0])){
+				  if(isset($exAdmin[0]) and !empty($exAdmin[0])){
 				  echo '
 				  <div class="row">
             			    <div class="input-group">
