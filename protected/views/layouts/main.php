84f9a67c894b7e0e3de5de5fc1188701d12bddd1
laptopsr <laptopsr@gmail.com>
2015-09-02 13:36:36 +0300

    yhteenveto kohteet

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -80,7 +80,6 @@ Yii::app()->clientScript->registerPackage('bootstrapCSS');
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo Yii::t('main', 'Mobiili'); ?> <span class="caret"></span></a>
           <ul class="dropdown-menu">
         	<li><?php echo CHtml::link(Yii::t('main', 'Mobiili lista'),array('/sivexkuitti/index')); ?></li>
-        	<li><?php echo CHtml::link(Yii::t('main', 'Yhteenveto tunnit'),array('/sivexkuitti/yhteenveto')); ?></li>
           </ul>
         </li>
 
@@ -89,6 +88,14 @@ Yii::app()->clientScript->registerPackage('bootstrapCSS');
           <ul class="dropdown-menu">
         	<li><?php echo CHtml::link(Yii::t('main', 'Toteutuneet taulukko'),array('/toteutuneet/index')); ?></li>
         	<li><?php echo CHtml::link(Yii::t('main', 'Toteutuneet hallinta'),array('/toteutuneet/admin')); ?></li>
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo Yii::t('main', 'Yhteenvedot'); ?> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+        	<li><?php echo CHtml::link(Yii::t('main', 'Työtunnit'),array('/sivexkuitti/yhteenveto')); ?></li>
+        	<li><?php echo CHtml::link(Yii::t('main', 'Kohteet'),array('/sivexkuitti/kyhteenveto')); ?></li>
           </ul>
         </li>
 
---
+++ protected/views/sivexkuitti/kyhteenveto.php
@@ -0,0 +1,85 @@
+<div class="row">
+<?php
+/* @var $this SivexkuittiController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->breadcrumbs=array(
+	Yii::t('main', 'Kohteet'),
+);
+
+?>
+
+<h1><?php echo Yii::t('main', 'Yhteenveto kohteet'); ?></h1>
+
+
+<div class="row">
+  <form action="#" id="yhtveto" method="POST">
+  <div class="col-md-12">
+   <?php
+    $model=new Sivexkuitti;
+    $list = CHtml::listData(Sivexkuitti::model()->findAll(array('group' => 'kohde_kannasta','order' => 'kohde_kannasta')), 'kohde_kannasta', 'kohde_kannasta');
+
+    echo '<select name="kohteet" class="selectpicker" id="kohteet" class="btn btn-default" title="Kohteet">';
+    foreach($list as $key=>$val){
+       	 echo '<option value="'.$key.'">'.$val.'</option>';
+    }
+    echo '</select>';
+   ?>
+
+   <input type="date" name="from" id="from" class="btn btn-default" value="<?php echo Yii::app()->session['from']; ?>">
+
+   <input type="date" name="to" id="to" class="btn btn-default" value="<?php echo Yii::app()->session['to']; ?>">
+
+   <input type="submit" class="btn btn-primary" value="<?php echo Yii::t('main', 'haku'); ?>">
+   </form>
+  </div>
+</div>
+
+<br>
+
+<?php if(Yii::app()->session['from'] and Yii::app()->session['to']) : ?>
+  <table class="table table-striped table-bordered">
+  <thead>
+  <tr>
+  <th><?php echo Yii::t('main', 'Osoite'); ?></th>
+  <th><?php echo Yii::t('main', 'Toteutuneet tunnit'); ?></th>
+  </tr>
+  </thead>
+
+<?php $this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_kyhteenveto',
+)); ?>
+
+  </table>
+<?php endif; ?>
+</div>
+
+	<div id="showres" class="modal fade" tabindex="-1" role="dialog"></div>
+
+
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+$("#yhtveto").on('submit',function(e){
+
+  var from = $("#from").val();
+  var to = $("#to").val();
+
+    if (from  === '') {
+        $('#from').css({"border" : "2px #f14010 solid"}).focus();
+        return false;
+    }
+    if (to  === '') {
+        $('#to').css({"border" : "2px #f14010 solid"}).focus();
+        return false;
+    }
+
+});
+
+
+
+});
+</script>
