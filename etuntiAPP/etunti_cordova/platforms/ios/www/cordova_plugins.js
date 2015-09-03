b0e4549e5e2ef966b3888a83d8b458fc60b3b58e
Elias Luoma <elias.luoma@santelo.fi>
2015-09-03 11:44:00 +0300

    Lisätty Device Plugin

--- etuntiAPP/etunti_cordova/platforms/ios/www/cordova_plugins.js
+++ etuntiAPP/etunti_cordova/platforms/ios/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.NfcPlugin"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "com.dnrps.nfc.plugin": "0.0.0"
+    "com.dnrps.nfc.plugin": "0.0.0",
+    "cordova-plugin-device": "1.0.2-dev"
 }
 // BOTTOM OF METADATA
 });
