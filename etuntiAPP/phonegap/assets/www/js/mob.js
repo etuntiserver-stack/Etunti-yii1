2a4e39e98c6f510d6f8c0c28a54756c7e7dee10c
laptopsr <laptopsr@gmail.com>
2015-09-03 15:48:58 +0300

    mob

--- etuntiAPP/phonegap/assets/www/js/mob.js
+++ etuntiAPP/phonegap/assets/www/js/mob.js
@@ -308,11 +308,7 @@ function row(tilanne,st){
 
 		if(sp[0] == 'imeiError')
 		{
-<<<<<<< HEAD
-		  $("#result2").html("<h2>"+sp[1]+" "+sp[2]+"</h2>").show();
-=======
 		  $("#result2").html("<h2>"+sp[1]+" "+sp[2]+"</h2>").show()
->>>>>>> origin/master
 		  return false;
 		} 
 
