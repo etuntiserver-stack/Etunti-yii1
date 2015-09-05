29a422aa62d17d33e02e0408210fde4121046e39
laptopsr <laptopsr@gmail.com>
2015-09-05 19:27:07 +0300

    m2

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -276,7 +276,7 @@ public function actionImei($dom)
 
 		if(isset($_POST['tag']) and $_POST['tag'] != 'notag')
 		{
-		  $kohteet = Kohteet::model()->find(" tag_id='".$_POST['tag']."' ");
+		  $kohteet = Kohteet::model()->find(" tag_id='".trim($_POST['tag'])."' ");
 		    if(isset($kohteet['osoite']) and !empty($kohteet['osoite']))
 		    {
 		      $get_osoite = $kohteet['osoite'];
