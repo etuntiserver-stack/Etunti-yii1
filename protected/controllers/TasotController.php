220cca02b9c646a724b8569412663ac3c1cb76f1
laptopsr <laptopsr@gmail.com>
2015-09-07 00:03:11 +0300

    oikeideet

--- protected/controllers/TasotController.php
+++ protected/controllers/TasotController.php
@@ -29,15 +29,15 @@ class TasotController extends Controller
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'expression' => "Yii::app()->User->isAdmin()",
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'expression' => "Yii::app()->User->isAdmin()",
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'expression' => "Yii::app()->User->isAdmin()",
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
