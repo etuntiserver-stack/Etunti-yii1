5773daa82d1f59a0de91ab1e5aea81919bcdd1ef
laptopsr <laptopsr@gmail.com>
2015-09-11 12:55:54 +0300

    m

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -48,11 +48,8 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-
-		if(!Yii::app()->user->isGuest)
 		$this->render('index');
-		else
-		$this->render('start');
+
 	}
 
 	/**
