cecd4e913f376af563022fd4356ccc7d1b1b40de
laptopsr <laptopsr@gmail.com>
2015-09-13 18:26:12 +0300

    m2

--- protected/views/mobile/_view.php
+++ protected/views/mobile/_view.php
@@ -142,7 +142,7 @@ else
 	  <div class="col-sm-2">
 	  <?php 
 	     $filename = "../../img/tekijat/".Yii::app()->user->domain."/".$data->tid.".jpg";
-	     if (file_exists($filename))
+	     if (file_exists(Yii::app()->request->baseUrl."img/tekijat/".Yii::app()->user->domain."/".$data->tid.".jpg"))
 	     echo '<img src="'.$filename.'" class="img-thumbnail">';
 	     else
 	     echo '<img src="../../img/tekijat/noname.jpg" class="img-responsive">';
