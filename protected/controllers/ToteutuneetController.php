6be0c5dbd20c44358ed3e113828bf4214a45be17
laptopsr <laptopsr@gmail.com>
2015-09-14 14:36:37 +0300

    m2

--- protected/controllers/ToteutuneetController.php
+++ protected/controllers/ToteutuneetController.php
@@ -43,14 +43,18 @@ class ToteutuneetController extends Controller
 
 	public function isEtuntiAdmin() {
 
-		$tas = explode(",",Yii::app()->user->adminPaketti);
+	$tas = '';
+	if(isset(Yii::app()->user->adminPaketti))
+	$tas = explode(",",Yii::app()->user->adminPaketti);
+
 		if(isset(Yii::app()->user->adminID) and in_array('2',$tas))
 		{
 		$m = Administrators::model()->findbypk(Yii::app()->user->adminID);
-	        if($m->id == Yii::app()->user->adminID)
-	            return true;
+	       	if($m->id == Yii::app()->user->adminID)
+	       	  return true;
 		else
-	            return false;
+	       	   return false;		
+
 		} else {
 	            return false;
 		}
