4d1b7fa26ce9cdc34b8b901bf6a5db8c08dd3be2
laptopsr <laptopsr@gmail.com>
2015-09-20 09:40:18 +0300

    t

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -89,20 +89,20 @@ function mail_attachment_lahettaminen($filename, $path, $mailto, $from_mail, $fr
     $content = chunk_split(base64_encode($content));
     $uid = md5(uniqid(time()));
     $name = basename($file);
-    $header = "From: ".$from_name." <".$from_mail.">\r\n";
-    $header .= "Reply-To: ".$replyto."\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-    $header .= "This is a multi-part message in MIME format.\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-type:text/plain; charset=UTF-8\r\n";
-    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $header .= $message."\r\n\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n"; // use different content types here
+    $header = "From: ".$from_name." <".$from_mail.">\n";
+    $header .= "Reply-To: ".$replyto."\n";
+    $header .= "MIME-Version: 1.0\n";
+    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\n\n";
+    $header .= "This is a multi-part message in MIME format.\n";
+    $header .= "--".$uid."\n";
+    $header .= "Content-type:text/plain; charset=UTF-8\n";
+    $header .= "Content-Transfer-Encoding: 7bit\n\n";
+    $header .= $message."\n\n";
+    $header .= "--".$uid."\n";
+    $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\n"; // use different content types here
     $header .= "Content-Transfer-Encoding: base64\r\n";
-    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
-    $header .= $content."\r\n\r\n";
+    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\n\n";
+    $header .= $content."\n\n";
     $header .= "--".$uid."--";
 
 
