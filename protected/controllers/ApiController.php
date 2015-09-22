4ada220cd0e21a4611b3a608446e1945fd3ab8be
laptopsr <laptopsr@gmail.com>
2015-09-22 08:13:43 +0300

    ei toimi

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -186,11 +186,12 @@ public function actionImei($dom)
 		    $cl = '';
 		    $admin = '';
 
+/*
 		    $model=new Viestinta;
 		    $sel .= $this->renderPartial('//viestinta/create',array(
 			'model'=>$model,
 		    ),true);
-
+*/
 		    foreach($viestinta as $val)
 		    {
 		      if($val->status == '0')
