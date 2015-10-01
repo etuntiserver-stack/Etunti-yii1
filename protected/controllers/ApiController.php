97d7355a1a5007c2604cc905ef0a5512374c9d79
laptopsr <laptopsr@gmail.com>
2015-10-01 11:46:14 +0300

    m

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -393,7 +393,7 @@ $expla = explode(".",$_POST['aloitan']);
 	        exit;
 		}
 
-                $mobupdate->loppui = $_POST['loppui'];
+                $mobupdate->loppui = date("d.m.Y H:i:s");
                 $mobupdate->status = 3;
                 $mobupdate->my_location = $mobupdate->my_location."**".$_POST['my_location'];
                 $mobupdate->save();
@@ -402,7 +402,7 @@ $expla = explode(".",$_POST['aloitan']);
 	     } elseif($mob->status == 2 and $_POST['status'] == 2 and !empty($_POST['loppui']))
 	     {
                 $mobupdate = Mob::model()->findbypk($mob->id);
-                $mobupdate->loppui = $_POST['loppui'];
+                $mobupdate->loppui = date("d.m.Y H:i:s");
                 $mobupdate->status = 2;
                 $mobupdate->my_location = $mobupdate->my_location."**".$_POST['my_location'];
                 $mobupdate->viesti = $mobupdate->viesti."/".$_POST['kohde_kannasta'];
@@ -412,7 +412,7 @@ $expla = explode(".",$_POST['aloitan']);
 	     } elseif($mob->status == 10 and $_POST['status'] == 10 and !empty($_POST['loppui']))
 	     {
                 $mobupdate = Mob::model()->findbypk($mob->id);
-                $mobupdate->loppui = $_POST['loppui'];
+                $mobupdate->loppui = date("d.m.Y H:i:s");
                 $mobupdate->status = 10;
                 $mobupdate->my_location = $mobupdate->my_location."**".$_POST['my_location'];
                 $mobupdate->viesti = $mobupdate->viesti."/".$_POST['kohde_kannasta'];
@@ -449,6 +449,7 @@ $expla = explode(".",$_POST['aloitan']);
                 $mobinsert->tid = $ttekija->id;
                 $mobinsert->tekijan_nimi = $ttekija->tekijan_nimi;
                 $mobinsert->tietoja = $_POST['tietoja'];
+                $mobinsert->aloitan = date("d.m.Y H:i:s");
                 $mobinsert->save();
                 $this->_sendResponse(200, $mobinsert->id."//".$mobinsert->kohde_kannasta);
 
