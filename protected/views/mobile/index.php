02f048f3f88dd000f1517a738a7a40e58ccff887
laptopsr <laptopsr@gmail.com>
2015-10-04 13:05:06 +0300

    test

--- protected/views/mobile/index.php
+++ protected/views/mobile/index.php
@@ -71,12 +71,12 @@ $fi = array(
   <div class="col-sm-3">
 <?php 
 	$crsun = new CDbCriteria();
-	$crsun->select = "  COUNT(*) as count ";
 	$crsun->condition = " 
 		DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') = CURDATE()
+		AND kohde!=''
 		AND tyoajanmerkinta NOT LIKE '%Ei lasketa%'
-
 	";
+
 	$s = Tyovuoroot::model()->findAll($crsun);
 	$a = Mobile::model()->findAll("status=1",array('select'=>'id'));	
 	$t = Mobile::model()->findAll(" DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') = CURDATE() and status=3",array('select'=>'id'));	
