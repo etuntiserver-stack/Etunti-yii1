98d6156efae6efc7e5c4eaf0ad0558307dbc3548
laptopsr <laptopsr@gmail.com>
2015-10-04 09:58:07 +0300

    App foo

---
+++ etuntiAPP/foo/platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/NSArray+Comparisons.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Applications/MAMP/htdocs/etuntifw/etuntiAPP/foo/platforms/ios/CordovaLib/Classes/NSArray+Comparisons.m \
+  /Applications/MAMP/htdocs/etuntifw/etuntiAPP/foo/platforms/ios/CordovaLib/Classes/NSArray+Comparisons.h \
+  /Applications/MAMP/htdocs/etuntifw/etuntiAPP/foo/platforms/ios/CordovaLib/Classes/CDVAvailabilityDeprecated.h
