7094fe9f663ac7d856ff626622a7c78b83315fbc
laptopsr <laptopsr@gmail.com>
2015-10-09 08:08:47 +0300

    HYVÄKSYTTY TUNNIT

--- protected/views/asiakasHyvaksynta/index.php
+++ protected/views/asiakasHyvaksynta/index.php
@@ -13,8 +13,45 @@ $this->menu=array(
 ?>
 
 <legend>
-<h1><?php echo Yii::t('main','HYVÄKSYTTY TUNNIT'); ?></h1>
+<h1><?php echo Yii::t('main','HYVÄKSYTTY TUNNIT'); ?> <i class="glyphicon glyphicon-ok"></i></h1>
 </legend>
+
+
+
+<form action="#" id="hyvaksytunnit" class="form-inline" method="POST">
+  <input type="hidden" name="hyvaksytunnit">
+
+    	<?php 
+       		$criteria = new CDbCriteria();
+		$criteria->order = " yhteyshenkilo ";
+
+        	$a = Asiakkaat::model()->findAll($criteria);
+		echo '<select name="asiakas" class="form-control input-sm">';
+
+	        echo '<option value="kaikki">Kaikki</option>';
+
+		foreach($a as $aa)
+		{
+		  if(!empty($aa->yrityksen_nimi))
+		    echo '<option value="'.$aa->id.'">'.$aa->yrityksen_nimi.'</option>';
+		  elseif(empty($aa->yhteyshenkilo) and empty($aa->yrityksen_nimi))
+		    echo '<option value="'.$aa->id.'">nimet puutuu '.$aa->id.'</option>';
+		  else
+		    echo '<option value="'.$aa->id.'">'.$aa->yhteyshenkilo.' ID:'.$aa->id.'</option>';
+		}
+		echo '</select>';
+	?>
+
+   <b class="glyphicon glyphicon-calendar"></b>
+   <input type="text" name="from" id="from" class="form-control form-group input-sm datepicker" value="<?php echo Yii::app()->session['from']; ?>">
+   <b class="glyphicon glyphicon-calendar"></b>
+   <input type="text" name="to" id="to" class="form-control form-group input-sm datepicker" value="<?php echo Yii::app()->session['to']; ?>">
+
+   <input type="submit" class="btn btn-sm btn-primary" value="<?php echo Yii::t('main', 'haku'); ?>">
+
+</form>
+
+
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
