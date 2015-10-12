763d7628ad7d5f8ac629e60bd0ad01b2d08836cf
laptopsr <laptopsr@gmail.com>
2015-10-12 15:00:30 +0300

    viestintä

--- protected/controllers/ViestintaController.php
+++ protected/controllers/ViestintaController.php
@@ -82,8 +82,58 @@ class ViestintaController extends Controller
 		if(isset($_POST['Viestinta']))
 		{
 			$model->attributes=$_POST['Viestinta'];
-			if($model->save())
+			if($model->save()){
+
+$t = Tyontekijat::model()->findbypk($model->tekija);
+
+if(isset($t->gcm_reg_id))
+{
+
+define( 'API_ACCESS_KEY', 'AIzaSyCnMuvBry0dxLPntaAW-HSLrcyrkwy6BzY' );
+$registrationIds = array( $t->gcm_reg_id );
+// prep the bundle
+$msg = array
+(
+	'message' 	=> $model->viesti,
+	'title'		=> 'ETUNTI',
+	'subtitle'	=> 'This is a subtitle. subtitle',
+	'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+	'vibrate'	=> 1,
+	'sound'		=> 1,
+	'largeIcon'	=> 'large_icon',
+	'smallIcon'	=> 'small_icon'
+);
+$fields = array
+(
+	'registration_ids' 	=> $registrationIds,
+	'data'			=> $msg
+);
+ 
+$headers = array
+(
+	'Authorization: key=' . API_ACCESS_KEY,
+	'Content-Type: application/json'
+);
+ 
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+
+$result = json_decode($result);
+
+print_r($result->success);
+
+}
+
+
 				$this->redirect(array('view','id'=>$model->id));
+			}
 		}
 
 		$this->render('create',array(
