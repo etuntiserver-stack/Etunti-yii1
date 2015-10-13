6e8c425768c2ba502f9686f2ae43d473c553d038
laptopsr <laptopsr@gmail.com>
2015-10-13 14:56:54 +0300

    app refresh

--- etuntiAPP/fi.etunti.mobile_andoidonly/platforms/android/assets/www/js/mob.js
+++ etuntiAPP/fi.etunti.mobile_andoidonly/platforms/android/assets/www/js/mob.js
@@ -6,44 +6,13 @@ $(document).ready(function(){
   var timer = setTimeout(tiedot,5000); 
 
 
-function initPushwoosh() {
-	var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
-	if(device.platform == "Android")
-	{
-		registerPushwooshAndroid();
-	}
-
-	if(device.platform == "iPhone" || device.platform == "iOS")
-	{
-		registerPushwooshIOS();
-	}
-
-	if (device.platform == "Win32NT") {
-	    registerPushwooshWP();
-	}
-}
-
-
-
-
 	    document.addEventListener("deviceready", onDeviceReady, false);
 	    function onDeviceReady() {
 
 
 	        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-		
-		/*
-	        document.getElementById('imei').value = cordova.plugins.uid.IMEI;
-		if(cordova.plugins.uid.IMEI > 0)
-		document.getElementById("all").style.display = "block";
-		*/
-
 		document.getElementById("all").style.display = "block";
 
-		/*
-        	initPushwoosh();
-        	app.receivedEvent('deviceready');
-		*/
 
 	    }
 	    function onSuccess(position) {
@@ -323,7 +292,7 @@ function row(tilanne,st){
         $.ajax({
            url: url+'/imei?dom='+domain,
 	   type:'POST',
- 	   data: { check : "testi", my_location : my_location, tag : tag, email : email, salasana : salasana, gcm_reg_id : $("#regId").text() },
+ 	   data: { check : "testi", my_location : my_location, tag : tag, email : email, salasana : salasana },
            success: function(data){
         	//console.log(data);
 		//$("#result2").html(data).show();
