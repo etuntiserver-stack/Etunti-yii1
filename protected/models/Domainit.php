06668176744b4b2a1872b1d6659457483d2d5c14
laptopsr <laptopsr@gmail.com>
2015-10-16 11:20:58 +0300

    app

--- protected/models/Domainit.php
+++ protected/models/Domainit.php
@@ -44,7 +44,7 @@ class Domainit extends CActiveRecord
 			'subtitle'	=> 'This is a subtitle. subtitle',
 			'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
 			'vibrate'	=> 1,
-			'sound'		=> 'beep37.wav', //http://etunti.fi/etuntiAPP/SOUNDS_FOR_APP/
+			'sound'		=> 'beep', // viella on "danger"
 			'largeIcon'	=> 'large_icon',
 			'smallIcon'	=> 'small_icon'
 		);
