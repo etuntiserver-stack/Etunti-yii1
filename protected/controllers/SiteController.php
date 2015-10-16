e81cb74b7f213c74b146c1d8f4c6b3ba564fbe31
laptopsr <laptopsr@gmail.com>
2015-10-16 17:13:16 +0300

    m

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -71,7 +71,7 @@ class SiteController extends Controller
 	
 	  $uploaddir = Yii::app()->basePath.'/../img/uploadedfromphone/'.$dom.'/';
 	  $uploadfile = $uploaddir . basename($model->id.'_'.$tyontekija.'.jpg');
-	  if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+	  if (move_uploaded_file($_POST['data'], $uploadfile)) {
 		echo 'ok';
 	  } else {
 		echo 'Ei onnistu!';
