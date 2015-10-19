815a0bbc80fe35cb81c89aa99100e9213bda7e2e
laptopsr <laptopsr@gmail.com>
2015-10-19 21:20:29 +0300

    kuva

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -32,40 +32,9 @@ class ApiController extends Controller
 
 
 public function actionTiedosto($dom)
-
-    {
-
-    switch($_GET['model'])
-    {
-        case 'mob':
+{
 
         $this->_sendResponse(200, CJSON::encode("test"));
-
-            break;
-
-        default:
-            // Model not implemented error
-            $this->_sendResponse(501, sprintf(
-                'Error: Mode <b>list</b> is not implemented for model <b>%s</b>',
-                $_GET['model']) );
-
-            Yii::app()->end();
-    }
-    // Did we get some results?
-    if(empty($models)) {
-        // No
-
-        $this->_sendResponse(200, 
-                sprintf('No items where found for model <b>%s</b>', $_GET['model']) );
-    } else {
-        // Prepare response
-        $rows = array();
-
-        foreach($models as $model)
-            $rows[] = $model->attributes;
-        // Send the response
-        $this->_sendResponse(200, CJSON::encode($rows));
-    }
 
 
 }
