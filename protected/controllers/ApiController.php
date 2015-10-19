91d49552074528a204dd7aaa20521617dba76a60
laptopsr <laptopsr@gmail.com>
2015-10-19 21:34:11 +0300

    m3

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -55,8 +55,44 @@ public function actionTiedosto($dom)
 	    	exit;
 	    	}
 
+//
 	    	if(isset($ttekija->id))
 	    	{
+
+	  	if (!file_exists(Yii::app()->basePath."/../img/uploadedfromphone/".$dom)) {
+	  		mkdir(Yii::app()->basePath."/../img/uploadedfromphone/".$dom, 0777, true);
+	  	}
+	
+		$kohdenID = $_POST['kohdenID'];
+
+		function saveImage($base64img,$dom,$kohde,$tekija){
+		    define('UPLOAD_DIR', Yii::app()->basePath."/../img/uploadedfromphone/".$dom."/");
+		    $base64img = str_replace('data:image/jpeg;base64,', '', $base64img); 
+		    $data = base64_decode($base64img);
+		    $file = UPLOAD_DIR . $kohde.'_'.$tekija.'_'.time().'.jpg';
+		    
+		    if(file_put_contents($file, $data))
+			echo 'saveOk';
+		    else
+			echo 'saveError';
+		}
+
+	  	$model = Kohteet::model()->findbypk($kohdenID);
+
+	   	$criteria = new CDbCriteria();
+	    	$criteria->condition = "  
+			salasana!='' 
+			AND tekijan_email = '".$_POST['email']."' 
+			AND salasana = '".$_POST['salasana']."' 
+	    	";
+            	$ttekija = Tyontekijat::model()->find($criteria);
+
+	  	if(isset($model->id) and isset($ttekija->id) and isset($_POST['img'])){
+			saveImage(utf8_decode($_POST['img']),$dom,$model->id,$ttekija->id);
+	     	} else {
+			echo 'Ei onnistu!';
+	     	}
+
 
             	$this->_sendResponse(200, "tekijaOK//Tämän työntekijä ei löydy");
 	    	exit;
