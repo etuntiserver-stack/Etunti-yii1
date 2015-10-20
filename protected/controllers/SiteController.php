e1d55a6b17ff467914ad638ca9b93d5861607e82
laptopsr <laptopsr@gmail.com>
2015-10-20 10:58:22 +0300

    m33

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -78,7 +78,7 @@ class SiteController extends Controller
 
 	  	if(isset($ttekija->id)){
 
-    	 	if (move_uploaded_file($_FILES['file']['tmp_name'], Yii::app()->basePath."/../img/uploadedfromphone/".$dom."/".$_POST['kohdenID']."_".$_POST['kohdenID']."_".$ttekija->id."_".date("YmdHi").".jpg")) 
+    	 	if (move_uploaded_file($_FILES['file']['tmp_name'], Yii::app()->basePath."/../img/uploadedfromphone/".$dom."/".$_POST['kohdenID']."_".$ttekija->id."_".date("YmdHi").".jpg")) 
 		{
         		print "saveOk";
     		} else {
