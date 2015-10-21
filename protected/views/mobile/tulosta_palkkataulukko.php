8fc64ce24545c267a2aaa450a1d9d692f60a2898
laptopsr <laptopsr@gmail.com>
2015-10-21 13:33:09 +0300

    logo

--- protected/views/mobile/tulosta_palkkataulukko.php
+++ protected/views/mobile/tulosta_palkkataulukko.php
@@ -15,6 +15,10 @@ td,th{
 }
 </style>
 
+  <?php $asetukset=Asetukset::model()->find("id=1"); ?>
+  <img src="<?php echo $asetukset->logon_polkku; ?>" height="<?php echo $asetukset->logon_korkeus; ?>">
+  <br>
+
 <h1> <?php echo Yii::t('main', 'PALKKATAULUKKO'); ?></h1>
 <h3><?php echo date("d.m.Y",strtotime(Yii::app()->session['from']))." - ".date("d.m.Y",strtotime(Yii::app()->session['to'])); ?></h3>
 
