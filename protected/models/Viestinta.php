ce4b2c061fcadab16975b4cab09d00d427afe9b0
laptopsr <laptopsr@gmail.com>
2015-10-26 11:34:47 +0200

    Viestit korjauksia

--- protected/models/Viestinta.php
+++ protected/models/Viestinta.php
@@ -73,7 +73,7 @@ class Viestinta extends DB2ActiveRecord
 			'pvm' => 'Pvm',
 			'tekija' => 'Vastaanottaja',
 			'viesti' => 'Viesti',
-			'admin' => 'Kirjoittaja',
+			'admin' => 'Lähettäjä',
 		);
 	}
 
