f4dda02e03bb4f167fce423e6186613d5f9f5719
laptopsr <laptopsr@gmail.com>
2015-10-26 10:34:07 +0200

    Tuntiyhteenvedot

--- protected/views/mobile/yhteenveto_m.php
+++ protected/views/mobile/yhteenveto_m.php
@@ -10,7 +10,7 @@ $this->breadcrumbs=array(
 ?>
 
 <legend>
-<h1> <?php echo Yii::t('main', 'YHTEENVETO MATKAT'); ?> <i class="glyphicon glyphicon-time"></i></h1>
+<h1> <?php echo Yii::t('main', 'Tuntiyhteenveto matkat'); ?> <i class="glyphicon glyphicon-time"></i></h1>
 </legend>
 
 <div class="row" id="haku">
