4bf45cc646a56cf56facd3088eed628fde9c2ee6
laptopsr <laptopsr@gmail.com>
2015-10-27 10:47:05 +0200

    raportit

--- protected/views/mobile/raportit_pdf_t.php
+++ protected/views/mobile/raportit_pdf_t.php
@@ -1,6 +1,7 @@
 <style>
 table{
 	width: 100%;
+	font-size:80%;
 }
 td,th{
 	padding:3px 7px;
@@ -14,6 +15,7 @@ td,th{
   <br>
 
 <h1><?php echo Yii::t('main', 'TOTEUTUNEET'); ?></h1>
+<p><?php echo date("d.m.Y",strtotime(Yii::app()->session['from'])).'-'.date("d.m.Y",strtotime(Yii::app()->session['to'])); ?></p>
 <br>
 <table>
   <thead>
