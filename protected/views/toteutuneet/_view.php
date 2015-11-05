b33422fd467ce5b62e68c1c5325444f16ca585b4
laptopsr <laptopsr@gmail.com>
2015-11-05 11:54:13 +0200

    mmm

--- protected/views/toteutuneet/_view.php
+++ protected/views/toteutuneet/_view.php
@@ -11,13 +11,13 @@ $did = date("Ymd",strtotime($data->aloitan));
 	 $tas = explode(",",Yii::app()->user->adminPaketti);
 	 if(in_array('2',$tas)) : 
 	?>
-	<td><?php $this->renderPartial('//tyovuoroot/did',array('pvm'=>$data->aloitan,'tid'=>Yii::app()->session['tekija'],'from'=>'mobiili')); ?></td>
+	<td><?php $this->renderPartial('//tyovuoroot/did',array('pvm'=>$data->aloitan,'tid'=>$data->tid,'from'=>'mobiili')); ?></td>
 	<?php endif; ?>
 	<!-- adminPaketti -->
-	<td><?php $this->renderPartial('luetutpvmtid',array('pvm'=>$data->aloitan,'tid'=>Yii::app()->session['tekija'],'from'=>'mobiili')); ?></td>
+	<td><?php $this->renderPartial('luetutpvmtid',array('pvm'=>$data->aloitan,'tid'=>$data->tid,'from'=>'mobiili')); ?></td>
 
 	<td id="<?php echo $did.'_'.$data->tid; ?>">
-	<?php $this->renderPartial('totpvmtid',array('pvm'=>$data->aloitan,'tid'=>Yii::app()->session['tekija'],'from'=>'mobiili')); ?>
+	<?php $this->renderPartial('totpvmtid',array('pvm'=>$data->aloitan,'tid'=>$data->tid,'from'=>'mobiili')); ?>
 	</td>
 	<td id="yht_<?php echo $did.'_'.$data->tid; ?>"><?php $this->renderPartial('yhteensapvm',array('pvm'=>date("Y-m-d",strtotime($data->aloitan)),'tid'=>$data->tid,'from'=>'mobiili')); ?></td>
 
