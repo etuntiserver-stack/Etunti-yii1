062ebc5ae912f500246a87743f3e8065eb75ab31
laptopsr <laptopsr@gmail.com>
2015-11-05 11:59:15 +0200

    mm

--- protected/controllers/ToteutuneetController.php
+++ protected/controllers/ToteutuneetController.php
@@ -351,7 +351,7 @@ class ToteutuneetController extends Controller
 
 
 		if(isset($explTekija[0]))
-	        $criteria->addCondition (" tid = '".Yii::app()->session['tekija']."'");
+	        $criteria->addCondition (" tid = '".$explTekija[0]."'");
 
 		if(Yii::app()->session['from'] and Yii::app()->session['to'])
 	        $criteria->addCondition ("DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".Yii::app()->session['from']."' AND '".Yii::app()->session['to']."' ");
