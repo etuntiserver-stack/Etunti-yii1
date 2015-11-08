7be135d6ff41f54db5d205dff0cb2747b8b9580f
laptopsr <laptopsr@gmail.com>
2015-11-08 16:28:35 +0200

    muutos

--- protected/views/lasku/_form.php
+++ protected/views/lasku/_form.php
@@ -474,14 +474,17 @@ $model->viivastyskorko = $firma->viivastyskorko;
 
 		<?php if(isset($model->id)) : ?>
 		<a href="lasku_pdf?id=<?php echo $model->id; ?>" target="_blank" class="btn btn-sm btn-default btn-group"><?php echo Yii::t('main','Esikatselu'); ?></a>
-		<a href="finvoice?id=<?php echo $model->id; ?>&finvoice=true" target="_blank" class="btn btn-sm btn-default btn-group"><?php echo Yii::t('main','Lähetä Finvoice'); ?></a>
+
+		<?php if(isset($model->id) and $model->tilanne == '1') : ?>
+		<a href="finvoice?id=<?php echo $model->id; ?>&finvoice=true" target="_blank" class="btn btn-sm btn-success btn-group"><?php echo Yii::t('main','Lähetä Finvoice'); ?></a>
+		<?php endif; ?>
 
 		<?php if(isset($model->id) and $model->tilanne != '1' and $model->tilanne != '2') : ?>
 		<a href="update?id=<?php echo $model->id; ?>&tilanne=1" class="btn btn-sm btn-success btn-group"><?php echo Yii::t('main','Hyväksy'); ?></a>
 		<?php endif; ?>
 
 		<?php if(isset($model->id) and $model->tilanne == '1') : ?>
-		<a href="finvoice?id=<?php echo $model->id; ?>" class="btn btn-sm btn-success btn-group"><?php echo Yii::t('main','Lähetä'); ?></a>
+		<a href="finvoice?id=<?php echo $model->id; ?>&pdf=true" class="btn btn-sm btn-success btn-group"><?php echo Yii::t('main','Lähetä PDF'); ?></a>
 		<?php endif; ?>
 
 		<?php if(isset($model->id) and $model->tilanne == '2') : ?>
--- protected/views/lasku/finvoice.php
+++ protected/views/lasku/finvoice.php
@@ -6,131 +6,15 @@ function base64url_encode($input) {
 
 if(isset($_GET['finvoice'])){
 
-/*
-$xml = '<?xml-stylesheet type="text/xsl" href="/../tiedostot/finvoice/Finvoice.xsl"?>
-<Finvoice Version="2.0" xmlns:xsi="http://www.w3.org/2001/XMLSchemainstance"
-xsi:noNamespaceSchemaLocation="/../tiedostot/finvoice/Finvoice.xsd">
-  <SellerPartyDetails>
-    <SellerPartyIdentifier>'.$yritys['y_tunnus'].'</SellerPartyIdentifier>
-    <SellerOrganisationName>'.$yritys['tyonantaja'].'</SellerOrganisationName>
-    <SellerOrganisationTaxCode></SellerOrganisationTaxCode>
-    <SellerPostalAddressDetails>
-      <SellerStreetName>'.$yritys['osoite'].'</SellerStreetName>
-      <SellerTownName>'.$yritys['postitoimipaikka'].'</SellerTownName>
-      <SellerPostCodeIdentifier>'.$yritys['postinumero'].'</SellerPostCodeIdentifier>
-      <CountryCode>FI</CountryCode>
-      <CountryName>Finland</CountryName>
-    </SellerPostalAddressDetails>
-  </SellerPartyDetails>
-  <SellerContactPersonName>'.$yritys['johtaja'].'</SellerContactPersonName>
-  <SellerCommunicationDetails>
-    <SellerPhoneNumberIdentifier></SellerPhoneNumberIdentifier>
-    <SellerEmailaddressIdentifier></SellerEmailaddressIdentifier>
-  </SellerCommunicationDetails>
-  <SellerInformationDetails>
-    <SellerHomeTownName></SellerHomeTownName>
-    <SellerVatRegistrationText></SellerVatRegistrationText>
-    <SellerPhoneNumber>'.$yritys['puhelin'].'</SellerPhoneNumber>
-    <SellerFaxNumber></SellerFaxNumber>
-    <SellerCommonEmailaddressIdentifier>'.$yritys['sahkoposti'].'</SellerCommonEmailaddressIdentifier>
-    <SellerWebaddressIdentifier></SellerWebaddressIdentifier>
-    <SellerFreeText></SellerFreeText>
-    <SellerAccountDetails>
-      <SellerAccountID IdentificationSchemeName="IBAN">'.$asetukset['iban'].'</SellerAccountID>
-      <SellerBic IdentificationSchemeName="BIC">'.$asetukset['bic'].'</SellerBic>
-    </SellerAccountDetails>
-  </SellerInformationDetails>
-  <BuyerPartyDetails>
-    <BuyerPartyIdentifier>'.$lasku['as_nro'].'</BuyerPartyIdentifier>
-    <BuyerOrganisationName>'.$lasku['yritys'].'</BuyerOrganisationName>
-    <BuyerOrganisationTaxCode></BuyerOrganisationTaxCode>
-    <BuyerPostalAddressDetails>
-      <BuyerStreetName>'.$lasku['osoite'].'</BuyerStreetName>
-      <BuyerTownName>'.$lasku['toimipaikka'].'</BuyerTownName>
-      <BuyerPostCodeIdentifier>'.$lasku['postinumero'].'</BuyerPostCodeIdentifier>
-      <CountryName></CountryName>
-    </BuyerPostalAddressDetails>
-  </BuyerPartyDetails>
-  <BuyerContactPersonName>'.$lasku['yhteyshenkilo'].'</BuyerContactPersonName>
-  <BuyerCommunicationDetails>
-    <BuyerPhoneNumberIdentifier></BuyerPhoneNumberIdentifier>
-    <BuyerEmailaddressIdentifier>'.$lasku['sahkoposti'].'</BuyerEmailaddressIdentifier>
-  </BuyerCommunicationDetails>
-  <InvoiceDetails>
-    <InvoiceTypeCode></InvoiceTypeCode>
-    <InvoiceTypeText>LASKU</InvoiceTypeText>
-    <OriginCode></OriginCode>
-    <InvoiceNumber>'.$lasku['id'].'</InvoiceNumber>
-    <InvoiceDate Format="CCYYMMDD">'.date("Ymd",strtotime($lasku['time'])).'</InvoiceDate>
-    <SellerReferenceIdentifier></SellerReferenceIdentifier>
-    <OrderIdentifier></OrderIdentifier>
-    <InvoiceTotalVatExcludedAmount AmountCurrencyIdentifier="EUR">'.$lasku['yhteensa_total_veroton'].'</InvoiceTotalVatExcludedAmount>
-    <InvoiceTotalVatAmount AmountCurrencyIdentifier="EUR">'.$lasku['yhteensa_total_verot'].'</InvoiceTotalVatAmount>
-    <InvoiceTotalVatIncludedAmount AmountCurrencyIdentifier="EUR">'.$lasku['yhteensa_total'].'</InvoiceTotalVatIncludedAmount>
-    <VatSpecificationDetails>
-      <VatBaseAmount AmountCurrencyIdentifier="EUR">'.$lasku['maksettu_euro'].'</VatBaseAmount>
-      <VatRatePercent>24</VatRatePercent>
-      <VatRateAmount AmountCurrencyIdentifier="EUR">'.$lasku['yhteensa_total_verot'].'</VatRateAmount>
-    </VatSpecificationDetails>
-    <PaymentTermsDetails>
-      <PaymentTermsFreeText></PaymentTermsFreeText>
-      <InvoiceDueDate Format="CCYYMMDD">'.date("Ymd",strtotime($lasku['erapaiva'])).'</InvoiceDueDate>
-      <PaymentOverDueFineDetails>
-        <PaymentOverDueFineFreeText></PaymentOverDueFineFreeText>
-        <PaymentOverDueFinePercent></PaymentOverDueFinePercent>
-      </PaymentOverDueFineDetails>
-    </PaymentTermsDetails>
-  </InvoiceDetails>
-  <PaymentStatusDetails>
-    <PaymentStatusCode></PaymentStatusCode>
-  </PaymentStatusDetails>
-  <VirtualBankBarcode>'.$lasku['saaja_virtualkoodi'].'</VirtualBankBarcode>';
+if($lasku['tyyppi'] == 'yritys')
+$BuyerOrganisationName = $lasku['yritys'];
+if($lasku['tyyppi'] == 'henkilo')
+$BuyerOrganisationName = $lasku['nimi'];
 
-  foreach($laskunRivit as $rivi){
-  $xml .= '
-  <InvoiceRow>
-    <ArticleIdentifier>'.$rivi['id'].'</ArticleIdentifier>
-    <ArticleName>'.$rivi['tkoodi'].'</ArticleName>
-    <DeliveredQuantity QuantityUnitCode="pcs">'.$rivi['kpl'].'</DeliveredQuantity>
-    <OrderedQuantity QuantityUnitCode="pcs">'.$rivi['kpl'].'</OrderedQuantity>
-    <UnitPriceAmount AmountCurrencyIdentifier="EUR" UnitPriceUnitCode="pcs">'.$rivi['hinta'].'</UnitPriceAmount>
-    <RowNormalProposedAccountIdentifier></RowNormalProposedAccountIdentifier>
-    <RowAccountDimensionText></RowAccountDimensionText>
-    <RowVatRatePercent>'.$rivi['alv'].',00</RowVatRatePercent>
-    <RowVatAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",($rivi['yhteensa_alv']-$rivi['veroton'])).'</RowVatAmount>
-    <RowVatExcludedAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$rivi['veroton']).'</RowVatExcludedAmount>
-    <RowAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$rivi['yhteensa_alv']).'</RowAmount>
-  </InvoiceRow>';
-  }
-
-$xml .= '
-  <SpecificationDetails />
-  <EpiDetails>
-    <EpiIdentificationDetails>
-      <EpiDate Format="CCYYMMDD">'.date("Ymd",strtotime($lasku['erapaiva'])).'</EpiDate>
-      <EpiReference></EpiReference>
-    </EpiIdentificationDetails>
-    <EpiPartyDetails>
-      <EpiBfiPartyDetails>
-        <EpiBfiIdentifier IdentificationSchemeName="BIC"></EpiBfiIdentifier>
-      </EpiBfiPartyDetails>
-      <EpiBeneficiaryPartyDetails>
-        <EpiNameAddressDetails></EpiNameAddressDetails>
-        <EpiBei></EpiBei>
-        <EpiAccountID IdentificationSchemeName="IBAN"></EpiAccountID>
-      </EpiBeneficiaryPartyDetails>
-    </EpiPartyDetails>
-    <EpiPaymentInstructionDetails>
-      <EpiPaymentInstructionId>1001</EpiPaymentInstructionId>
-      <EpiRemittanceInfoIdentifier IdentificationSchemeName="SPY"></EpiRemittanceInfoIdentifier>
-      <EpiInstructedAmount AmountCurrencyIdentifier="EUR"></EpiInstructedAmount>
-      <EpiCharge ChargeOption="SHA"></EpiCharge>
-      <EpiDateOptionDate Format="CCYYMMDD"></EpiDateOptionDate>
-    </EpiPaymentInstructionDetails>
-  </EpiDetails>
-<InvoiceUrlText>12345678+102030FK405060708091011121314156</InvoiceUrlText>
-</Finvoice>';
-*/
+if($lasku['tyyppi'] == 'yritys')
+$BuyerContactPersonName = $lasku['yhteyshenkilo'];
+if($lasku['tyyppi'] == 'henkilo')
+$BuyerContactPersonName = $lasku['nimi'];
 
 $xml = '<?xml version="1.0" encoding="ISO-8859-15"?>
 <!-- edited with XML Spy v4.2 U (http://www.xmlspy.com) by Jussi Paasikallio (OKOBANK Group) -->
@@ -166,7 +50,7 @@ $xml = '<?xml version="1.0" encoding="ISO-8859-15"?>
 <SellerBic IdentificationSchemeName="BIC">OKOYFIHH</SellerBic>
 </SellerAccountDetails>
 <InvoiceRecipientDetails>
-<InvoiceRecipientAddress></InvoiceRecipientAddress>
+<InvoiceRecipientAddress>'.$lasku['osoite'].'</InvoiceRecipientAddress>
 <InvoiceRecipientIntermediatorAddress></InvoiceRecipientIntermediatorAddress>
 </InvoiceRecipientDetails>
 </SellerInformationDetails>
@@ -177,55 +61,53 @@ $xml = '<?xml version="1.0" encoding="ISO-8859-15"?>
 </InvoiceSenderPartyDetails>
 <InvoiceRecipientPartyDetails>
 <InvoiceRecipientPartyIdentifier/>
-<InvoiceRecipientOrganisationName>Tilitoimisto Ryynänen</InvoiceRecipientOrganisationName>
-<InvoiceRecipientOrganisationName>Accounting Company Ryynänen</InvoiceRecipientOrganisationName>
+<InvoiceRecipientOrganisationName>'.$lasku['yritys'].'</InvoiceRecipientOrganisationName>
 <InvoiceRecipientPostalAddressDetails>
-<InvoiceRecipientStreetName>Mäkelänkatu 2</InvoiceRecipientStreetName>
-<InvoiceRecipientTownName>Helsinki</InvoiceRecipientTownName>
-<InvoiceRecipientPostCodeIdentifier>00102</InvoiceRecipientPostCodeIdentifier>
+<InvoiceRecipientStreetName>'.$lasku['osoite'].'</InvoiceRecipientStreetName>
+<InvoiceRecipientTownName>'.$lasku['toimipaikka'].'</InvoiceRecipientTownName>
+<InvoiceRecipientPostCodeIdentifier>'.$lasku['postinumero'].'</InvoiceRecipientPostCodeIdentifier>
 <CountryCode>FI</CountryCode>
 <CountryName>FINLAND</CountryName>
-<InvoiceRecipientPostOfficeBoxIdentifier>PL 22</InvoiceRecipientPostOfficeBoxIdentifier>
+<InvoiceRecipientPostOfficeBoxIdentifier></InvoiceRecipientPostOfficeBoxIdentifier>
 </InvoiceRecipientPostalAddressDetails>
 </InvoiceRecipientPartyDetails>
 <BuyerPartyDetails>
-<BuyerPartyIdentifier>0123456-7</BuyerPartyIdentifier>
-<BuyerOrganisationName>Sensorit Oy</BuyerOrganisationName>
-<BuyerOrganisationTaxCode>0123456-7</BuyerOrganisationTaxCode>
+<BuyerPartyIdentifier>'.$lasku['as_nro'].'</BuyerPartyIdentifier>
+<BuyerOrganisationName>'.$BuyerOrganisationName.'</BuyerOrganisationName>
+<BuyerOrganisationTaxCode>'.$lasku['y_tunnus'].'</BuyerOrganisationTaxCode>
 <BuyerPostalAddressDetails>
-<BuyerStreetName>Sempalokatu 2</BuyerStreetName>
-<BuyerTownName>HELSINKI</BuyerTownName>
-<BuyerPostCodeIdentifier>00122</BuyerPostCodeIdentifier>
+<BuyerStreetName>'.$lasku['osoite'].'</BuyerStreetName>
+<BuyerTownName>'.$lasku['toimipaikka'].'</BuyerTownName>
+<BuyerPostCodeIdentifier>'.$lasku['postinumero'].'</BuyerPostCodeIdentifier>
 </BuyerPostalAddressDetails>
 </BuyerPartyDetails>
-<BuyerContactPersonName>Hannes Puumalainen</BuyerContactPersonName>
+<BuyerContactPersonName>'.$BuyerContactPersonName.'</BuyerContactPersonName>
 <BuyerCommunicationDetails>
-<BuyerPhoneNumberIdentifier>050-543 2658</BuyerPhoneNumberIdentifier>
-<BuyerEmailaddressIdentifier>hannes.puumalainen@sensorit.fi</BuyerEmailaddressIdentifier>
+<BuyerPhoneNumberIdentifier>'.$lasku['puhelin'].'</BuyerPhoneNumberIdentifier>
+<BuyerEmailaddressIdentifier>'.$lasku['sahkoposti'].'</BuyerEmailaddressIdentifier>
 </BuyerCommunicationDetails>
 <DeliveryPartyDetails>
 <DeliveryPartyIdentifier/>
-<DeliveryOrganisationName>Helsingin Tanssihalli</DeliveryOrganisationName>
+<DeliveryOrganisationName>'.$lasku['t_yritys'].'</DeliveryOrganisationName>
 <DeliveryPostalAddressDetails>
-<DeliveryStreetName>Satamakatu 2</DeliveryStreetName>
-<DeliveryTownName>Helsinki</DeliveryTownName>
-<DeliveryPostCodeIdentifier>00100</DeliveryPostCodeIdentifier>
+<DeliveryStreetName>'.$lasku['t_osoite'].'</DeliveryStreetName>
+<DeliveryTownName>'.$lasku['t_toimipaikka'].'</DeliveryTownName>
+<DeliveryPostCodeIdentifier>'.$lasku['t_postinumero'].'</DeliveryPostCodeIdentifier>
 <CountryName/>
 <DeliveryPostofficeBoxIdentifier/>
 </DeliveryPostalAddressDetails>
 </DeliveryPartyDetails>
 <DeliveryDetails>
-<DeliveryDate Format="CCYYMMDD">20041205</DeliveryDate>
-<DeliveryMethodText>Noudetaan</DeliveryMethodText>
-<DeliveryTermsText>Vapaasti varastosta VOB</DeliveryTermsText>
-<TerminalAddressText>Vantaan postiterminaali</TerminalAddressText>
-<WaybillIdentifier>419/2004</WaybillIdentifier>
-<WaybillTypeCode>WBGF</WaybillTypeCode>
-<DelivererIdentifier>DeliID12222</DelivererIdentifier>
-<DelivererName>Oy Lähettifirma Ab</DelivererName>
-<DelivererName>Packgage Ltd.</DelivererName>
-<DelivererCountryCode>FI</DelivererCountryCode>
-<DelivererCountryName>FINLAND</DelivererCountryName>
+<DeliveryDate Format="CCYYMMDD">'.date("Ymd").'</DeliveryDate>
+<DeliveryMethodText></DeliveryMethodText>
+<DeliveryTermsText></DeliveryTermsText>
+<TerminalAddressText></TerminalAddressText>
+<WaybillIdentifier></WaybillIdentifier>
+<WaybillTypeCode></WaybillTypeCode>
+<DelivererIdentifier></DelivererIdentifier>
+<DelivererName></DelivererName>
+<DelivererCountryCode></DelivererCountryCode>
+<DelivererCountryName></DelivererCountryName>
 <ManufacturerIdentifier>13331231233</ManufacturerIdentifier>
 <ManufacturerName>AKG International</ManufacturerName>
 <ManufacturerCountryCode>DE</ManufacturerCountryCode>
@@ -239,24 +121,24 @@ $xml = '<?xml version="1.0" encoding="ISO-8859-15"?>
 <InvoiceDate Format="CCYYMMDD">20041215</InvoiceDate>
 <SellerReferenceIdentifier>MYY21231</SellerReferenceIdentifier>
 <OrderIdentifier>TIL21222</OrderIdentifier>
-<InvoiceTotalVatExcludedAmount AmountCurrencyIdentifier="EUR">2830,30</InvoiceTotalVatExcludedAmount>
-<InvoiceTotalVatAmount AmountCurrencyIdentifier="EUR">622,68</InvoiceTotalVatAmount>
-<InvoiceTotalVatIncludedAmount AmountCurrencyIdentifier="EUR">3352,98</InvoiceTotalVatIncludedAmount>
+<InvoiceTotalVatExcludedAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total_veroton']).'</InvoiceTotalVatExcludedAmount>
+<InvoiceTotalVatAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total_verot']).'</InvoiceTotalVatAmount>
+<InvoiceTotalVatIncludedAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total']).'</InvoiceTotalVatIncludedAmount>
 <VatSpecificationDetails>
-<VatBaseAmount AmountCurrencyIdentifier="EUR">2830,30</VatBaseAmount>
+<VatBaseAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total_veroton']).'</VatBaseAmount>
 <VatRatePercent>22</VatRatePercent>
-<VatRateAmount AmountCurrencyIdentifier="EUR">622,68</VatRateAmount>
+<VatRateAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total_verot']).'</VatRateAmount>
 </VatSpecificationDetails>
 <PaymentTermsDetails>
-<PaymentTermsFreeText>5 pävää ./.2%, 14 päivää netto</PaymentTermsFreeText>
+<PaymentTermsFreeText>'.$lasku['maksuehto'].' pv</PaymentTermsFreeText>
 <InvoiceDueDate Format="CCYYMMDD">'.date("Ymd",strtotime($lasku['erapaiva'])).'</InvoiceDueDate>
 <CashDiscountDate Format="CCYYMMDD">20041220</CashDiscountDate>
-<CashDiscountBaseAmount AmountCurrencyIdentifier="EUR">3352,98</CashDiscountBaseAmount>
+<CashDiscountBaseAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total']).'</CashDiscountBaseAmount>
 <CashDiscountPercent>2</CashDiscountPercent>
-<CashDiscountAmount AmountCurrencyIdentifier="EUR">67,06</CashDiscountAmount>
+<CashDiscountAmount AmountCurrencyIdentifier="EUR"></CashDiscountAmount>
 <PaymentOverDueFineDetails>
 <PaymentOverDueFineFreeText>Viivästyskorko 16%</PaymentOverDueFineFreeText>
-<PaymentOverDueFinePercent>16</PaymentOverDueFinePercent>
+<PaymentOverDueFinePercent>'.$lasku['viivastyskorko'].'</PaymentOverDueFinePercent>
 </PaymentOverDueFineDetails>
 </PaymentTermsDetails>
 </InvoiceDetails>
@@ -267,13 +149,13 @@ $xml = '<?xml version="1.0" encoding="ISO-8859-15"?>
 
 foreach($laskunRivit as $rivi){
 $xml .= '<InvoiceRow>
-<RowSubIdentifier>'.$rivi['id'].'</RowSubIdentifier>
-<ArticleIdentifier>'.$rivi['id'].'</ArticleIdentifier>
+<RowSubIdentifier></RowSubIdentifier>
+<ArticleIdentifier></ArticleIdentifier>
 <ArticleName>'.$rivi['tkoodi'].'</ArticleName>
 <DeliveredQuantity QuantityUnitCode="kpl">'.$rivi['kpl'].'</DeliveredQuantity>
 <OrderedQuantity QuantityUnitCode="kpl">'.$rivi['kpl'].'</OrderedQuantity>
 <UnitPriceAmount AmountCurrencyIdentifier="EUR">'.$rivi['hinta'].'</UnitPriceAmount>
-<RowIdentifier>TIL2122</RowIdentifier>
+<RowIdentifier></RowIdentifier>
 <RowDeliveryDate Format="CCYYMMDD"></RowDeliveryDate>
 <RowAgreementIdentifier></RowAgreementIdentifier>
 <RowRequestOfQuotationIdentifier></RowRequestOfQuotationIdentifier>
@@ -302,24 +184,24 @@ $xml .= '<InvoiceRow>
 
 $xml .= '<EpiDetails>
 <EpiIdentificationDetails>
-<EpiDate Format="CCYYMMDD">20041215</EpiDate>
+<EpiDate Format="CCYYMMDD">'.$lasku['erapaiva'].'</EpiDate>
 <EpiReference>2004486</EpiReference>
 </EpiIdentificationDetails>
 <EpiPartyDetails>
 <EpiBfiPartyDetails>
-<EpiBfiIdentifier IdentificationSchemeName="BIC">OKOYFIHH</EpiBfiIdentifier>
+<EpiBfiIdentifier IdentificationSchemeName="BIC">'.$asetukset['bic'].'</EpiBfiIdentifier>
 </EpiBfiPartyDetails>
 <EpiBeneficiaryPartyDetails>
-<EpiNameAddressDetails>Pullin Musiikki Oy</EpiNameAddressDetails>
-<EpiBei>0199920-7</EpiBei>
-<EpiAccountID IdentificationSchemeName="BBAN">50001520000081</EpiAccountID>
+<EpiNameAddressDetails>'.$yritys['tyonantaja'].'</EpiNameAddressDetails>
+<EpiBei></EpiBei>
+<EpiAccountID IdentificationSchemeName="BBAN">'.$asetukset['tilinumero'].'</EpiAccountID>
 </EpiBeneficiaryPartyDetails>
 </EpiPartyDetails>
 <EpiPaymentInstructionDetails>
-<EpiRemittanceInfoIdentifier IdentificationSchemeName="SPY">00000000000002004486</EpiRemittanceInfoIdentifier>
-<EpiInstructedAmount AmountCurrencyIdentifier="EUR">3352,98</EpiInstructedAmount>
+<EpiRemittanceInfoIdentifier IdentificationSchemeName="SPY">'.$lasku['viitenumero'].'</EpiRemittanceInfoIdentifier>
+<EpiInstructedAmount AmountCurrencyIdentifier="EUR">'.str_replace(".",",",$lasku['yhteensa_total']).'</EpiInstructedAmount>
 <EpiCharge ChargeOption="SHA">SHA</EpiCharge>
-<EpiDateOptionDate Format="CCYYMMDD">20041229</EpiDateOptionDate>
+<EpiDateOptionDate Format="CCYYMMDD">'.$lasku['erapaiva'].'</EpiDateOptionDate>
 </EpiPaymentInstructionDetails>
 </EpiDetails>
 <InvoiceUrlNameText></InvoiceUrlNameText>
@@ -411,13 +293,13 @@ header('Content-Disposition: inline; filename="report.xml"');
 	//unlink($file);
 
 
-exit;
 }
 
 
 
 
 
+if(isset($_GET['pdf'])){
 
 $username = "Sivex";
 $password = "Etunti2000";
@@ -443,8 +325,11 @@ $account_info = curl_exec($ch);
 $account_info = json_decode($account_info, true);
 
   $prep = array();
-  foreach($account_info as $k => $v ) {
-    $prep[$k] = $k.":".$v;
+  if(isset($account_info[0]))
+  {
+    foreach($account_info as $k => $v ) {
+      $prep[$k] = $k.":".$v;
+    }
   }
 
 echo '<pre>';
@@ -497,7 +382,7 @@ curl_close($ch);
 
 
   $id = $_GET['id'];
-  if(isset($send_response) and $send_response['status'] == 'CO'){
+  if(isset($send_response[0]) and $send_response['status'] == 'CO'){
 
 
   foreach($send_response as $k => $v ) {
@@ -509,7 +394,7 @@ curl_close($ch);
   //echo '<h2>'.Yii::t('main','Lasku lähetetty onnistuneesti').'</h2>';
   }
 
-
+}
 
 
 
