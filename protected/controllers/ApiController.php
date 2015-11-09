96f82f30048561e703ccfa7c168beddfa689abf0
laptopsr <laptopsr@gmail.com>
2015-11-09 14:13:17 +0200

    m

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -543,7 +543,7 @@ public function actionImei($dom)
 
 	    $criteria = new CDbCriteria();
 	    $criteria->order = " 
-		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s') 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') 
 		AND status IN (1,2,10)
 		AND loppui='' DESC, id DESC ";
 	    $criteria->condition = " 
