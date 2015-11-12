c0dabdfd3e1068857b9381e67f84faf190dd0fe9
laptopsr <laptopsr@gmail.com>
2015-11-12 13:36:33 +0200

    update

--- test.php
+++ test.php
@@ -1,3 +1,25 @@
 <?php
-?>
+/*
+$ch = curl_init();
 
+$data = array('cid'=>'1008168', 'apicode'=>'kvrj44mqp9');
+
+curl_setopt($ch, CURLOPT_URL, 'https://www.trustpoint.fi/API/statusupdates.php');
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+$result = curl_exec($ch);
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+*/
+
+?>
+<!--
+<form action="https://www.trustpoint.fi/API/statusupdates.php" method="POST">
+<input type="text" name="cid">
+<input type="text" name="apicode">
+<input type="submit">
+</form>
+-->
