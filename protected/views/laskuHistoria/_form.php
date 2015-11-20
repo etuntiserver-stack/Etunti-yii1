efd83d0b6c4cf0a66c12542cbc3c5be5834223a2
laptopsr <laptopsr@gmail.com>
2015-11-20 14:57:48 +0200

    lasku historia

---
+++ protected/views/laskuHistoria/_form.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this LaskuHistoriaController */
+/* @var $model LaskuHistoria */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'lasku-historia-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'lid'); ?>
+		<?php echo $form->textField($model,'lid'); ?>
+		<?php echo $form->error($model,'lid'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'time'); ?>
+		<?php echo $form->textField($model,'time'); ?>
+		<?php echo $form->error($model,'time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'status'); ?>
+		<?php echo $form->textField($model,'status',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'status'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'yht_euro'); ?>
+		<?php echo $form->textField($model,'yht_euro',array('size'=>50,'maxlength'=>50)); ?>
+		<?php echo $form->error($model,'yht_euro'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
