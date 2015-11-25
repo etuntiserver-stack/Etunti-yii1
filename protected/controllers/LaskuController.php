85b1c66a8f101f185022fb6ba7ec0e2be9630073
laptopsr <laptopsr@gmail.com>
2015-11-25 10:23:00 +0200

    m

--- protected/controllers/LaskuController.php
+++ protected/controllers/LaskuController.php
@@ -504,7 +504,7 @@ echo '</pre>';
 	 {
 	  foreach ($xml as $r) {
 		$str = '';
-	    	$str = 'statustime:'.trim($r->statustime).'//jobid:'.trim($r->jobid).'//billnum:'.trim($r->billnum).'//statusref:'.trim($r->statusref).'//statustext:'.trim($r->statustext).'//statuscode:'.trim($r->statuscode).'//statusid:'.trim($r->statusid);
+	    	$str = 'statustime:'.trim($r->statustime).'//jobid:'.trim($r->jobid).'//billnum:'.trim($r->billnum).'//statusref:'.trim($r->statusref).'//statustext:'.trim($r->statustext).'//statuscode:'.trim($r->statuscode).'//statusid:'.trim($r->statusid).'//paydate:'.trim($r->paydate).'//paydate:'.trim($r->amount).'//statustype:'.trim($r->statustype);
 	
 		$l = Lasku::model()->find(" trust_jobid='".trim($r->jobid)."' ");
 		if(isset($l['id']))
