368a9e9025b389c6bf7a81184abf65386ca9d425
laptopsr <laptopsr@gmail.com>
2015-12-02 13:20:44 +0200

    tuoteet ja palvelut

--- protected/views/laskutusTuotteet/_form.php
+++ protected/views/laskutusTuotteet/_form.php
@@ -11,31 +11,30 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'tuotenimi'); ?>
-		<?php echo $form->textField($model,'tuotenimi',array('size'=>60,'maxlength'=>100,'class'=>'form-control')); ?>
+		<?php echo $form->textField($model,'tuotenimi',array('size'=>60,'maxlength'=>100,'class'=>'form-control input-sm')); ?>
 		<?php echo $form->error($model,'tuotenimi'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'hinta_alv_0'); ?>
-		<?php echo $form->textField($model,'hinta_alv_0',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+		<?php echo $form->textField($model,'hinta_alv_0',array('size'=>20,'maxlength'=>20,'class'=>'form-control input-sm')); ?>
 		<?php echo $form->error($model,'hinta_alv_0'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'alv'); ?>
-		<?php echo $form->textField($model,'alv',array('size'=>10,'maxlength'=>10,'class'=>'form-control')); ?>
+		<?php echo $form->textField($model,'alv',array('size'=>10,'maxlength'=>10,'class'=>'form-control input-sm')); ?>
 		<?php echo $form->error($model,'alv'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'yksikko'); ?>
-		<?php echo $form->textField($model,'yksikko',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+		<?php echo $form->textField($model,'yksikko',array('size'=>20,'maxlength'=>20,'class'=>'form-control input-sm')); ?>
 		<?php echo $form->error($model,'yksikko'); ?>
 	</div>
 
@@ -51,7 +50,7 @@
 /*
 	<div class="row">
 		<?php echo $form->labelEx($model,'hinta_alv_sis'); ?>
-		<?php echo $form->textField($model,'hinta_alv_sis',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+		<?php echo $form->textField($model,'hinta_alv_sis',array('size'=>20,'maxlength'=>20,'class'=>'form-control input-sm')); ?>
 		<?php echo $form->error($model,'hinta_alv_sis'); ?>
 	</div>
 */
