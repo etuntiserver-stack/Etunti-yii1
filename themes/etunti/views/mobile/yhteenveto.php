413fc47c6451d0ad8d407c8e74a2db83c20e4f83
laptopsr <laptopsr@gmail.com>
2016-01-11 09:58:23 +0200

    theme

--- themes/etunti/views/mobile/yhteenveto.php
+++ themes/etunti/views/mobile/yhteenveto.php
@@ -178,7 +178,7 @@ $this->breadcrumbs=array(
         $total_lu += $data->l_tunnit;
 	$tp = $this->Tp($data->tid,$from,$to);
 	$totalTp += $tp;
-	$tot_sun = $this->renderPartial('//mobile/suunniteltu',array('id'=>$data->tid,'kohde_tid'=>'tid'),true);
+	$tot_sun = $this->renderPartial('//mobile/suunniteltu',array('id'=>$data->tid,'kohde_tid'=>'tid','from'=>$from,'to'=>$to),true);
 	$total_sunniteltu += $tot_sun;
 
 	$this->renderPartial('_yhteenveto',array('data'=>$data,'tp'=>$tp,'tot_sun'=>$tot_sun,'from'=>$from,'to'=>$to));
