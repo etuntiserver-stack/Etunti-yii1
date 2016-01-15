54882960f8f2a1326bbf2ee3388d42eb52e18b5f
laptopsr <laptopsr@gmail.com>
2016-01-15 12:14:40 +0200

    m

--- themes/classic/views/site/index.php
+++ themes/classic/views/site/index.php
@@ -75,7 +75,7 @@
                     <div class="col-md-3">
                         <div class="logo">
                             <a href="index.html" title="Return Home">
-                                <img src="../img/logo.png" alt="Logo" class="logo_img">
+                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets_classic/img/logo.png" alt="Logo" class="logo_img">
                             </a>
                         </div>
                     </div>
