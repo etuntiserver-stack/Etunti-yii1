e3f5c024873909900186b4dbcd1c57a0e8830523
laptopsr <laptopsr@gmail.com>
2016-01-19 09:58:14 +0200

    luo asiakas lomake

--- themes/etunti/views/asiakkaat/create.php
+++ themes/etunti/views/asiakkaat/create.php
@@ -2,13 +2,23 @@
 
 ?>
 
-  <div class="panel heading-border">
-   <div class="panel-body bg-light">
-      <div class="section-divider mb40" id="spy1">
-      <span> <?php echo Yii::t('main', 'LUO ASIAKAS'); ?> <i class="glyphicon glyphicon-user"></i> </span>
-      </div>
+        <!-- begin: .tray-center -->
+        <div class="tray-center">
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	   <h2 class="myBgColors p10"> <i class="glyphicon glyphicon-user"></i> <?php echo Yii::t('main', 'LUO ASIAKAS'); ?> </h2>
 
-   </div>
-  </div>
+
+            <div class="admin-form">
+              <div class="panel heading-border">
+                <div class="panel-body bg-light">
+                 <div class="row">
+		  <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+                 </div>
+                </div>
+              </div>
+            </div>
+
+        <!-- loppu: .tray-center -->
+        </div>
+
+
