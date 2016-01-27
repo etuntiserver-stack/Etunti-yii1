04e3c6e629c711ed31de73298ff397cfaff79c4f
laptopsr <laptopsr@gmail.com>
2016-01-27 10:41:46 +0200

    m

--- themes/classic/views/site/index.php
+++ themes/classic/views/site/index.php
@@ -633,7 +633,7 @@ $kysymykset[] = array(
       <div class="modal-footer">
         <div class="testerintulos"></div>
         <button type="button" class="btn btn-lg btn-primary">Näytä tulos</button>
-        <a href="#" class="btn btn-lg btn-default tarjouspyynto" data-toggle="modal" data-target=".pyydatarjous_lomake">Lähetä tulos sähköpostiini</a>
+        <!--<a href="#" class="btn btn-lg btn-default tarjouspyynto" data-toggle="modal" data-target=".pyydatarjous_lomake">Lähetä tulos sähköpostiini</a>-->
         <a href="#" class="btn btn-lg btn-default tarjouspyynto" data-toggle="modal" data-target=".pyydatarjous_lomake">Pyydä tarjous</a>
       </div>
     </div><!-- /.modal-content -->
