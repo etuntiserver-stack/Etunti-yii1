099ceb6c9793de0ff492dbbf93dead69c19f66b4
laptopsr <laptopsr@gmail.com>
2016-01-29 14:48:44 +0200

    m

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -88,7 +88,7 @@ if (
                     <!-- Logo-->
                     <div class="col-md-3">
                         <div class="logo">
-                            <a href="index.php" title="Return Home">
+                            <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php" title="Return Home">
                                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets_classic/img/logo.png" alt="Logo" class="logo_img">
                             </a>
                         </div>
