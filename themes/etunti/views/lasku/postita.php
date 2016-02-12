67fecf490622d9fd379e7ee8b0d752e3fd845104
laptopsr <laptopsr@gmail.com>
2016-02-12 10:50:39 +0200

    m

--- themes/etunti/views/lasku/postita.php
+++ themes/etunti/views/lasku/postita.php
@@ -1,19 +1,29 @@
 <?php
 
+?>
+
+<b>DR</b>	Draft. Only for massmail.<br>
+<b>NE</b>	New. Job has been loaded but not confirmed to be sent<br>
+<b>CO</b>	Confirmed. Job is confirmed to be sent at 11 o’clock next work day.<br>
+<b>CA</b>	Canceled. Job is cancelled.<br>
+<b>PR</b>	Processing. Job currently in been processed and printed to be mailed<br>
+<b>SE</b>	Sent. Job has been sent.<br>
+
+<br><br>
+<?php
+
   foreach($lasku as $l)
   {
 	echo $l->postita_jobid.'<br>';
   }
 
+  echo '<br>';
 
 $username = $asetukset['postita_username'];
 $password = $asetukset['postita_password'];
 $auth_string = $username . ":" . $password;
 
-
 $url = 'https://postita.fi/api/job_list/';
-
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -28,9 +38,25 @@ $account_info = json_decode($account_info, true);
 echo '<pre>';
 print_r($account_info);
 echo '</pre>';
-
-
 curl_close($ch);
 
+
+
+$url = 'https://postita.fi/api/job_pdf/1949441';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERPWD, $auth_string);
+curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+
+$account_info = curl_exec($ch);
+$account_info = json_decode($account_info, true);
+
+echo '<pre>';
+print_r($account_info);
+echo '</pre>';
+curl_close($ch);
 
 ?>
