fee1084291b6dc42ef063c70d279428c2364e93c
laptopsr <laptopsr@gmail.com>
2016-02-18 10:18:14 +0200

    m

--- themes/etunti/views/lasku/finvoice.php
+++ themes/etunti/views/lasku/finvoice.php
@@ -290,13 +290,7 @@ $xml = encodeXml (array(
                 "language" => "fin", # laskun kieli
                 "sendtype" => $sendtype, # laskun lähetystapa
                 "postclass" => $postclass, # 1 = postitus 1.luokassa
-                "color" => 0, # 0 = mustavalko//
-                "printoperator" => "enfo", # tulostusoperaattori
-                "billtemplate" => "CUSTOM", # laskupohja
-                "collectionprocess" => "AUTO", # saatavan laji
-                "netamount" => $lasku['yhteensa_total_veroton'], # veroton hinta yhteensä
-                "vatamount" => $lasku['yhteensa_total_verot'], # veron määrä yhteensä
-                "totalamount" => $lasku['yhteensa_total'], # verollinen loppusumma
+                "amount" => $lasku['yhteensa_total'], # verollinen loppusumma
 
                 # Myytävät tuotteet
                 "payrow" => $rowsArray,
