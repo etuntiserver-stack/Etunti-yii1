65a50be1b0734318adef4bffd80fd789ea585b16
laptopsr <laptopsr@gmail.com>
2016-03-10 13:41:49 +0200

    f

--- js/tvuoroot.js
+++ js/tvuoroot.js
@@ -18,6 +18,7 @@ $('#tyontekijat').multiselect({
 	nonSelectedText: 'Tyhjä',
 	selectAllText: 'Valitse kaikki',
 	allSelectedText: 'Kaikki',
+	selectedText: 'valittu',
 });
 /*
 $('#tyontekijat').selectpicker({
