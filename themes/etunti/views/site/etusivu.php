ae0f5bc139012cc2635a91084ab08667325cc2aa
laptopsr <laptopsr@gmail.com>
2016-03-30 10:18:36 +0300

    m

--- themes/etunti/views/site/etusivu.php
+++ themes/etunti/views/site/etusivu.php
@@ -27,8 +27,7 @@ if (!file_exists(Yii::app()->basePath."/../backup/".Yii::app()->user->domain.'/'
   } else {
 
   	exec("/usr/bin/mysqldump -u estromfi_".Yii::app()->user->domain." -pKristinA1 estromfi_".Yii::app()->user->domain."", $output);
-print_r($output);
-  	//file_put_contents($file.'.gz', gzencode($output));
+  	file_put_contents($file, $output);
       	echo '<span id="uusiVarmuskopioText">uusi varmuskopio on tehty</span>';
 
   }
