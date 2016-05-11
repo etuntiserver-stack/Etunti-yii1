a632acbe5b02151aa5fb458ce602a9e94d55ab8c
Roman <laptopsr@gmail.com>
2016-05-11 10:11:00 +0300

    robots

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -547,7 +547,7 @@
           <li>
             <a class="accordion-toggle" href="#">
               <span class="fa fa-envelope"></span>
-              <span class="sidebar-title"><?php echo Yii::t('main', 'Viestintä hallinta'); ?></span>
+              <span class="sidebar-title"><?php echo Yii::t('main', 'Viestinnän hallinta'); ?></span>
               <span class="caret"></span>
             </a>
             <ul class="nav sub-nav">
