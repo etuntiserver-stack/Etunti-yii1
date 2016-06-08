2e97843a9a6942f36a67a57fe85db81639b353cc
Roman <laptopsr@gmail.com>
2016-06-08 14:51:31 +0300

    palaute

--- themes/etunti/views/palautteet/_view.php
+++ themes/etunti/views/palautteet/_view.php
@@ -1,37 +1,70 @@
 <?php
-/* @var $this PalautteetController */
-/* @var $data Palautteet */
+/* @var $this KohteetController */
+/* @var $data Kohteet */
+
+		$as = Asiakkaat::model()->findbypk($data->asiakas_id);
+			
+		$nimi = '';
+		if(isset($as->yrityksen_nimi) and !empty($as->yrityksen_nimi))
+		$nimi = $as->yrityksen_nimi;
+		elseif(isset($as->yhteyshenkilo) and !empty($as->yhteyshenkilo))
+		$nimi = $as->yhteyshenkilo;
+
+
+		$criteria=new CDbCriteria;
+		$criteria->order = " DATE(time) DESC ";
+		$criteria->condition = "
+			asiakas_id='".$data->asiakas_id."' 
+			AND keskustelu_id!=id
+		";
+		$p_juttelu = Palautteet::model()->findAll($criteria);
 ?>
 
-<div class="view">
+<tr>
+	<td>
+		<?php echo date("d.m.Y H:i", strtotime($data->time)); ?>
+	</td>
+	<td>
+		<?php echo $nimi; ?>
+	</td>
+	<td>
+		<?php echo $data->otsikko; ?>
+	</td>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
+<?php
+	  	$bod = '';
+	  	$bod .= '<td>';
+		$bod .= '<p>'.$data->teksti.'</p>';
+		   foreach($p_juttelu as $data2)
+		   {
+			$bod .= '<p>'.$data2->teksti.'</p>';
+		   }
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('keskustelu_id')); ?>:</b>
-	<?php echo CHtml::encode($data->keskustelu_id); ?>
-	<br />
+		if($data->status == 0)
+		$bod .= CHtml::link(Yii::t('main', 'Vasta'), Yii::app()->request->baseUrl.'/index.php/palautteet/vastaus?id='.$data->keskustelu_id,array('class'=>'btn btn-primary btn-sm', 'style'=>'color:white'));
+	  	$bod .= '</td>';
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('time')); ?>:</b>
-	<?php echo CHtml::encode($data->time); ?>
-	<br />
+	  	$bod .= '<td>';
+		if($data->status == 0)
+	  	$bod .= '<span class="btn btn-sm btn-warning btn-block">'.Yii::t('main', 'avoin').'</span>';
+		elseif($data->status == 3)
+	  	$bod .= '<span class="btn btn-sm btn-success btn-block">'.Yii::t('main', 'suljettu').'</span>';
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('asiakas_id')); ?>:</b>
-	<?php echo CHtml::encode($data->asiakas_id); ?>
-	<br />
+		if(isset(Yii::app()->user->adminID) and $data->status != 3)
+		{
+		$bod .= CHtml::link('Sulje', '#', array(
+		'submit'=>array('/asiakkaat/update', "suljeJuttelu"=>$data->keskustelu_id, "id"=>$data->asiakas_id), 
+		'class'=>'btn btn-danger btn-sm btn-block', 'style'=>'color:white'
+		));
+		}
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('teksti')); ?>:</b>
-	<?php echo CHtml::encode($data->teksti); ?>
-	<br />
+	  	$bod .= '</td>';
+		echo $bod;
+?>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('otsikko')); ?>:</b>
-	<?php echo CHtml::encode($data->otsikko); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
-	<?php echo CHtml::encode($data->status); ?>
-	<br />
+	<td>
+		<?php echo CHtml::link('', array('update', 'id'=>$data->id), array('class'=>'fa fa-pencil-square-o')); ?>
+	</td>
+</tr>
 
 
-</div>
