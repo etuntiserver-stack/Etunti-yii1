8be7c04390445b3c98841256f45c6c4b8ab70943
Roman <laptopsr@gmail.com>
2016-06-08 09:50:24 +0300

    Asiakashistoria päivämäärä filtterit

--- themes/customer/views/asiakkaat/update.php
+++ themes/customer/views/asiakkaat/update.php
@@ -67,8 +67,68 @@ $head = $model->osoite;
 
 
 
-	   <h2 class="myBgColors p10"> <i class="glyphicon glyphicon-user"></i> <?php echo Yii::t('main', 'Asiakas historia'); ?> </h2>
+	<h2 class="myBgColors p10"> <i class="glyphicon glyphicon-user"></i> <?php echo Yii::t('main', 'Asiakas historia'); ?> </h2>
 
+        <div class="tray-center">
+
+   	    <form id="mobForm" action="#" class="form-inline" method="POST">
+   	    <input type="hidden" name="mob_hae">
+
+            <div class="admin-form">
+              <div class="panel heading-border">
+                <div class="panel-body bg-light">
+
+                 <div class="row">
+                   <div class="col-sm-2">
+                        <div class="section">
+                          <label class="field prepend-icon">
+
+   			    <input type="text" class="gui-input datepickerFI" name="from" value="<?php if(isset($_POST['from'])) echo date('d.m.Y', strtotime($_POST['from'])); ?>" placeholder="<?php echo Yii::t('main', 'Aloitus'); ?>..">
+
+                            <label for="firstname" class="field-icon">
+                              <i class="fa fa-calendar"></i>
+                            </label>
+                          </label>
+                        </div>
+                   </div>
+
+                   <div class="col-sm-2">
+                        <div class="section">
+                          <label class="field prepend-icon">
+
+   			    <input type="text" class="gui-input datepickerFI" name="to" value="<?php if(isset($_POST['to'])) echo date('d.m.Y', strtotime($_POST['to'])); ?>" placeholder="<?php echo Yii::t('main', 'Lopetus'); ?>..">
+
+                            <label for="firstname" class="field-icon">
+                              <i class="fa fa-calendar"></i>
+                            </label>
+                          </label>
+                        </div>
+                   </div>
+
+
+                      <div class="col-md-2">
+        	        <input type="submit" class="btn btn-primary btn-lg haemob btn-block myBgColors" value="Hae">
+		      </div>
+
+                 </div>
+		
+
+                </div>
+              </div>
+
+	    </form>
+
+            </div>
+        </div>
+
+
+
+<?php
+	$from = '';
+	$to = '';
+	if(isset($_POST['from'])) $from = date("Y-m-d", strtotime($_POST['from']));
+	if(isset($_POST['to'])) $to = date("Y-m-d", strtotime($_POST['to']));
+?>
 
 	<h3><?php echo Yii::t('main', 'Työvuorot'); ?></h3>
         <div class="tray-center">
@@ -76,7 +136,7 @@ $head = $model->osoite;
               <div class="panel heading-border">
                 <div class="panel-body bg-light">
 
-		  <?php echo $this->tyovuorotCRM($model); ?>
+		  <?php echo $this->tyovuorotCRM($model, $from, $to); ?>
 
                 </div>
               </div>
@@ -89,7 +149,7 @@ $head = $model->osoite;
               <div class="panel heading-border">
                 <div class="panel-body bg-light">
 
-		  <?php echo $this->laskutuksetCRM($model); ?>
+		  <?php echo $this->laskutuksetCRM($model, $from, $to); ?>
 
                 </div>
               </div>
@@ -102,7 +162,7 @@ $head = $model->osoite;
               <div class="panel heading-border">
                 <div class="panel-body bg-light">
 
-		  <?php echo $this->tarjouksetCRM($model); ?>
+		  <?php echo $this->tarjouksetCRM($model, $from, $to); ?>
 
                 </div>
               </div>
