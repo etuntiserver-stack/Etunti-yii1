3ee83d3e6f2db219d12e399c826a9d28a035368d
Roman <laptopsr@gmail.com>
2016-08-10 08:55:38 +0300

    Työvuorot Yhtyeshenkilö näytää tyhjä bugi

--- themes/etunti/views/tyovuoroot/autoinsert.php
+++ themes/etunti/views/tyovuoroot/autoinsert.php
@@ -104,12 +104,12 @@
 				echo '<option value="">'.Yii::t('main', 'Valitse asiakas').'</option>';
 			foreach($as as $a)
 			{
-				if(!empty($a->yritys))
-				$nm = $a->yritys;
-				elseif(empty($a->yritys) and !empty($a->yhteyshenkilo))
+				if(!empty($a->yrityksen_nimi))
+				$nm = $a->yrityksen_nimi;
+				elseif(!empty($a->yhteyshenkilo))
 				$nm = $a->yhteyshenkilo;
 				else
-				$nm = Yii::t('main', 'Tyhjä');
+				$nm = Yii::t('main', 'Asiakas ID:').$a->id;
 
 				echo '<option value="'.$a->id.'">'.$nm.'</option>';
 			}
