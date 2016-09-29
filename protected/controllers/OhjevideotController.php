688a0903c3104532556654cd60141a86e7387702
Roman <laptopsr@gmail.com>
2016-09-29 09:21:00 +0300

    Ohjevideot MOV

--- protected/controllers/OhjevideotController.php
+++ protected/controllers/OhjevideotController.php
@@ -144,7 +144,12 @@ class OhjevideotController extends Controller
 	 */
 	public function actionDelete($id)
 	{
+		$mod = $this->loadModel($id);
 		$this->loadModel($id)->delete();
+		$file=Yii::getPathOfAlias('webroot').'/ohjevideot/'.$mod->tiedoston_nimi;
+
+		if(file_exists($file))
+		unlink($file);
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
