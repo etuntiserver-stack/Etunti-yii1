d329f26278ba021b02d0030851783135839074e9
Roman <laptopsr@gmail.com>
2016-10-17 12:55:57 +0300

    Asiakas postinumero ja postitoimipaikka pakolliseksi

--- protected/models/Asiakkaat.php
+++ protected/models/Asiakkaat.php
@@ -45,7 +45,7 @@ class Asiakkaat extends DB2ActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('tyyppi, osoite', 'required'),
+			array('tyyppi, osoite, postinumero, kaupunki', 'required'),
                         array('asiakasnumero','unique', 'message'=>'Tämä asiakasnumero on jo olemassa!'),
 			//array('etunimi, sukunimi, osoite, kaupunki, postinumero, puhelin, sahkoposti, ryhma, aktiivinen', 'required'),
 			array('kirjeenluokka, muistutuslasku_auto, aktiivinen, netvisorkey', 'numerical', 'integerOnly'=>true),
