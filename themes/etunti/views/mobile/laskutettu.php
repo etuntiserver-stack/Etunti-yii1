f603f6941739d8aedc21cae9145d907c2b407798
Roman <laptopsr@gmail.com>
2016-10-17 09:23:18 +0300

    Kun laitetaan täppä, niin pitäisi tulla alertti:

--- themes/etunti/views/mobile/laskutettu.php
+++ themes/etunti/views/mobile/laskutettu.php
@@ -262,9 +262,9 @@ $('.chckbxHyvaksynta').on('click', function(event, state) {
            url: location.protocol + "//" + location.host + '/index.php/mobile/laskutettu',
            type: "POST",
 	   data: { id : thisID[1], ajax : "true", las : "1", tot : tot },
-
            success: function(data){
 		console.log(data);
+		alert('Kohde merkitty laskutetuksi.');
            }
         });
       } else {
