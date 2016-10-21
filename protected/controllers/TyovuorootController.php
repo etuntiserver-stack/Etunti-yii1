9423e9a301aeac58c2e9cb4d066cf48c926ac2d9
Roman <laptopsr@gmail.com>
2016-10-21 09:06:34 +0300

    Asiakas ty;vuorokortilla valitsemalla anna Valitse kohde

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -82,6 +82,7 @@ class TyovuorootController extends Controller
 	{
 		$model = Kohteet::model()->findAll(" asiakas_id='".$id."' ");
 			$bd = '';
+			$bd .= '<option>'.Yii::t('main', 'Valitse kohde').'</option>';
 			foreach($model as $k)
 			$bd .= '<option value="'.$k->id.'">'.$k->osoite.'</option>';
 
