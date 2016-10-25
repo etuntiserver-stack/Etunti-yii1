e67c251ffab928b3362b6f51fafe919c7c6a23f8
Roman <laptopsr@gmail.com>
2016-10-25 12:35:05 +0300

    Ajankohtaista etusivulla collapsiin

--- themes/classic/views/site/_uutiset.php
+++ themes/classic/views/site/_uutiset.php
@@ -6,30 +6,30 @@
 
 <div class="row small">
  <div class="col-md-12">
- <p>
 
-
-
-     <div class="">
-
-
-	<div class="row">
+	<div class="section">
 	 <div class="col-sm-2">
 	  <?php echo CHtml::encode(date("d.m.Y", strtotime($data->time))); ?>
 	 </div>
 
 	 <div class="col-sm-8 col-sm-offset-1">
-	  <span><?php echo str_replace("\n", "<br>", $data->teksti); ?></span>
-	  <p><b><?php echo CHtml::encode($data->luoja); ?></b></p>
+	  <span class="link text-primary" data-toggle="collapse" data-target="#open_<?php echo $data->id; ?>">
+		<span class="caret"></span> <?php echo $data->otsikko; ?>
+	  </span>
+
+	  <br>
+	  <div class="collapse" id="open_<?php echo $data->id; ?>">
+	  <br>
+	  <p>
+	  	<span><?php echo str_replace("\n", "<br>", $data->teksti); ?></span>
+	  	<p><b><?php echo CHtml::encode($data->luoja); ?></b></p>
+	  </p>
+
+	  </div>
+
 	 </div>
 	</div>
 
-
-    </div>
-
-
-
- </p>
  </div>
 </div>
 
