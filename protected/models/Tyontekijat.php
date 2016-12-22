f3c61ca729b5845122eddf6b9ff1c5813b417b28
laptopsr <laptopsr@gmail.com>
2016-12-22 12:12:54 +0200

    tyontekijat kortti netvisor

--- protected/models/Tyontekijat.php
+++ protected/models/Tyontekijat.php
@@ -79,7 +79,7 @@ public $tunnus;
 		// will receive user inputs.
 		return array(
 			array('tekijan_nimi, aktiivinen', 'required'),
-			array('tekijan_pankkitili, tekijan_konttori, ammattinimike', 'netvisorCheck', 'on'=>'insert, update'),
+			array('tekijan_pankkitili, tekijan_konttori, ammattinimike, tekijan_henkilotunnus', 'netvisorCheck', 'on'=>'insert, update'),
 			array('online_varauksen_valmina, ilmoitus_merkkipaivasta_vuosi, tyontekijan_numero', 'numerical', 'integerOnly'=>true),
 			array('imei', 'length', 'max'=>100),
 			array('tyo_toimialue, laiten_puh, tekijan_nimi, tekijan_katuosoite, tekijan_pankkitili, salasana', 'length', 'max'=>100),
