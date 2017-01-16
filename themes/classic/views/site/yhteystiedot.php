670853776e5cbe5a9124b11715afd2f173d9faa9
Roman <laptopsr@gmail.com>
2017-01-16 10:13:15 +0200

    Ari etusivulle

--- themes/classic/views/site/yhteystiedot.php
+++ themes/classic/views/site/yhteystiedot.php
@@ -27,19 +27,6 @@
 <div class="text-left">
   <div class="row">
    <div class="col-md-1">
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/etusivuimg/etuntipikkulogo.jpg" class="img-thumbnail">
-   </div>
-   <div class="col-md-8">
-    <span class="small yhteysInfo">
-	<p>
-	Haemme tiimiimme uutta myyjää.
-	</p>
-    </span>
-   </div>
-  </div>
-  <br>
-  <div class="row">
-   <div class="col-md-1">
     <img src="<?php echo Yii::app()->request->baseUrl; ?>/etusivuimg/johtaja.jpg" class="img-thumbnail">
    </div>
    <div class="col-md-8">
@@ -65,6 +52,22 @@
 	Turkka Rantanen<br>
 	turkka.rantanen@etunti.fi<br>
 	+358 40 124 9082
+	</p>
+    </span>
+   </div>
+  </div>
+  <br>
+  <div class="row">
+   <div class="col-md-1">
+    <img src="<?php echo Yii::app()->request->baseUrl; ?>/etusivuimg/ari.png" class="img-thumbnail">
+   </div>
+   <div class="col-md-8">
+    <span class="small yhteysInfo">
+	<p>
+	Myyjä<br>
+	Ari Hirvimäki<br>
+	etuntimyynti@etunti.fi<br>
+	+358 40 124 9081
 	</p>
     </span>
    </div>
