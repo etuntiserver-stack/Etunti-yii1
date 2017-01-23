26e3751fb7305838487c291a2ce984c601ccaeff
Roman <laptopsr@gmail.com>
2017-01-23 14:04:38 +0200

    onlinevaraus yo kellot

--- protected/controllers/OnlinevarausController.php
+++ protected/controllers/OnlinevarausController.php
@@ -833,8 +833,10 @@ $months=array(
 		$criteria->condition = "
 			pvm='".date("d.m.Y", strtotime($date))."'
 			AND tid IN ( SELECT id FROM sivex_ttekijat WHERE online_varauksen_valmina=1 )
-			AND alku!='00:00' AND loppu!='00:00'
+			AND UNIX_TIMESTAMP(STR_TO_DATE(alku, '%H:%i')) >='".strtotime($onlinevaraus_alku.":00")."'
+			AND UNIX_TIMESTAMP(STR_TO_DATE(loppu, '%H:%i')) <='".strtotime($onlinevaraus_loppu.":00")."'
 		";
+//			AND alku!='00:00' AND loppu!='00:00'
 		if(!empty($tyo_toimialue))
 		{
 			$criteria->addCondition ("
