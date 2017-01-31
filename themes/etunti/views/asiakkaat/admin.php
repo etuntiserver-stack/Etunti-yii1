1ca59ae90ea3e9a981a7b03c8b4c6024b53bc0ff
Roman <laptopsr@gmail.com>
2017-01-31 10:26:21 +0200

    asiakkaat netvisor henkiloasiakas

--- themes/etunti/views/asiakkaat/admin.php
+++ themes/etunti/views/asiakkaat/admin.php
@@ -1,4 +1,16 @@
 <?php
+$criteria=new CDbCriteria;
+$criteria->condition=" netvisorkey!=0 AND tyyppi='henkilo' AND yhteyshenkilo!=''";
+$ask = Asiakkaat::model()->findAll($criteria);
+foreach($ask as $d)
+{
+echo $d->yhteyshenkilo.'<br>';
+//netvisorCustomer($tila, $model);
+}
+
+
+exit;
+
 /* @var $this AsiakkaatController */
 /* @var $model Asiakkaat */
 Yii::app()->clientScript->registerScript('search', "
