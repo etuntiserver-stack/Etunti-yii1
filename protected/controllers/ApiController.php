bb5122a70d8e566ef28595f09fd338842a5437fa
laptopsr <laptopsr@gmail.com>
2017-02-01 14:01:44 +0200

    APP tehty tyot

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -511,7 +511,7 @@ public function actionImei($dom)
 		    $criteria->condition = " 
 			tid = '".$ttekija->id."' 
 			AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') 
-			BETWEEN '".date("Y-m-d", strtotime("-$asetuklset->app_hyvaksytyt_tyot_vkomaara week"))."' AND '".date("Y-m-d")."' 
+			BETWEEN '".date("Y-m-d", strtotime("-$asetukset->app_hyvaksytyt_tyot_vkomaara week"))."' AND '".date("Y-m-d")."' 
 			AND loppui!=''
 		    ";
 	            $mob = Mob::model()->findAll($criteria);
@@ -523,7 +523,7 @@ public function actionImei($dom)
 		    }
 
 
-		    $sel = '<h2>'.Yii::t('app', 'Tekemasi työt. Vko määrä:').' '.(int)$asetuklset->app_hyvaksytyt_tyot_vkomaara.'</h2>';
+		    $sel = '<h2>'.Yii::t('app', 'Tekemasi työt. Vko määrä:').' '.$asetukset->app_hyvaksytyt_tyot_vkomaara.'</h2>';
 		    foreach($mob as $val)
 		    {
 		    $kesto = '00:00';
