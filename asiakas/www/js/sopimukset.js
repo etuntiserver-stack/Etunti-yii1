3e8a2373c928d19d4d5887076047d6783f5f6164
laptopsr <laptopsr@gmail.com>
2017-02-22 16:19:48 +0200

    dico

--- asiakas/www/js/sopimukset.js
+++ asiakas/www/js/sopimukset.js
@@ -61,7 +61,14 @@ function getUrlVars() {
 
 		if(d['liite'] !== '')
 		{
-			window.open("data:application/pdf;base64," + escape(d['liite'])); 
+
+			var myBase64 = escape(d['liite']);
+			var contentType = "application/pdf";
+			// if cordova.file is not available use instead :
+			// var folderpath = "file:///storage/emulated/0/";
+			var folderpath = cordova.file.externalRootDirectory;
+			var filename = "temp_etunti.pdf";
+			savebase64AsPDF(folderpath,filename,myBase64,contentType);
 
 		}
     	   },
@@ -71,6 +78,8 @@ function getUrlVars() {
         });
 
     });
+
+
 
 
     function reloadSkin()
@@ -87,6 +96,80 @@ function getUrlVars() {
 	 locale: 'fi',
         });
     }
+
+
+
+
+
+function b64toBlob(b64Data, contentType, sliceSize) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+}
+
+function savebase64AsPDF(folderpath,filename,content,contentType){
+    // Convert the base64 string in a Blob
+    var DataBlob = b64toBlob(content,contentType);
+    
+    console.log("Starting to write the file :3");
+    
+    window.resolveLocalFileSystemURL(folderpath, function(dir) {
+        console.log("Access to the directory granted succesfully");
+		dir.getFile(filename, {create:true}, function(file) {
+            console.log("File created succesfully.");
+            file.createWriter(function(fileWriter) {
+                console.log("Writing content to file");
+                fileWriter.write(DataBlob);
+
+		//alert(folderpath + filename)
+        	//window.open(folderpath + filename, '_blank');
+
+
+
+		cordova.plugins.fileOpener2.open(
+		    '/sdcard/temp_etunti.pdf',
+		    'application/pdf', 
+		    { 
+		        error : function(e) { 
+		            console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+		        },
+		        success : function () {
+		            console.log('file opened successfully');                
+		        }
+		    }
+		);
+
+
+
+            }, function(){
+                alert('Unable to save file in path '+ folderpath);
+            });
+		});
+    });
+}
+
+
+
+
+
 
 
 
