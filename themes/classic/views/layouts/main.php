243b272ebd6c91e25b0f5066a46d09598b2e883b
laptopsr <laptopsr@gmail.com>
2017-03-10 10:57:29 +0200

    htaccess mozilla redirect

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -1,3 +1,11 @@
+<?php
+   if ((isset($_SERVER['HTTPS']) and strlen(strstr($agent, 'Firefox')) > 0)) 
+   {
+    $redirect_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $redirect_url");
+    exit();
+   }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -15,16 +23,9 @@
         <meta name="keywords" content="HTML5 Template" />
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<?php
-   if ((isset($_SERVER['HTTPS']) and strlen(strstr($agent, 'Firefox')) > 0)) 
-   {
-    $redirect_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect_url");
-    exit();
-   }
 
 
-
+<?
     if (isset($_SERVER['HTTP_USER_AGENT'])) {
     	$agent = $_SERVER['HTTP_USER_AGENT'];
     }
