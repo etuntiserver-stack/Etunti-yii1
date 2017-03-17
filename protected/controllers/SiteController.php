03dbf6051f5676498c0051ad4d7e1b188e3b0abe
Roman <laptopsr@gmail.com>
2017-03-17 12:17:16 +0200

    kaikki sahkopostit template muuttunut

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -41,7 +41,7 @@ class SiteController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', 
-				'actions'=>array('etusivu','ohjesivu','etusivu_esimerki', 'change_color', 'valiko', 'valiko_ajax', 'kohderyhma', 'ohjevideot', 'mobemu', 'etusivu_ajax', 'ulkonaky', 'autocomplete', 'synkronoi_gps_sijainti'),
+				'actions'=>array('etusivu','ohjesivu','etusivu_esimerki', 'change_color', 'valiko', 'valiko_ajax', 'kohderyhma', 'ohjevideot', 'mobemu', 'etusivu_ajax', 'ulkonaky', 'autocomplete', 'synkronoi_gps_sijainti', 'mail_template'),
                 		'expression'=>"Yii::app()->controller->isEtuntiAdmin()",
 			),
 			array('allow', 
@@ -136,6 +136,12 @@ class SiteController extends Controller
 		}
 
 		echo urldecode($count);
+	}
+
+
+	public function actionMail_template()
+	{
+		$this->renderPartial('mail_template');
 	}
 
 	public function actionUlkonaky()
--- themes/etunti/views/site/mail_template.php
+++ themes/etunti/views/site/mail_template.php
@@ -307,7 +307,7 @@
     <td align="center" valign="top" width="100%" style="background-color: #ffffff;  border-top: 1px solid #e5e5e5; border-bottom: 1px solid #e5e5e5;">
       <center>
       <br>
-      <p><?= $content ?></p>
+      <p><?php //$content ?>blaa</p>
       <br>
       </center>
     </td>
@@ -331,45 +331,3 @@
 </div>
 </body>
 </html>
-<?php /*
-<html>
-<head>
-	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
-</head>
-<body>
-<table cellspacing="0" cellpadding="10" style="color:#666;font:13px Arial;line-height:1.4em;width:100%;">
-	<tbody>
-		<tr>
-            <td style="color:#4D90FE;font-size:22px;border-bottom: 2px solid #4D90FE;">
-	    <?php
-		if(isset(Yii::app()->user->domain))
-		{
-			$d = Domainit::model()->find("domain='".Yii::app()->user->domain."'");
-			if(isset($d->yritys))
-			echo $d->yritys;
-		} else {
-			echo 'ETUNTI.FI';
-		}
-	    ?>gg
-            </td>
-		</tr>
-		<tr>
-            <td style="color:#777;font-size:16px;padding-top:5px;">
-            	<?php if(isset($data['description'])) echo $data['description'];  ?>
-            </td>
-		</tr>
-		<tr>
-            <td>
-				<?php echo $content ?>
-            </td>
-		</tr>
-		<tr>
-            <td style="padding:15px 20px;text-align:right;padding-top:5px;border-top:solid 1px #dfdfdf">
-				https://etunti.fi
-			</td>
-		</tr>
-	</tbody>
-</table>
-</body>
-</html>
-*/ ?>
