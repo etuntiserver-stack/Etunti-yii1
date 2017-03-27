dd008c87986cb15ae84bc554f7c62c8747fed4c8
laptopsr <laptopsr@gmail.com>
2017-03-27 12:39:02 +0300

    log on valmis luultavasti

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if( $_SERVER['REMOTE_ADDR'] == '::1' or $_SERVER['REMOTE_ADDR'] == '127.0.0.1' o
 {
 	defined('YII_DEBUG') or define('YII_DEBUG',true);
 } else {
-	defined('YII_DEBUG') or define('YII_DEBUG',false);
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
 }
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
