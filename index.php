30df9206a5919585277c61bd1a252258a865afa2
Roman <laptopsr@gmail.com>
2017-03-28 14:59:44 +0300

    app

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $yii=dirname(__FILE__).'/YII/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
+/*
 if( 
 	($_SERVER['REMOTE_ADDR'] == '::1' 
 	or $_SERVER['REMOTE_ADDR'] == '127.0.0.1' 
@@ -17,6 +18,9 @@ if(
 } else {
 	defined('YII_DEBUG') or define('YII_DEBUG',false);
 }
+*/
+
+	defined('YII_DEBUG') or define('YII_DEBUG',false);
 
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
