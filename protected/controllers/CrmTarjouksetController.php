8e51e3f77d0270c8ee94aaf26fbec9ae4dd2f928
Roman <laptopsr@gmail.com>
2017-04-11 10:57:15 +0300

    pdf ongelma

--- protected/controllers/CrmTarjouksetController.php
+++ protected/controllers/CrmTarjouksetController.php
@@ -568,7 +568,7 @@ class CrmTarjouksetController extends Controller
 //($path.'.docx', $path.'.pdf');
 
 			//shell_exec('unoconv -f pdf '.$path.'.docx');
-shell_exec('soffice --convert-to pdf '.$path.'.docx --headless');
+shell_exec('soffice --headless --convert-to pdf '.$path.'.docx');
 			$this->redirect(array('index'));
 
 	}
