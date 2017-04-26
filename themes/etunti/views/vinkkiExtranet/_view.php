8cd77c3ae1b171e4ab1e188d0f8b1e9fc5d7ed54
laptopsr <laptopsr@gmail.com>
2017-04-26 09:03:25 +0300

    valmis

--- themes/etunti/views/vinkkiExtranet/_view.php
+++ themes/etunti/views/vinkkiExtranet/_view.php
@@ -18,6 +18,19 @@
 
 <tr>
 	<td>
+		<?php echo CHtml::link('<i class="fa fa-pencil-square-o" aria-hidden="true" style="font-size: 110%"></i>', 
+				array('update', 'id'=>$data->id), 
+				array(
+					'class'=>'btn btn-primary myBgColors', 
+					'style'=>'color:white', 
+					'data-toggle'=>'tooltip', 
+					'data-placement'=>'top', 
+					'title'=>Yii::t('main', 'Muokkaa') 
+				)
+			); 
+		?>
+	</td>
+	<td>
 		<?php echo date("d.m.Y H:i", strtotime($data->time)); ?>
 	</td>
 	<td>
@@ -37,9 +50,6 @@
 	</td>
 	<td>
 		<?php echo $this->tilaMuutos($data->tila); ?>
-	</td>
-	<td>
-		<?php echo CHtml::link('', array('update', 'id'=>$data->id), array('class'=>'fa fa-pencil-square-o')); ?>
 	</td>
 </tr>
 
