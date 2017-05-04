457f36447510c55563d5944be9d0fba9a790449f
Elias Luoma <elias.luoma@santelo.fi>
2017-05-04 13:46:10 +0300

    Lisätty excec

--- themes/classic/views/site/aloita.php
+++ themes/classic/views/site/aloita.php
@@ -15,12 +15,19 @@
 
 <div class="container">
 
+<?php
+
+echo exec('ls -la');
+
+?> 
+  
+
 <div class="row">
  <div class="col-sm-6 col-sm-offset-3">
   <div class="panel panel-default">
     <div class="panel-heading"><?php echo Yii::t('main', 'Aloita Etunnin käyttäminen'); ?></div>
     <div class="panel-body">
-
+      <form action="#" method="POST">
         <label><?php echo Yii::t('main', 'Yrityksen nimi')?></label>
         <input type="text" id="yrityksen_nimi" name="yrityksen_nimi" class="form-control input-lg" required autofocus>
         <label><?php echo Yii::t('main', 'Yritystunnus')?></label>
@@ -36,7 +43,7 @@
         
 	<br>
         <button class="btn btn-lg btn-primary btn-group submit" type="submit"><?php echo Yii::t('main', 'Aloita'); ?></button>
-    
+    </form>
 	<div id="success"></div>
 
     </div>
@@ -46,71 +53,5 @@
 
 
 </div>
-<br><br>
 
-
-<script type="text/javascript">
-$(document).ready(function(){
-
-  $(".submit").click(function(){
-
-	$('#success').html('');
-
-	$.ajax({
-	  url: 'change_password',
-	  data: { vanha_salasana: $('#vanha_salasana').val(), uusi_salasana: $('#uusi_salasana').val(), varmista_uusi_salasana: $('#varmista_uusi_salasana').val() },
-	  type:'POST',
-	  success:function(data){
-
-		try
-		{
-		   data = JSON.parse(data);
-	  	   console.log(data);
-		}
-		catch(e)
-		{
-		   $('#success').html('<br><span class="alert alert-danger btn-block">Tarkista tiedot</span>');
-		   return false;
-		}
-
-
-		if(data == 'InvalidPassword')
-		{
-			$('#success').html('<br><span class="alert alert-danger btn-block">Vanha salasana on erilainen</span>');
-		}
-		if(data == 'varmistaUusi')
-		{
-			$('#success').html('<br><span class="alert alert-danger btn-block">Uusi salasana on erilainen</span>');
-		}
-		if(data == 'emptyUusi')
-		{
-			$('#success').html('<br><span class="alert alert-warning btn-block">Uusi salasana on tyhjä</span>');
-		}
-		if(data == 'error')
-		{
-			$('#success').html('<br><span class="alert alert-danger btn-block">Käyttäjätunnus ei löydy</span>');
-		}
-		if(data[0] == 'ok')
-		{
-			window.location.href="etusivu";
-			$('input').val('');
-			$('#success').html('<br><span class="alert alert-success btn-block">Salasanasi vaihdettu. Kirjaudu ulos ja käytä uusi salasana.</span>');
-			$('.submit').remove();
-		}
-
-		//$("#salasanaLomake").submit();
-	  },
-	  error:function(data){
-  		console.log(data); 
-	  }
-  	});
-
-	return false;
-  });
-
-
-
-
-});
-</script>
 
