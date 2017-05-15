bd7db50cfa84b771adc97ec99c970504d7f378c2
laptopsr <laptopsr@gmail.com>
2017-05-15 18:49:09 +0300

    Kasvokuvat isommaksi

--- themes/etunti/views/tyovuoroot/index.php
+++ themes/etunti/views/tyovuoroot/index.php
@@ -173,7 +173,7 @@
 		echo '
 		<div class="row">
 		  <div class="col-sm-12">
-		    	<a href="#" class="getTekijanTiedot" for="'.$t->id.'"><img src="'.$img.'" alt="avatar" class="mw30 br64 mr15"><br> '.$this->etuSukunimi($t->id).'</a>
+		    	<a href="#" class="getTekijanTiedot" for="'.$t->id.'"><img src="'.$img.'" alt="avatar" class="mw50 br64 mr15"><br> '.$this->etuSukunimi($t->id).'</a>
 			<br>
 			<span '.$cl.'><b id="vk_'.$week.'_'.$t->id.'">'.$kokoViikko. '</b> ('.$vktyoaika.')</span>
 		  </div>
