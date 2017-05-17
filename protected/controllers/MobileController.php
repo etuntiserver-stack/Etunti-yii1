2587a2f39e030cf24cbd858e232b6ed8cc6236f0
laptopsr <laptopsr@gmail.com>
2017-05-17 11:09:01 +0300

    kokeilu nro 1

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -208,7 +208,7 @@ function num($val){
 			        $content = $this->renderPartial('raportit_pdf_l', array('model' => $model, 'tyyppi' => 'Luetut'),true);
 				//preg_match_all('/<div class=\"tb\">(.*?)<\/div>/s',$html,$match);
 				//$this->htmlToXls($match[0][0], 'luetut');
-				$this->transformContentTo($content, 'xls');
+				$this->transformContentTo($content, 'xlsx');
 			        exit;
 			}
 
