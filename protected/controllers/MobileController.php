bf105ba58bfe1a0616284863c6e74c5985e2343e
laptopsr <laptopsr@gmail.com>
2017-05-17 11:23:32 +0300

    kokeilu nro 1

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -263,10 +263,15 @@ function num($val){
 
 			if(isset($_POST['luoPDF']))
 			{
+				/*
 			        $html2pdf = Yii::app()->ePdf->HTML2PDF('L', 'A4', 'en', 'true', 'UTF-8', array(3,10,5,10));
 				$html2pdf->setDefaultFont('Arial');
 			        $html2pdf->WriteHTML($this->renderPartial('raportit_pdf_l', array('model' => $model, 'tyyppi' => 'Hyväksytyt'),true));
 			        $html2pdf->Output();
+				*/
+				$content = '<link rel="stylesheet" type="text/css" href="../../css/raportit_table2.css">';
+				$content .= $this->renderPartial('raportit_pdf_l', array('model' => $model, 'tyyppi' => 'Hyväksytyt'), true);
+				$this->transformContentTo($content, 'pdf');
 			        exit;
 			}
 
