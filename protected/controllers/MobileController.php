266e6b01d372f3e4e7a57698fa347ff741e1de0c
laptopsr <laptopsr@gmail.com>
2017-05-24 15:58:16 +0300

    raportit taulu PDF

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -109,7 +109,7 @@ class MobileController extends Controller
 
 			$c = '
 			<html>
-			<link rel="stylesheet" type="text/css" href="../css/raportit_table.css">
+			<link rel="stylesheet" type="text/css" href="../css/raportit_table2.css">
 			';
 			$c .= $content;
 			$c .= '</html>';
