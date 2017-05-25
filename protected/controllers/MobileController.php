ca5807db737cab1304b2f468edadb99d136cd638
laptopsr <laptopsr@gmail.com>
2017-05-25 16:42:34 +0300

    raportit taulu PDF

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -86,7 +86,7 @@ class MobileController extends Controller
 	{
 		if(isset($_POST['content']) and isset($_POST['ext']))
 		{
-			$this->transformHtmlTo('<b>dfdsfsdf</b>', $_POST['ext']);
+			$this->transformHtmlTo($_POST['content'], $_POST['ext']);
 			exit;
 		}
 		echo json_encode('false');
