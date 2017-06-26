58b9431524dce1860d7bf92c9b8c4fcba8ca39b8
Roman Sizov <laptopsr@gmail.com>
2017-06-26 09:07:55 +0300

    Tarjoukset lisää työnkuvaus sarake

--- themes/etunti/views/crmTarjoukset/index.php
+++ themes/etunti/views/crmTarjoukset/index.php
@@ -68,16 +68,19 @@
 				';
 */
 
+			 	echo '<tr><td>b';
+
 		// <-- file_safe_opener
 		$filepath = '../../'.$file;
 		echo CHtml::link(end($explNimi),
 			array('/site/file_safe_opener', 'filepath' => $filepath, 'ext' => 'docx'),
 			array(
 				'target'=>'_blank',
-				'class'=>'text-danger poista',
-				'for' => Yii::app()->baseUrl.$this->templates_polkku().end($explNimi)
+				'class'=>'text-danger'
 		));
 		//     file_safe_opener// -->
+			 	echo '</td><i class="poista text-danger fa fa-trash link" for="'.Yii::app()->baseUrl.$this->templates_polkku().end($explNimi).'"></i><td>';
+			 	echo '</tr>';
 
 			  }
 			  ?>
