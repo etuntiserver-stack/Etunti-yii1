e9977b19e580ca61b23003649896dfc356e9dedc
Roman Sizov <laptopsr@gmail.com>
2017-06-29 08:56:03 +0300

    noreply

--- protected/controllers/DicoController.php
+++ protected/controllers/DicoController.php
@@ -182,7 +182,7 @@ public function actionLogin($domain)
 
 							$subject='=?UTF-8?B?'.base64_encode("Työvuoro peruutettu").'?=';
 							$headers="From: ".$asiakas." <".$model->sahkoposti.">\r\n".
-								"Reply-To: no_replay@etunti.fi\r\n".
+								"Reply-To: noreply@etunti.fi\r\n".
 								"MIME-Version: 1.0\r\n".
 								"Content-type: text/html; charset=UTF-8";
 
@@ -275,7 +275,7 @@ public function actionLogin($domain)
 
 							$subject='=?UTF-8?B?'.base64_encode($asiakas ." suosittele").'?=';
 							$headers="From: ".$asiakas." <".$model->sahkoposti.">\r\n".
-								"Reply-To: no_replay@etunti.fi\r\n".
+								"Reply-To: noreply@etunti.fi\r\n".
 								"MIME-Version: 1.0\r\n".
 								"Content-type: text/html; charset=UTF-8";
 
