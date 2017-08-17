bafbe80ce8c777aa47c278743afc781e78650f24
Roman Sizov <laptopsr@gmail.com>
2017-08-17 22:14:20 +0300

    nopea korjaus

--- themes/classic/views/site/crontab.php
+++ themes/classic/views/site/crontab.php
@@ -23,7 +23,7 @@
         if( $_SERVER['REMOTE_ADDR'] == '::1' or $_SERVER['REMOTE_ADDR'] == '127.0.0.1' )
         {
       	    Yii::app()->db1->username = 'root';
-            Yii::app()->db1->password = '';
+            Yii::app()->db1->password = '111111';
     	} else {
       	    Yii::app()->db1->username = 'root';
             Yii::app()->db1->password = 'Etunti2017!';
