e91684279fee600aa3aa09aefc8fafdb557dcc4e
Roman Sizov <laptopsr@gmail.com>
2017-08-17 22:03:15 +0300

    nopea korjaus

--- themes/etunti/views/tyovuoroot/viikko.php
+++ themes/etunti/views/tyovuoroot/viikko.php
@@ -4,14 +4,13 @@
 	$eilasketa = $site[0]->eiLasketa();
 
        	$criteria = new CDbCriteria();
-        $criteria->select = "
-	SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppu, '%H:%i'), '%H:%i'), DATE_FORMAT(STR_TO_DATE(alku, '%H:%i'), '%H:%i')))) as l_tunnit";
-
+        $criteria->select = "SUM(TIME_TO_SEC(TIMEDIFF(loppu, alku))) as l_tunnit";
+//	SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppu, '%H:%i'), '%H:%i'), DATE_FORMAT(STR_TO_DATE(alku, '%H:%i'), '%H:%i')))) as l_tunnit";
         $criteria->condition = " 
 		tid = '".$tid."'  
 		AND $eilasketa
 		AND DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%u') = '".$viikko."'
-		AND DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y') = '$year'
+		AND DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y') = '".$year."'
 	";
 
 	$tv = Tyovuoroot::model()->find($criteria); 
