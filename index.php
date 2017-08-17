ef542dc47a6e3fc8d514bfa4849dc9cca6137c48
Roman Sizov <laptopsr@gmail.com>
2017-08-17 11:13:50 +0300

    http refer

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 if( 
 	($_SERVER['REMOTE_ADDR'] == '::1' 
 	or $_SERVER['REMOTE_ADDR'] == '127.0.0.1' 
-	or strpos($_SERVER['HTTP_REFERER'], "dev") !== false
 	)
 	and (isset($_SERVER['HTTP_REFERER']) and strpos($_SERVER['HTTP_REFERER'], "api/mob") === false )
 )
