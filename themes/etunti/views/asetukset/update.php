fbbce4558f57930d2b8a8d0187d1a2d062befeac
Roman Sizov <laptopsr@gmail.com>
2017-08-25 11:44:40 +0300

    Pliiz megrata kaikki branchia, ei ole mukava tyoskennella. Nyt lisäsin tuohon branchit toinen tehtävä, koska olisi virhe samalla tiedostolla

--- themes/etunti/views/asetukset/update.php
+++ themes/etunti/views/asetukset/update.php
@@ -61,6 +61,27 @@ if(isset($_POST['uploaded_toimitusehdot']))
   } 
 }
 
+if(isset($_POST['uploaded_Konevuokraus_toimitusehdot']))
+{
+
+  if (!file_exists(Yii::app()->basePath."/../tiedostot/firma/".Yii::app()->user->domain)) {
+  	mkdir(Yii::app()->basePath."/../tiedostot/firma/".Yii::app()->user->domain, 0777, true);
+  }
+
+  $uploaddir = Yii::app()->basePath.'/../tiedostot/firma/'.Yii::app()->user->domain.'/';
+  $temp = explode(".", $_FILES["file"]["name"]);
+  if(end($temp) == 'pdf')
+  {
+	$uploadfile = $uploaddir . basename('Konevuokraus_toimitusehdot.'.end($temp));
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile))
+	$this->redirect(array('update', 'id'=>1));
+
+  } else {
+
+	Yii::app()->user->setFlash('danger', "Lataaminen ei onnistunut, odottelaan PDF");
+	$this->redirect(array('update', 'id'=>1));
+  }
+}
 
 
 
@@ -253,6 +274,24 @@ exit;
          <span class="button"><?php echo Yii::t('main', 'Toimitusehdot'); ?></span>
          <input type="file" class="gui-file" name="file" id="t_file" onChange="document.getElementById('tiedostoUP').value = this.value;">
          <input type="text" class="gui-input" name="uploaded_toimitusehdot" id="tiedostoUP" placeholder="Valitse tiedosto..">
+         <label class="field-icon">
+          <i class="fa fa-upload"></i>
+         </label>
+       </label>
+	<span class="input-group-btn">
+          <input type="submit" value="Lataa" class="btn btn-primary btn-group myBgColors" />
+	</span>
+    </div>
+  </form>
+ </div>
+
+ <div class="admin-form col-sm-6">
+  <form id="uploadimage" action="#" class="form-input" method="post" enctype="multipart/form-data">
+     <div class="section input-group">
+       <label class="field prepend-icon append-button file">
+         <span class="button"><?php echo Yii::t('main', 'Konevuokraus toimitusehdot'); ?></span>
+         <input type="file" class="gui-file" name="file" id="t_file" onChange="document.getElementById('tiedostoUP').value = this.value;">
+         <input type="text" class="gui-input" name="uploaded_Konevuokraus_toimitusehdot" id="tiedostoUP" placeholder="Valitse tiedosto..">
          <label class="field-icon">
           <i class="fa fa-upload"></i>
          </label>
