b7f09abc05682aa4992d16e4dc36773e89f1493f
Roman Sizov <laptopsr@gmail.com>
2017-09-05 14:53:53 +0300

    hallinta pois

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -986,10 +986,12 @@ $('.tvchange').change(function(){
               <span class="caret"></span>
             </a>
             <ul class="nav sub-nav">
+<!--
               <li>
                 <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/tyovuoroot/hallinta">
                   <span class="fa fa-clock-o"></span> <?php echo Yii::t('main', 'Hallinta'); ?></a>
               </li>
+-->
               <li>
                 <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/tyovuoroot/index">
                   <span class="fa fa-clock-o"></span> <?php echo Yii::t('main', 'Työvuorot'); ?></a>
