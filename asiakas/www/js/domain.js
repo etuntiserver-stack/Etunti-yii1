6ae23b033aab9fd70b0f41af380acec446c72e73
Roman Sizov <laptopsr@gmail.com>
2017-09-06 12:20:28 +0300

    edico footer domain.jsilla

--- asiakas/www/js/domain.js
+++ asiakas/www/js/domain.js
@@ -172,5 +172,23 @@ $("body").ready(function(){
 
 
 
+   $("#eFooter").replaceWith(''+
+    '<div class="nav navbar-fixed-bottom navbar-shadow myBgColors">' +
+     '<div class="container" id="footer-body">' +
+      '<center>' +
+	
+	'<div class="row">' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="index.html"><h2 class="fa fa-home"></h2></a></li>' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="historia.html?tyyppi=naytaTyovuorot"><h2 class="fa fa-calendar"></h2></a></li>' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="historia.html?tyyppi=naytaToteutuneetTunnit"><h2 class="fa fa-check-square"></h2></a></li>' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="historia.html?tyyppi=naytaVinkit"><h2 class="fa fa-thumbs-o-up"></h2></a></li>' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="historia.html?tyyppi=naytaPalautteet"><h2 class="fa fa-smile-o"></h2></a></li>' +
+	   '<li class="col-xs-2"><a class="myBgColors" href="index.html?sivu=asiakirjat"><h2 class="fa fa-file-text-o"></h2></a></li>' +
+	'</div>' +
+
+      '</center>' +
+     '</div>' +
+    '</div>'
+   );
 
 });
