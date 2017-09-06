ff370a0beb73d52371d267cc56fc7fe6664fd6e0
Roman Sizov <laptopsr@gmail.com>
2017-09-06 11:27:01 +0300

    EDico vinkit sähköposti

--- protected/controllers/DicoController.php
+++ protected/controllers/DicoController.php
@@ -373,7 +373,7 @@ public function actionLogin($domain)
 
 							$subject='=?UTF-8?B?'.base64_encode($asiakas ." suosittele").'?=';
 							$headers="From: ".$asiakas." <".$model->sahkoposti.">\r\n".
-								"Reply-To: noreply@etunti.fi\r\n".
+								"Reply-To: no-reply@etunti.fi\r\n".
 								"MIME-Version: 1.0\r\n".
 								"Content-type: text/html; charset=UTF-8";
 
