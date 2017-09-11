30d990a9b992712ef3bab54bc1e01c961ffbfdc5
Roman Sizov <laptopsr@gmail.com>
2017-09-11 09:24:24 +0300

    xls dev palvelimella

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -190,9 +190,8 @@ class MobileController extends Controller
 			exec('pandoc -s '.$path.$tiedosto.'.html -o '.$path.$tiedosto.'.xls', $output, $return);
 		        if (file_exists( Yii::app()->basePath.'/../tiedostot/temp/'.Yii::app()->user->domain.'/'.$tiedosto.'.xls' ))
 			{
-		                $content = file_get_contents($path.$tiedosto.'.xls');
-				Yii::app()->request->sendFile("excel.xls",$content);
-				exit;//
+				echo $c;
+				exit;
 			}
 		}
 
