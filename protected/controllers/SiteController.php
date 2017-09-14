e39e993c7254ee0fc95e3cb4184447017d1d73cb
Roman Sizov <laptopsr@gmail.com>
2017-09-14 14:16:20 +0300

    viestit vilkkuminen

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -1590,7 +1590,7 @@ $(document).ready(function(){
 			status=0 AND tekija='toimisto'
 		";
 	  	$v = Viestinta::model()->findAll($criteria);
-		  $viestit = 0;
+		  $viestit = '0';
 		if(count($v) > 0)
 		  $viestit = (int)$v->count;
 
