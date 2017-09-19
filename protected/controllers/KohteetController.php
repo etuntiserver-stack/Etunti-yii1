2ec0ce0635c0802c826aebcd15409d0725a3f917
Roman Sizov <laptopsr@gmail.com>
2017-09-19 19:08:51 +0300

    veikon sotto kohde vie etusivulle / ei toimi

--- protected/controllers/KohteetController.php
+++ protected/controllers/KohteetController.php
@@ -328,6 +328,7 @@ class KohteetController extends Controller
 	        $latAuto = '';
 	        $lngAuto = '';
 	    	$coordinates = $this->getlatlong($model->osoite);
+exit;
 		if($coordinates and isset($coordinates->results[0]->geometry->location->lat))
 	        $latAuto = $coordinates->results[0]->geometry->location->lat.',';
 		if($coordinates and isset($coordinates->results[0]->geometry->location->lng))
@@ -371,7 +372,6 @@ class KohteetController extends Controller
 		}
 		catch(Exception $e){
 			throw new Exception ('Error ' .$e);
-			exit;
 			return false;
 		}
 
