ce81c372861bb42f742b50ef4c03fc35fbf304cc
Roman Sizov <laptopsr@gmail.com>
2017-09-28 14:12:26 +0300

    log haku tilanteet väriksi

--- protected/messages/fi/log.php
+++ protected/messages/fi/log.php
@@ -6,6 +6,8 @@ return array(
 'Create' => '<span class="btn btn-block btn-success">Luonti</span>',
 'Update' => '<span class="btn btn-block btn-warning">Muokkaus</span>',
 'Delete' => '<span class="btn btn-block btn-danger">Poisto</span>',
+'Move' => '<span class="btn btn-block btn-success">Siirtö</span>',
+'Copy' => '<span class="btn btn-block btn-success">Kopio</span>',
 );
 
 
