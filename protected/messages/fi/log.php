175cdede485e02fcbae42176d433e67638a5014e
Roman Sizov <laptopsr@gmail.com>
2017-09-29 08:23:36 +0300

    loki

--- protected/messages/fi/log.php
+++ protected/messages/fi/log.php
@@ -8,7 +8,8 @@ return array(
 'Delete' => '<span class="btn btn-block btn-danger">Poisto</span>',
 'Move' => '<span class="btn btn-block btn-success">Siirtö</span>',
 'Copy' => '<span class="btn btn-block btn-success">Kopio</span>',
-'Create from APP' => '<span class="btn btn-block btn-success fa fa-mobile"> Luonti</span>',
+'Create from APP' => '<span class="btn btn-block btn-success">Luonti APP</span>',
+'Update by APP' => '<span class="btn btn-block btn-warning">Muokkaus APP</span>',
 );
 
 
