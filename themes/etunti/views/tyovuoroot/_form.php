db270016797adf566b36ad827a560d7a8e0d7278
Roman Sizov <laptopsr@gmail.com>
2017-10-12 13:04:58 +0300

    kolmas versio test

--- themes/etunti/views/tyovuoroot/_form.php
+++ themes/etunti/views/tyovuoroot/_form.php
@@ -683,6 +683,16 @@ $('.mult').multiselect({
  		return false;
  	}
 
+	/* <-- Tarkistetaan Alkaen pvm */
+	var thisVal = $('#pfrom').val().split('.');
+	var inputDate = new Date(+thisVal[1]+"/"+thisVal[0]+"/"+thisVal[2]); //"11/21/2011"
+	var todaysDate = new Date();
+	if(inputDate.setHours(0,0,0,0) < todaysDate.setHours(0,0,0,0)) {
+		alert('Toistuvan työvuoron aloitus päivämäärä ei voida muokata alkamaan menneisyydestä.');
+		return false;
+	}
+	/*    Tarkistetaan Alkaen pvm --> */
+
 	if( ($('#submitButton').val() === 'Luo') || ($('#submitButton').val() === 'Tallenna') ) 
 		$('#submitButton').hide();
 
@@ -1128,14 +1138,6 @@ function laatikonPaivays(thisDataReturn){
 	laskePituus();
   });
 
-  $('#pfrom').blur(function(){
-	var thisVal = $(this).val().split('.');
-	var inputDate = new Date(+thisVal[1]+"/"+thisVal[0]+"/"+thisVal[2]); //"11/21/2011"
-	var todaysDate = new Date();
-	if(inputDate.setHours(0,0,0,0) < todaysDate.setHours(0,0,0,0)) {
-		alert('Toistuvan työvuoron aloitus päivämäärä ei voida muokata alkamaan menneisyydestä.');
-	}
-  });
   
   if( $('#Tyovuoroot_kohde').val() !== '' ){
 	var thisID = $('#Tyovuoroot_kohde option:selected').val();
