0bc4fb62dbfe49cdb8feb64c1eb0ec9ff9200d67
Roman Sizov <laptopsr@gmail.com>
2017-10-17 14:11:46 +0300

    sopimus

--- protected/controllers/CrmSopimuksetController.php
+++ protected/controllers/CrmSopimuksetController.php
@@ -323,8 +323,7 @@ $randstring = generateRandomString();
   					$tiedosto = $site[0]->tiedostonNimiAsiakasKohdeAika($tiedosto, $model->asiakas_id, $model->tarjous->kohteen_osoite, date('Y-m-d'));
 				}
 				//     Tiedoston nimi -->
-echo $tiedosto;
-exit;
+
 
 				CrmSopimukset::model()->updateByPk($model->id, array('liite'=>$tiedosto));
 
