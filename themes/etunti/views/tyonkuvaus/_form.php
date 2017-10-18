86af921fc6f36a5e096ca25c51315a72da9f46f0
Roman Sizov <laptopsr@gmail.com>
2017-10-18 11:23:55 +0300

    Tyonkuvaukset plussa merkki alas

--- themes/etunti/views/tyonkuvaus/_form.php
+++ themes/etunti/views/tyonkuvaus/_form.php
@@ -198,8 +198,6 @@ $(document).ready(function(){
 	?>
 
 
-	      <a href="#" title="" class="add-author"><i class="fa fa-plus" aria-hidden="true"></i></a>
-
 		<table class="table table-bordered authors-list">
 		    <tr>
 		        <th></td><td><?php echo Yii::t('main','Tilat'); ?></th>
@@ -388,8 +386,8 @@ $(document).ready(function(){
 		    </tr>
 		<?php endif; ?>
 		</table>
-
-
+		<br>
+	      	<p style="margin-left: 7px"><a href="#" title="" class="add-author"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a></p>
 
 		<div class="input-group" id="kuvauksetTuoteesta" style="display:none">
 		 <?php echo CHtml::dropDownList("", "", $tal, array("multiple" => "multiple", "class"=> "form-control mult tyonkuvaus_tilat_selecter")); ?>
