264c796f06e53fb9ceb539e9d2401d331fcc72a1
Roman Sizov <laptopsr@gmail.com>
2017-10-19 13:09:33 +0300

    poisto valmis kai

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -708,22 +708,11 @@ class TyovuorootController extends Controller
 			";
 			$m = Tyovuoroot::model()->findAll($poistoCriteria);
 
-
-			$toistuva = ToistuvatTyovuorot::model()->findByPk($model->toistuva_id);
-			$edelliset_tvuoro_arr = json_decode($toistuva->tvuoro_ids, true);
-
-			$edelliset_tvuoro_ids = array();
-			if(is_array($edelliset_tvuoro_arr))
-			$edelliset_tvuoro_ids = $edelliset_tvuoro_arr;
-
 			
 			foreach($m as $model)
 			{
-				$ketjustaPois = $model->id;
-				$edelliset_tvuoro_ids = array_values( array_diff($edelliset_tvuoro_ids, array($ketjustaPois)) );
 
 				$return[] = array('tid'=>$model->tid, 'pvm'=>$model->pvm, 'ymd'=>date("Ymd",strtotime($model->pvm)));
-
 				$this->toistuvaDeletePvm($model->toistuva_id, $model->pvm);
 
 				// <-- LOG
@@ -780,11 +769,7 @@ class TyovuorootController extends Controller
 
 			} else {
 
-				ToistuvatTyovuorot::model()->updateByPk($model->toistuva_id, array(
-					'tvuoro_ids'=>json_encode($edelliset_tvuoro_ids),
-					//'pfrom'=>date("d.m.Y", strtotime($_POST['pto'].' +1 day')),
-				));
-
+				// Keksi loogikka
 			}
 
 
@@ -1370,30 +1355,6 @@ class TyovuorootController extends Controller
 			// jos on tyopaari -->
 
 
-
-			// <-- tvuoro_ids Updater
-			if( $saankoSuoritta == 1 and count($return) > 0 )
-			{
-
-				$tvuoro_ids	= array();
-				foreach($return as $k=>$item)
-				{
-					foreach($item as $item2)
-					{
-						if(isset($item2['tvuoro_id']))
-							$tvuoro_ids[] = $item2['tvuoro_id'];
-					}
-				}
-
-				if( count($tvuoro_ids) > 0 )
-				{
-					ToistuvatTyovuorot::model()->updatebypk($toistuva->id, array('tvuoro_ids' => json_encode($tvuoro_ids)));
-				}
-
-			}
-			//  tvuoro_ids Updater -->
-
-
 			// <-- LOG
 			if( $saankoSuoritta == 1 )
 			{
@@ -1731,24 +1692,6 @@ class TyovuorootController extends Controller
 			}
 
 
-			// <-- tvuoro_ids Updater
-			if( $saankoSuoritta == 1 and count($return) > 0 )
-			{
-
-				$tvuoro_ids	= array();
-				foreach($return as $k=>$item)
-				{
-					foreach($item as $item2)
-					{
-						if(isset($item2['tvuoro_id']))
-							$tvuoro_ids[] = $item2['tvuoro_id'];
-					}
-				}
-
-				if( count($tvuoro_ids) > 0 )
-				{
-
-					ToistuvatTyovuorot::model()->updatebypk($toistuva->id, array('tvuoro_ids' => json_encode($tvuoro_ids)));
 				
 					if(isset($edellinenToistuva->id))
 					{
@@ -1773,10 +1716,7 @@ class TyovuorootController extends Controller
 					//     LOG -->
 					}
 
-				}
 
-			}
-			//  tvuoro_ids Updater -->
 
 			if( count($return) > 0 )
 				echo json_encode($return);
