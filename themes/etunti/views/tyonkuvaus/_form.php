b5d9999c43d951d7718834edabcc4f87760e5271
Roman Sizov <laptopsr@gmail.com>
2017-10-24 08:56:06 +0300

    unohdin komittaa

--- themes/etunti/views/tyonkuvaus/_form.php
+++ themes/etunti/views/tyonkuvaus/_form.php
@@ -388,7 +388,7 @@ $(document).ready(function(){
 		<?php endif; ?>
 		</table>
 		<br>
-		<p style="margin-left: 10px"><a href="#" title="" class="add-author"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a></p>
+		<p style="margin-left: 7px"><a href="#" title="" class="add-author"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a></p>
 
 
 		<div class="input-group" id="kuvauksetTuoteesta" style="display:none">
