66866ff08f9f5ce5704092d87c4b65d94e914992
Roman Sizov <laptopsr@gmail.com>
2017-10-26 09:50:16 +0300

    eDico työnkuvaukset - luo asiakas linkki

--- themes/etunti/views/tyonkuvaus/create.php
+++ themes/etunti/views/tyonkuvaus/create.php
@@ -5,8 +5,9 @@
         <!-- begin: .tray-center -->
         <div class="tray-center">
 
-	   <h2 class="myBgColors p10"> <i class="glyphicon glyphicon-user"></i> <?php echo Yii::t('main', 'LUO'); ?> </h2>
+	   <h2 class="myBgColors p10"> <i class="glyphicon glyphicon-user"></i> <?php echo Yii::t('main', 'Uusi työnkuvaus'); ?> </h2>
 
+	   <p><?php echo CHtml::link('<span class="myBgColors btn btn-success">'.Yii::t('main', 'Luo asiakas').'</span>',Yii::app()->request->baseUrl.'/index.php/asiakkaat/create',array('data-toggle'=>'tooltip', 'data-placement'=>'top', 'title' => Yii::t('main', 'Lisää asiakas') )); ?></p>
 
             <div class="admin-form">
               <div class="panel heading-border">
