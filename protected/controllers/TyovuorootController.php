8c72146f19c97497a376581fc18e9b091fbfebc7
Roman Sizov <laptopsr@gmail.com>
2017-10-27 10:52:45 +0300

    Työvuorot asiakkaan mukaan, linkilllä menuun

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -28,7 +28,7 @@ class TyovuorootController extends Controller
 	{
 		return array(
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete','create','update','index','view','updatetime','showohje','did','muisti','operatio','viikko','fromto','autoinsert','autoremove','viikkottain', 'viikkottain_pdf', 'laheta','kk','pvmtid','laheta_k', 'muistin', 'muisticlear', 'muistissa', 'vkolopput', 'vkolopchange', 'uusitilaus', 'tv2', 'PoistaTv', 'valitse_kokopaiva', 'tv_kohteet', 'siivous_tyonimike', 'getKohdeByAsiakas', 'getKohdeById', 'getAsiakasByKohde', 'paivita_laatikot', 'poista_toistuva', 'onko_sama', 'asiakas_autocomplete', 'kohde_autocomplete', 'check_paallekkain', 'get_tekijantiedot', 'is_asiakas', 'is_yhteyshenkilo', 'hallinta', 'didnew', 'palautta_toistuva_pvm'),
+				'actions'=>array('admin','delete','create','update','index','view','updatetime','showohje','did','muisti','operatio','viikko','fromto','autoinsert','autoremove','viikkottain', 'viikkottain_pdf', 'laheta','kk','pvmtid','laheta_k', 'muistin', 'muisticlear', 'muistissa', 'vkolopput', 'vkolopchange', 'uusitilaus', 'tv2', 'PoistaTv', 'valitse_kokopaiva', 'tv_kohteet', 'siivous_tyonimike', 'getKohdeByAsiakas', 'getKohdeById', 'getAsiakasByKohde', 'paivita_laatikot', 'poista_toistuva', 'onko_sama', 'asiakas_autocomplete', 'kohde_autocomplete', 'check_paallekkain', 'get_tekijantiedot', 'is_asiakas', 'is_yhteyshenkilo', 'lista', 'didnew', 'palautta_toistuva_pvm'),
                 		'expression'=>"Yii::app()->controller->isEtuntiAdmin()",
 			),
 			array('deny', // allow admin user to perform 'admin' and 'delete' actions
@@ -3089,54 +3089,53 @@ class TyovuorootController extends Controller
 		return $list;
 	}
 
-	public function actionHallinta()
+	public function actionLista()
 	{
-
-		if(Yii::app()->request->getPost('tekijaPaaSivulla'))
-		Yii::app()->session['tekijaPaaSivulla'] = Yii::app()->request->getPost('tekijaPaaSivulla');
-		if(Yii::app()->request->getPost('from'))
-		Yii::app()->session['from'] = Yii::app()->request->getPost('from');
-		if(Yii::app()->request->getPost('to'))
-		Yii::app()->session['to'] = Yii::app()->request->getPost('to');
-		if(Yii::app()->request->getPost('status'))
-		Yii::app()->session['status'] = Yii::app()->request->getPost('status');
-		if(Yii::app()->request->getPost('osoite'))
-		Yii::app()->session['osoite'] = Yii::app()->request->getPost('osoite');
 
 		$from = date("d.m.Y", strtotime('first day of this month'));
 		$to = date("d.m.Y");
-		if(isset(Yii::app()->session['from']))
-		$from = date("d.m.Y", strtotime(Yii::app()->session['from']));
-		if(isset(Yii::app()->session['to']))
-		$to = date("d.m.Y", strtotime(Yii::app()->session['to']));
+		if(isset($_GET['from']))
+		$from = date("d.m.Y", strtotime($_GET['from']));
+		if(isset($_GET['to']))
+		$to = date("d.m.Y", strtotime($_GET['to']));
 
 
 		$criteria = new CDBCriteria;
+        	$criteria->order = " DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') DESC ";
         	$criteria->condition = " 				
 			DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') 
 			BETWEEN '".date("Y-m-d", strtotime($from))."' AND '".date("Y-m-d", strtotime($to))."' 
 		";
 
-		if(isset(Yii::app()->session['tekijaPaaSivulla']))
+		if(isset($_GET['tekijaPaaSivulla']))
 		{
-			$impl = implode(",", Yii::app()->session['tekijaPaaSivulla']);
+			$impl = implode(",", $_GET['tekijaPaaSivulla']);
 	        	$criteria->addCondition (" tid IN ($impl) ");
 		}
-		if(isset(Yii::app()->session['status']))
+		if(isset($_GET['status']))
 		{
-			$impl_status = implode(",", Yii::app()->session['status']);
+			$impl_status = implode(",", $_GET['status']);
 	        	$criteria->addCondition (" status IN ($impl_status) ");
 		}
-		if(isset(Yii::app()->session['osoite']))
+		if(isset($_GET['yrityksen_nimi']))
 		{
-	        	$criteria->addCondition (" kohde IN (SELECT id FROM sivex_kohdet WHERE osoite LIKE '%".Yii::app()->session['osoite']."%') ");
+	        	$criteria->addCondition (" kohde IN (SELECT id FROM sivex_kohdet WHERE 
+				asiakas_id IN (
+					SELECT id FROM asiakkaat WHERE
+					yrityksen_nimi LIKE '%".$_GET['yrityksen_nimi']."%' OR yhteyshenkilo LIKE '%".$_GET['yrityksen_nimi']."%'
+				)
+			) ");
+		}
+		if(isset($_GET['osoite']))
+		{
+	        	$criteria->addCondition (" kohde IN (SELECT id FROM sivex_kohdet WHERE osoite LIKE '%".$_GET['osoite']."%') ");
 		}
 
 		$model = Tyovuoroot::model()->findAll($criteria);
 
 		//$dataProvider->pagination->pageSize = 50;
 
-		$this->render('hallinta', array(
+		$this->render('lista', array(
 			'model' => $model,
 			'from' => $from,
 			'to' => $to,
