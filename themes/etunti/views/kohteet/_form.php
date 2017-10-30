b0fedbd4091d745fbfcbc3b055714fb047e2d56e
Roman Sizov <laptopsr@gmail.com>
2017-10-30 11:11:44 +0200

    kohdet aktiivinen

--- themes/etunti/views/kohteet/_form.php
+++ themes/etunti/views/kohteet/_form.php
@@ -230,6 +230,7 @@ $(document).ready(function(){
 	<div class="section fill mb5">
 		<?php echo $form->labelEx($model,'aktiivinen'); ?>
 		<?php
+		if(empty($model->aktiivinen)) $model->aktiivinen = 0;
 		$list = array(1=>'Kyllä',0=>'Ei');
         	echo $form->dropDownList($model, 'aktiivinen', $list,
 		array('class'=>'form-control'));
