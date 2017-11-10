376c016b88feabf663a40d5f1da8bc92072fd794
Roman Sizov <laptopsr@gmail.com>
2017-11-10 09:16:15 +0200

    matkat ja sairaus

--- themes/etunti/views/mobile/tidfromtomatkat.php
+++ themes/etunti/views/mobile/tidfromtomatkat.php
@@ -1,5 +1,19 @@
 <?php
 
+       	$criteria = new CDbCriteria();
+	$criteria->select = "
+	SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit
+	";
+	$criteria->condition = " tid = '".$tid."' AND
+	DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') 
+	BETWEEN '".$from."' AND '".$to."' 
+	AND aloitan!='' AND loppui!=''
+	AND status=2
+	AND sairaus!=1
+	AND id NOT IN (SELECT kid FROM sivexkuitti_repaired) ";
+
+	$lu = Mobile::model()->find($criteria); 
+
 
        	$criteria = new CDbCriteria();
 	$criteria->select = "
@@ -10,24 +24,11 @@
 	BETWEEN '".$from."' AND '".$to."' 
 	AND aloitan!='' AND loppui!=''
 	AND status=2
-	AND kid IN (SELECT id FROM sivexkuitti) ";
+	AND sairaus!=1
+	";
 
 	$tot = Toteutuneet::model()->find($criteria); 
 
-
-
-       	$criteria = new CDbCriteria();
-	$criteria->select = "
-	SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit
-	";
-	$criteria->condition = " tid = '".$tid."' AND
-	DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') 
-	BETWEEN '".$from."' AND '".$to."' 
-	AND aloitan!='' AND loppui!=''
-	AND status=2
-	AND id NOT IN (SELECT kid FROM sivexkuitti_repaired) ";
-
-	$lu = Mobile::model()->find($criteria); 
 
 	echo $lu->l_tunnit+$tot->t_tunnit;
 
