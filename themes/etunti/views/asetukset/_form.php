ad4029e2230d68e8e284cdabd894c2381b29b286
Roman Sizov <laptopsr@gmail.com>
2017-11-16 11:23:14 +0200

    oma kirjautumistunnus asetuksessa, rajoitus ei voi olla sama kun olemassa toisessa firmassa

--- themes/etunti/views/asetukset/_form.php
+++ themes/etunti/views/asetukset/_form.php
@@ -135,14 +135,13 @@
 
   </div><div class="col-sm-4">
     <legend><h2><?php echo Yii::t('main','Järjestelmän asetukset'); ?></h2></legend>
-<!--
 	<div class="section fill mb5">
-		<?php echo $form->labelEx($model,'erikoislauantai'); ?>
-		<?php echo $form->textarea($model,'erikoislauantai',array('rows'=>8,'maxlength'=>3000,'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'erikoislauantai'); ?>
-	</div>
--->
+		<?php $domainit = Domainit::model()->find(" domain='".Yii::app()->user->domain."' "); ?>
+		<?php echo $form->labelEx($model,'kirjautumistunnus'); ?>
+		<?php echo $form->textField($model,'kirjautumistunnus',array('value' =>$domainit->kirjautumistunnus, 'size'=>20,'maxlength'=>100,'class'=>'form-control')); ?>
 
+		<?php echo $form->error($model,'kirjautumistunnus'); ?>
+	</div>
   </div>
 </div><!-- form -->
 
