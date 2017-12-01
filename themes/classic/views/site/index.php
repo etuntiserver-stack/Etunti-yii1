22c61efeb02610211a7f7e4c38d81ebdf7a95102
Roman Sizov <laptopsr@gmail.com>
2017-12-01 12:02:12 +0200

    REBASE uusi-etusivu

--- themes/classic/views/site/index.php
+++ themes/classic/views/site/index.php
@@ -5,65 +5,6 @@
 
 <div class="container">
 
-<<<<<<< HEAD
-
-<script src='https://www.google.com/recaptcha/api.js'></script>
-
-<div class="row">
- <div class="col-sm-offset-1 col-sm-10">
-  <div class="panel panel-default">
-    <div class="panel-heading"><?=Yii::t('main', 'Aloita Etunnin käyttäminen')?></div>
-    <div class="panel-body">
-
-     <div class="row">
-      <div class="col-sm-5">
-
-	<legend><h3 class="text-info"><?php echo Yii::t('main', 'Ota käyttöön'); ?></h3></legend>
-      	<form action="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/aloita" id="aloita-lomake" method="POST" autocomplete="off">
-        <label><?php echo Yii::t('main', 'Yrityksen nimi')?></label>
-        <input type="text" id="yrityksen_nimi" name="yrityksen_nimi" class="form-control input-lg" required autofocus>
-        <label><?php echo Yii::t('main', 'Y-tunnus')?></label>
-        <input type="text" id="yritys_tunnus" name="yritys_tunnus" class="form-control input-lg" required>
-        <label><?php echo Yii::t('main', 'Puhelinnumero')?></label>
-        <input type="text" id="puhelinnumero" name="puhelinnumero" class="form-control input-lg">
-        <label><?php echo Yii::t('main', 'Sähköpostiosoite')?></label>
-        <input type="text" id="sahkoposti" name="sahkoposti" class="form-control input-lg" required>
-        <label><?php echo Yii::t('main', 'Vahvista sähköpostiosoite')?></label>
-        <input type="text" id="sahkoposti2" name="sahkoposti2" class="form-control input-lg" required>
-	<br>
-	<p><a href="#" data-toggle="modal" data-target="#kehdot">Käyttöehdot</a></p>
-
-	<p><div class="g-recaptcha" data-sitekey="6LcY2joUAAAAAFhvZM36PBNwej6vJiaHBZNsXeL4"></div></p>
-
-        <p><button class="btn btn-lg btn-primary btn-group submit" type="submit"><?php echo Yii::t('main', 'Ota käyttöön'); ?></button></p>
-    	</form>
-	<div id="success"></div>
-
-      </div>
-      <div class="col-sm-5 pull-right">
-
-	<legend><h3 class="text-info"><?php echo Yii::t('main', 'Kirjaudu'); ?></h3></legend>
-		<form action="<?php echo Yii::app()->request->baseUrl; ?>/index.php/user/login" method="POST">
-		<label><?php echo Yii::t('main', 'Kirjautumistunnus')?></label>
-		<input type="text" class="form-control input-lg" name="UserLogin[domain]" required>
-		<label><?php echo Yii::t('main', 'Käyttäjätunnus')?></label>
-		<input type="text" class="form-control input-lg" name="UserLogin[username]" required>
-		<label><?php echo Yii::t('main', 'Salasana')?></label>
-		<input type="password" class="form-control input-lg" name="UserLogin[password]" required>
-		<br>
-		<input type="submit" class="btn btn-lg btn-primary btn-group" value="Kirjaudu">
-		</form>
-		<a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/salasanan_palauttaminen" class="link"><?php echo Yii::t('main', 'Unohditko salasanasi?'); ?></a>
-
-      </div>
-     </div>
-
-    </div>
-  </div>
- </div>
-</div>
-=======
->>>>>>> 13cd71d08d6ae34d0b8a3840299b0a88bf78b539
 
 <script src='https://www.google.com/recaptcha/api.js'></script>
 
@@ -125,18 +66,6 @@
 <!-- Modal -->
 <div id="kehdot" class="modal fade" role="dialog">
   <div class="modal-dialog modal-lg">
-
-
-
-
-
-<<<<<<< HEAD
-=======
-
-<!-- Modal -->
-<div id="kehdot" class="modal fade" role="dialog">
-  <div class="modal-dialog modal-lg">
->>>>>>> 13cd71d08d6ae34d0b8a3840299b0a88bf78b539
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
