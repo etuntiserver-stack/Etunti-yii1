9fc00613a44bc7bf8ed0ffc276a2b7baed9e95b3
Roman Sizov <laptopsr@gmail.com>
2017-12-11 11:49:13 +0200

    ET-1318 Etunti Asiakashaku

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2250,14 +2250,14 @@ $(document).ready(function(){
 		foreach($m as $data)
 		{
 
-		    if($model == 'Asiakkaat' and $data->tyyppi == 'yritys')
+		    if($model == 'Asiakkaat' and $sarake == 'yrityksen_nimi' and $data->tyyppi == 'yritys')
 		    {
 		    $arr[] = array(
 		        'label'=>$data->yrityksen_nimi,
 		        'value'=>$data->yrityksen_nimi,    
 		        'id'=>$data->id,
         	    );
-		    } else if($model == 'Asiakkaat' and $data->tyyppi == 'henkilo')
+		    } else if($model == 'Asiakkaat' and $sarake == 'yrityksen_nimi' and $data->tyyppi == 'henkilo')
 		    {
 		    $arr[] = array(
 		        'label'=>$data->yhteyshenkilo,
