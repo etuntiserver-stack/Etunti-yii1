b0072bdfd59d67ef1aebcb6e3498aa94c7101dc0
Roman Sizov <laptopsr@gmail.com>
2017-12-14 10:00:52 +0200

    Laatutasot asetuksesta pois

--- protected/models/Asetukset.php
+++ protected/models/Asetukset.php
@@ -120,10 +120,10 @@ class Asetukset extends DB2ActiveRecord
                      'app_hyvaksytyt_tyot_vkomaara' => 'int(3) DEFAULT 4 ',
                      'app_naytetaanko_hyvaksyttyt_tunnit' => 'int(1) ',
                      'app_naytta_avain' => 'int(1) ',
-                     'edico_laatutaso_1' => 'text ',
-                     'edico_laatutaso_2' => 'text ',
-                     'edico_laatutaso_3' => 'text ',
-                     'edico_muut_kulut' => 'text ',
+                     //'edico_laatutaso_1' => 'text ',
+                     //'edico_laatutaso_2' => 'text ',
+                     //'edico_laatutaso_3' => 'text ',
+                     //'edico_muut_kulut' => 'text ',
                      'tapaturmavakuutus' => 'int(11) ',
                      'ryhmahenkivakuutus' => 'int(11) ',
                      'tyottomyysvakuutusmaksu' => 'int(11) ',
@@ -176,7 +176,7 @@ class Asetukset extends DB2ActiveRecord
 			array('trust_url, checkout_salasana, trust_ws_api_url, netvisor_customer_id, netvisor_partner_id, netvisor_userkey, netvisor_partnerkey, netvisor_organisation_identifier, merkkipaivailmoitukset_sahkoposti, netvisor_mita_lahetetaan, gtm, apuaika_palkkalaji', 'length', 'max'=>255),
 			array('viikonloppulisa_la, viikonloppulisa_su, vinkki_tunnit, vinkki_prosentti', 'length', 'max'=>10),
 			array('aikavali_halytys', 'length', 'max'=>3),
-			array('oikeudet, pyhapaivat, erikoislauantai, tilausvahvistus, rekisteriseloste, onlinevaraus_laatu_luotettavuus, onlinevaraus_takuu_turvallisuus, onlinevaraus_asiakaspalvelu, onlinevaraus_arvio_siivouksesta, ilmoitus_toistuvien_tyovuorojen_paattymisesta_saajat, ilmoitus_uudesta_kuvasta_saajat, edico_muut_kulut, edico_laatutaso_1, edico_laatutaso_2, edico_laatutaso_3,peruutusehdot', 'safe'),
+			array('oikeudet, pyhapaivat, erikoislauantai, tilausvahvistus, rekisteriseloste, onlinevaraus_laatu_luotettavuus, onlinevaraus_takuu_turvallisuus, onlinevaraus_asiakaspalvelu, onlinevaraus_arvio_siivouksesta, ilmoitus_toistuvien_tyovuorojen_paattymisesta_saajat, ilmoitus_uudesta_kuvasta_saajat, peruutusehdot', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('id, syntyrin_emails, paivan_uutinen, logon_polkku, logon_korkeus, johtaja, viivastyskorko, tilinumero, iban, bic, trust_cid, trust_api, palvelu_tyyppi, trust_url, pyhapaivat, erikoislauantai, sovellus_tyovuorot', 'safe', 'on'=>'search'),
