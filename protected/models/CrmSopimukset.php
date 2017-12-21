92ef983af9224e895e920c86a44195d6c7ad53fa
Roman Sizov <laptopsr@gmail.com>
2017-12-21 10:24:46 +0200

    sopimus ilman tarjousta, huono idea

--- protected/models/CrmSopimukset.php
+++ protected/models/CrmSopimukset.php
@@ -79,9 +79,9 @@ public $template;
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('tarjous_id, template', 'required'),
+			array('template', 'required'), //tarjous_id
 			//array('time, asiakas_id, teksti, hyvaksyn_koodi, asiakkaan_sahkoposti, status, liite', 'required'),
-			array('asiakas_id, status, yhteystiedot_id, voimassaolo', 'numerical', 'integerOnly'=>true),
+			array('tarjous_id, asiakas_id, status, yhteystiedot_id, voimassaolo', 'numerical', 'integerOnly'=>true),
 			array('hyvaksyn_koodi, liite, template', 'length', 'max'=>255),
 			array('asiakkaan_sahkoposti', 'length', 'max'=>100),
 			array('teksti', 'safe'),
