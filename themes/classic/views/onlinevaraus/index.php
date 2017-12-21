887740113d3ec7f0d89d2d3855e0d067b9240063
Roman Sizov <laptopsr@gmail.com>
2017-12-21 11:41:37 +0200

    header pois onlinevarauksesta

--- themes/classic/views/onlinevaraus/index.php
+++ themes/classic/views/onlinevaraus/index.php
@@ -58,7 +58,7 @@ Asetukset::model()->updatebypk(1, array('onlinevaraus_loppu'=>18));
 ?>
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/onlinevaraus_2.css">
 
-<div class="container-fluid well">
+<div class="container-fluid">
 <br>
 
 <div class="row">
