289e7e69d09b463ccdca46a8157bde9fb1fce0cf
Roman Sizov <laptopsr@gmail.com>
2017-12-27 11:09:24 +0200

    sopimus tarjouksesta

--- protected/models/Asetukset.php
+++ protected/models/Asetukset.php
@@ -141,7 +141,9 @@ class Asetukset extends DB2ActiveRecord
 		     'edico_tehdyt_tyot' => 'varchar(100)',
 		     'netvisor_lahetetaanko_tyontekija' => 'int(1)',
 		     'lasketaanko_lounastauko' => 'int(1)',
+
 		     'apuaika_meneeko_laskutukseen' => 'int(1) DEFAULT 0',
+		     'apuaika_palkkalaji' => 'varchar(255)',
 
 		);
 		$is_added_somthing = false;
