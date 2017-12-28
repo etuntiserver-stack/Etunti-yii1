931b3763074bd1f77bf1bad88eeb0697495413b0
Roman Sizov <laptopsr@gmail.com>
2017-12-28 13:31:06 +0200

    sopimus ilman tarjousta

--- themes/etunti/views/crmTarjoukset/_view.php
+++ themes/etunti/views/crmTarjoukset/_view.php
@@ -92,7 +92,9 @@
 		<?php if( isset($data->sopimukset->tarjous_id) and $data->sopimukset->tarjous_id == $data->id ) : ?>
 		<?php echo CHtml::link('<span class="btn btn-success btn-block">'.Yii::t('main', 'Valmis').'</span>',Yii::app()->request->baseUrl.'/index.php/crmSopimukset/update?id='.$data->sopimukset->id); ?>
 		<?php else : ?>
+		<?php if( $data->status != 3 ) : ?>
 		<?php echo CHtml::link('<span class="btn btn-primary btn-block">'.Yii::t('main', 'Tee sopimus').'</span>',Yii::app()->request->baseUrl.'/index.php/crmSopimukset/create?sopimus_tarjouksesta=true&id='.$data->id); ?>
+		<?php endif; ?>
 		<?php endif; ?>
 	</td>
 </tr>
