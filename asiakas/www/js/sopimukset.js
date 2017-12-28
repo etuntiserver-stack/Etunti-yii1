602425999c7fb14b94f3841edd353f96e85ba674
Roman Sizov <laptopsr@gmail.com>
2017-12-28 12:54:57 +0200

    sopimus ilman tarjousta

--- asiakas/www/js/sopimukset.js
+++ asiakas/www/js/sopimukset.js
@@ -146,7 +146,7 @@ function saveFileToStorage(link, filename, ext){
 		    function(error) {
 		        console.log("download error source " + error.source);
 		        console.log("download error target " + error.target);
-		        console.log("upload error code" + error.code);
+		        alert("upload error code" + error.code);
 		    },
 		    false,
 		    {
