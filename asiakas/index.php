1798785786e4fa9cab6efbccdc07e062230bcd9a
Roman Sizov <laptopsr@gmail.com>
2018-01-05 10:48:25 +0200

    asiakas sivu

---
+++ asiakas/index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<style>
+body {
+    background: url(../../etusivuimg/Etunti-tausta-2.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    font-size: 18px;
+    font-family: "Ubuntu", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    line-height: 1.6;
+    color: #777;
+    -webkit-font-smoothing: antialiased;
+    font-weight: normal;
+}
+iframe{
+	/*border-radius: 15px;*/
+	border: 4px #ddd solid;
+}
+</style>
+<body style="margin-top:40px;padding:0px;overflow:hidden">
+<div class="container">
+ <div class="row">
+  <div class="col-sm-offset-3 col-sm-6">
+    <iframe src="www" frameborder="0" style="overflow:hidden;height:80%;width:100%" height="100%" width="100%" ></iframe>
+  </div>
+ </div>
+</div>
+</body>
