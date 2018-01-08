b579513d60ce1b9d686c61726a07157f695e45c2
Roman Sizov <laptopsr@gmail.com>
2018-01-08 10:34:45 +0200

    virheloki

--- protected/config/main.php
+++ protected/config/main.php
@@ -98,6 +98,11 @@ session_start();
 					'enabled'=>YII_DEBUG,
                     			//'categories'=>'system.*',
 				), 
+				array(
+				        'class'=>'CWebLogRoute',
+                			'levels'=>'error, warning', //'trace, info, error, warning, vardump'
+
+				),
 			        array(
 				        'class'=>'CEmailLogRoute',
                 			'levels'=>'error', //'trace, info, error, warning, vardump'
