1fa6a5b204a4e0a9464bf15e7167f3e103a76508
Roman Sizov <laptopsr@gmail.com>
2018-02-06 10:42:33 +0200

    maksettu sivu

--- themes/onlinevaraus/views/onlinevaraus/palvelu_save_ajax.php
+++ themes/onlinevaraus/views/onlinevaraus/palvelu_save_ajax.php
@@ -186,9 +186,16 @@
 	$body = 
 	'
 <div class = "row panel panel-default select-service-panel">
- <div class = "panel-body collapse" id="order_summary">
-  <div class="row">
-   <div class="col-sm-8 col-sm-offset-2"> 
+   <div class="panel-heading">
+      <h3>
+       <button class="pull-right btn btn-lg btn-warning" data-toggle="collapse" data-target="#order_summary" id="show_yhteenveto">'.Yii::t('main', 'Näytä lisää').'</button>
+       '.Yii::t('main', 'Yhteenveto').'
+      </h3>
+   </div>
+   <div class="row">
+   <div class="col-sm-8 col-sm-offset-2">   
+   <div class = "panel-body collapse" id="order_summary">
+
 	';
 
 
@@ -293,14 +300,8 @@
 
 	$body .= '
    </div>
-  </div>
- </div>
-   <div class="panel-footer">
-      <h3>
-       <button class="pull-right btn btn-lg btn-warning" data-toggle="collapse" data-target="#order_summary" id="show_yhteenveto">'.Yii::t('main', 'Näytä lisää').'</button>
-       '.Yii::t('main', 'Yhteenveto').'
-      </h3>
-   </div><!--footer-->
+   </div>
+   </div>
 </div>'; //panel
 
 	echo json_encode($body);
