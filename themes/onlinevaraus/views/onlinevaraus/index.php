41d9033fb6842c622f5a301a0a3b9b07ad2972fc
Elias Luoma <elias.luoma@santelo.fi>
2018-02-07 11:40:41 +0200

    Yhteenvedon levys

--- themes/onlinevaraus/views/onlinevaraus/index.php
+++ themes/onlinevaraus/views/onlinevaraus/index.php
@@ -286,7 +286,7 @@
   <div class="col-sm-6 col-sm-offset-3">
 
    <div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
+    <div class="col-sm-10 col-sm-offset-1" >
 	    <div id="panGetContent"></div>
    </div>
    </div>
