e0febefe37e0740df99a148361b3dce25f106890
Roman Sizov <laptopsr@gmail.com>
2018-03-07 09:24:59 +0200

    aloita lomakessa on rajoituksia nyt

--- protected/models/Domainit.php
+++ protected/models/Domainit.php
@@ -65,7 +65,8 @@ public $viesti;
                      'aktiivinen' => 'int(1) DEFAULT 1 ',
                      'maksullinen' => 'int(1) DEFAULT 0 ',
                      'ilmainen_versio_kayttotunnit' => 'int(11) ',
-                     'kirjautumistunnus' => 'varchar(100) ',
+                     'kirjautumistunnus' => 'varchar(100)',
+		     'y_tunnus' => 'varchar(100)',
                 );
 
 		foreach($table_structure as $key=>$value)
@@ -192,7 +193,7 @@ public $viesti;
 		return array(
 			array('domain', 'required'),
 			array('huoltokatko, palveluhinta_persiivoja, tyovuorohinta_persiivoja, muut_tyokaluhinta, aktiivinen, maksullinen', 'numerical', 'integerOnly'=>true),
-			array('time, domain, paketti, yritys, pakettin_nimetus', 'length', 'max'=>100),
+			array('time, domain, paketti, yritys, pakettin_nimetus, y_tunnus', 'length', 'max'=>100),
 			array('puhelin, sahkoposti', 'length', 'max'=>255),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
