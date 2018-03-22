de934a6190ad16eed90c6d335f2cacfc5ea8ef08
Roman Sizov <laptopsr@gmail.com>
2018-03-22 14:20:55 +0200

    autoremove

--- protected/models/Asetukset.php
+++ protected/models/Asetukset.php
@@ -237,7 +237,7 @@ class Asetukset extends DB2ActiveRecord
 			'onlinevaraus_asiakaspalvelu' => Yii::t('main', 'Asiakaspalvelu'),
 			'onlinevaraus_arvio_siivouksesta' => Yii::t('main', 'Arvio palvelusta'),
 			'onlinevaraus_aikavali' => Yii::t('main', 'Varauksessa matka-aika, ennen ja jälkeen'),
-			'onlinevaraus_autoremove' => Yii::t('main', 'Vahvistamaton varaus poistetaan, tuntia'),
+			'onlinevaraus_autoremove' => Yii::t('main', 'Vahvistamaton varaus poistetaan, minuuttia'),
 			'aikavali_halytys' => Yii::t('main', 'Aikaväli hälytys (min)'),
 			'ilmoitus_avoimista_kohteesta_sahkopostiin' => Yii::t('main', 'Ilmoitus määräajan ylittäneistä kohteista sähköpostiin'),
 			'ilmoitus_myohastyneista_kohteesta_sahkopostiin' => Yii::t('main', 'Ilmoitus myöhästyneistä kohteesta sähköpostiin'),
