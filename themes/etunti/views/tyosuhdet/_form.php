9dad811c3c409626f12aa5e5635219b650b60682
Roman Sizov <laptopsr@gmail.com>
2018-04-05 11:30:40 +0300

    tallenna painike

--- themes/etunti/views/tyosuhdet/_form.php
+++ themes/etunti/views/tyosuhdet/_form.php
@@ -331,12 +331,12 @@ $(document).ready(function(){
         placeholder: "__:__"
   });
 
+  $('#Tyosuhdet_loppu').blur(function() {
+	alert('Olet merkinnyt työsuhteen päättyväksi. Viimeisen palkanmaksun jälkeen tulisi poistaa kaikki työntekijän tiedot, joita ei lain mukaan tarvitse säilyttää. Näitä tietoja ovat muun muassa palkka- ja verotiedot.');
+  });
 
   $('#tyosuhdet-form').on('submit',function(e) {
 
-     if( '<?=$model->loppu?>' == '' && $('#Tyosuhdet_loppu').val() !== '' ){
-	alert('Olet merkinnyt työsuhteen päättyväksi. Viimeisen palkanmaksun jälkeen tulisi poistaa kaikki työntekijän tiedot, joita ei lain mukaan tarvitse säilyttää. Näitä tietoja ovat muun muassa palkka- ja verotiedot.');
-     }
      if( $('#modelID').val() )
      {
 	  $.ajax({
