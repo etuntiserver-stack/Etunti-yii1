a7999eb397dc625eb53cc9fb77167d86bd029574
Roman Sizov <laptopsr@gmail.com>
2018-04-06 12:01:29 +0300

    images

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -19,7 +19,7 @@ if(isset($_GET['mail'])){
 	}
 }
 
-echo Yii::app()->getBasePath().'/img/tekijat/'.strtolower(Yii::app()->user->domain);
+echo dirname(Yii::app()->getBasePath()).'/img/tekijat/'.strtolower(Yii::app()->user->domain);
 //phpinfo();
 
 $this->breadcrumbs=array(
