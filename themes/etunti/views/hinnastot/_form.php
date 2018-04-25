cb5ebe54fc2388ae4222ab6f60bcea953df6de3e
Roman Sizov <laptopsr@gmail.com>
2018-04-25 13:44:53 +0300

    hinnasto alvsis ongelma

--- themes/etunti/views/hinnastot/_form.php
+++ themes/etunti/views/hinnastot/_form.php
@@ -47,6 +47,14 @@
 
 <div class="row">
  <div class="col-sm-12">
+
+	<div class="row">
+	 <div class="col-sm-4">
+		<b>Hinnat sis. ALV</b> <input type="radio" name="alvsis" value="sis"> <br>
+		<b>Hinnat ALV0</b> <input type="radio" name="alvsis" value="nolla" checked>
+	 </div>
+	</div>
+
   <div class="table-responsive">
    <table class="table table-bordered bg-white" id="TableHinnasto">
     <tr>
@@ -54,8 +62,8 @@
      <th><?=Yii::t('main', 'HINTA TUOTTEISTA JA PALVELUISTA')?></th>
      <th><?=Yii::t('main', 'HINNASTON HINTA')?></th>
      <th><?=Yii::t('main', 'HINNASTON ALV%')?></th>
-     <th width="1"><?=Yii::t('main', 'YHTEENSÄ')?></th>
-     <th width="1"><?=Yii::t('main', 'YKSIKKÖ')?></th>
+     <th width="150"><?=Yii::t('main', 'YHTEENSÄ')?></th>
+     <th width="100"><?=Yii::t('main', 'YKSIKKÖ')?></th>
      <th></th>
     </tr>
 
@@ -80,7 +88,7 @@
 	 <?php endforeach; ?>
 	</select>
      </td>
-     <td><input type="number" class="form-control hinnasto_yht" name="Rivi[tuote][hinnasto_yht][]" readonly step="any" value="<?=$r->hinnasto_yht?>"></td>
+     <td><input type="number" class="form-control hinnasto_yht" name="Rivi[tuote][hinnasto_yht][]" step="any" value="<?=$r->hinnasto_yht?>"></td>
      <td>
 	<select name="Rivi[tuote][yksikko][]" class="form-control yksikkovalikko">
 	 <option value=>Valitse</option>
@@ -132,7 +140,7 @@
 	 <?php endforeach; ?>
 	</select>
      </td>
-     <td><input type="number" class="form-control hinnasto_yht" name="Rivi[tuote][hinnasto_yht][]" readonly step="any"></td>
+     <td><input type="number" class="form-control hinnasto_yht" name="Rivi[tuote][hinnasto_yht][]" step="any"></td>
      <td>
 	<select name="Rivi[tuote][yksikko][]" class="form-control yksikkovalikko">
 	 <option value=>Valitse</option>
@@ -167,19 +175,25 @@ $(document).ready(function(){
   });
 
   $(document).delegate(".hinnasto_hinta","keyup",function(){
-	var hinnasto_hinta = parseFloat($(this).val());
-	var alv = $(this).closest('tr').find('.hinnasto_alv').val();
-	var yht = parseFloat((hinnasto_hinta/100*alv), 10);
-	var summ = hinnasto_hinta+yht;
-	$(this).closest('tr').find('.hinnasto_yht').val(summ.toFixed(2));
+	var alvsis = $('input[name=alvsis]:checked').val();
+	if( alvsis == 'nolla'){
+		var hinnasto_hinta = parseFloat($(this).closest('tr').find('.hinnasto_hinta').val());
+		var alv = $(this).closest('tr').find('.hinnasto_alv').val();
+		var yht = hinnasto_hinta/100*alv;
+		var summ = hinnasto_hinta+yht;
+		$(this).closest('tr').find('.hinnasto_yht').val(summ.toFixed(2));
+	}
   });
 
   $(document).delegate(".hinnasto_alv","change",function(){
-	var hinnasto_hinta = parseFloat($(this).closest('tr').find('.hinnasto_hinta').val());
-	var alv = $('option:selected', this).val();
-	var yht = parseFloat((hinnasto_hinta/100*alv), 10);
-	var summ = hinnasto_hinta+yht;
-	$(this).closest('tr').find('.hinnasto_yht').val(summ.toFixed(2));
+	var alvsis = $('input[name=alvsis]:checked').val();
+	if( alvsis == 'nolla'){
+		var hinnasto_hinta = parseFloat($(this).closest('tr').find('.hinnasto_hinta').val());
+		var alv = $(this).closest('tr').find('.hinnasto_alv').val();
+		var yht = hinnasto_hinta/100*alv;
+		var summ = hinnasto_hinta+yht;
+		$(this).closest('tr').find('.hinnasto_yht').val(summ.toFixed(2));
+	}
   });
 
   $(document).delegate(".poistarivi","click",function(){
