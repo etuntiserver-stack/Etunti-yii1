ec60f2078f07574f98c43f938275f86035ea0b88
Roman Sizov <laptopsr@gmail.com>
2018-05-02 12:21:02 +0300

    laskutuksessa Otsikkorivissä pitäisi Hintasaraketta levittää ja Alv-% saraketta kaventaa. Nyt hinta Chromella Hinta ei näy kokonaan ja Mozillalla hintatieto jää täysin piiloon

--- themes/etunti/views/lasku/_form.php
+++ themes/etunti/views/lasku/_form.php
@@ -667,8 +667,8 @@ echo '<input type="hidden" id="palvelu_tyyppi" value="'.$asetukset->palvelu_tyyp
 	<TH class="col-sm-2">Tuote/Palvelu</TH>
 	<TH class="col-sm-1">Määrä</TH>
 	<TH class="col-sm-1">Yksikkö <span class="btn btn-primary btn-xs myBgColors muokaValiko" for="laskutus_yksikko"><i class="fa fa-pencil-square-o"></i></span></TH>
-	<TH class="col-sm-1">Hinta</TH>
-	<TH class="col-sm-1">ALV %</TH>
+	<TH class="col-sm-2">Hinta</TH>
+	<TH>ALV %</TH>
 	<TH class="col-sm-1">ALV</TH>
 	<TH class="col-sm-1">Ale %</TH>
 	<TH class="col-sm-1">Veroton</TH>
