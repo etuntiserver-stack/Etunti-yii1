280db5b16debed7890cfc87d9726d98e01fdfc70
Roman Sizov <laptopsr@gmail.com>
2018-05-03 09:27:05 +0300

    test

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -711,7 +711,7 @@ public function actionImei($dom)
 				and DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') 
 				BETWEEN CURDATE() AND '".$aikaVali."'
 				AND piilota_mobiilista!=1
-				AND (peruutettu=0 OR peruutettu=NULL)
+				AND (peruutettu=0 OR peruutettu='')
 		    ";
 	            $tvuoro = Tyovuoroot::model()->findAll($criteria);
 
