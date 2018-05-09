aada964ba469d2725cebc76d5879a062a89a7366
Roman Sizov <laptopsr@gmail.com>
2018-05-09 13:50:56 +0300

    sovellus mysql kuormaus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -510,7 +510,7 @@ public function actionImei($dom)
 	// <-- Check Tyontekija
 	if(isset($_POST['email']) and isset($_POST['salasana']) )
 	{
-
+/*
 	    if(
 		isset($_SESSION['tid']) and isset($_SESSION['email']) and isset($_SESSION['salasana']) 
 		and $_SESSION['email'] == $_POST['email'] and $_SESSION['salasana'] == $_POST['salasana'])
@@ -527,6 +527,7 @@ public function actionImei($dom)
 
 	    if(!isset($_SESSION['tid']))
 	    {
+*/
 		$criteria = new CDbCriteria();
 		$criteria->condition = " 
 			aktiivinen=1 AND mobiili=1
@@ -540,7 +541,7 @@ public function actionImei($dom)
 			$_SESSION['salasana'] = $_POST['salasana'];
 		}
 
-	    }
+	    //}
 
 	}
 
