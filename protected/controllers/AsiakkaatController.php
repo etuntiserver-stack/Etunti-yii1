577a865947c0c239ce8e27017499c806628d88ca
Roman Sizov <laptopsr@gmail.com>
2018-05-15 14:57:03 +0300

    tunnukset lahetys

--- protected/controllers/AsiakkaatController.php
+++ protected/controllers/AsiakkaatController.php
@@ -436,13 +436,13 @@ class AsiakkaatController extends Controller
 
 				$subject = $yr.' toivottaa sinut tervetulleeksi käyttämään eDicoa';
 				$message = 'Hei '.$asiakas.'!<br>
+Olemme tehneet sinulle profiilin eDico-sovellukseen, jolla voit olla kätevästi yhteydessä meihin, antaa palautetta, tarkastella tilauksiasi ja vahvistaa sopimukset ja tarjoukset. <br>
+Lataa eDico-sovellus älylaitteeseesi alla olevan linkin kautta.<br>
+
 				<b>Domain:</b> '.Yii::app()->user->domain.'<br>
 				<b>Käyttäjätunnus:</b> '.$model->sahkoposti.'<br>
 				<b>Luo oma salasana:</b> <a href='.Yii::app()->createAbsoluteUrl('asiakkaat/salasana', array('domain' => Yii::app()->user->domain, 'token' => $token, 'asiakasid' => $model->id)).'>tästä</a><br>
 <p>
-Olemme tehneet sinulle profiilin eDico-sovellukseen, jolla voit olla kätevästi yhteydessä meihin, antaa palautetta, tarkastella tilauksiasi ja vahvistaa sopimukset ja tarjoukset. <br>
-Lataa eDico-sovellus älylaitteeseesi alla olevan linkin kautta.<br>
-
 <p>Ystävällisin terveisin,<br>
 Yritys '.$yr.'
 </p>
