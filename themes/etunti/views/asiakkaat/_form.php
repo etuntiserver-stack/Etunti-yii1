9298e50c75f7a0269d2c7838e10a50d6fe92fac2
Roman Sizov <laptopsr@gmail.com>
2018-06-01 10:09:51 +0300

    veikon korjaus

--- themes/etunti/views/asiakkaat/_form.php
+++ themes/etunti/views/asiakkaat/_form.php
@@ -287,7 +287,9 @@ $model->hinta_sis_alv = round($model->hinta_sis_alv, 2);
 		<?php echo $form->labelEx($model,'alennuskoodit'); ?>
 		<?php
 			$alennuskoodit = json_decode($model->alennuskoodit, true);
-			echo '<textarea class="form-control" name="Asiakkaat[alennuskoodit]">'.implode("\n", $alennuskoodit).'</textarea>';
+			if( is_array($alennuskoodit) ){
+			  echo '<textarea class="form-control" name="Asiakkaat[alennuskoodit]">'.implode("\n", $alennuskoodit).'</textarea>';
+			}
 		?>
 	</div>
 	<?php endif; ?>
