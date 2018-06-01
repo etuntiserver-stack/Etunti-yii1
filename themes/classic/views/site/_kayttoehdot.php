e3c74459f1a74849671a0ab2c5d3ac614728caf2
Roman Sizov <laptopsr@gmail.com>
2018-06-01 10:06:23 +0300

    uusi kayttoehdot

---
+++ themes/classic/views/site/_kayttoehdot.php
@@ -0,0 +1,116 @@
+<p><h3>Soveltamisala</h3></p>
+
+<p>Näitä sopimusehtoja sovelletaan Digisten Oy:n (myöhemmin Digisten tai Palveluntoimittaja) verkon välityksellä tarjoamaan Etunti-verkkopalveluun (myöhemmin Etunti tai Etunti- verkkopalvelu) sekä Tilaajan asiointiin verkkopalvelussa.</p>
+
+<p>Tilaajalla tarkoitetaan tahoa, joka tekee sopimuksen Palveluntoimittajan kanssa Etunti-verkkopalvelun käytöstä. Sopimuksen voi tehdä vain yritystunnuksen omaava oikeushenkilö. Henkilö, joka tekee sopimuksen oikeushenkilön puolesta, vakuuttaa että hänellä on oikeus tehdä sitova sopimus edustamansa oikeushenkilön puolesta. Käyttäjällä tarkoitetaan henkilöä, joka käyttää Palveluntoimittajan verkkopalvelua Tilaajan lukuun.</p>
+
+<p><h3>Sopimusasiakirjat</h3></p>
+
+<p>Sopimusasiakirjat määräämisjärjestyksessä ovat nämä Sopimusehdot sekä Palvelusopimus. Sopimusosapuolet ja palvelun sisältö määritellään Palvelusopimuksessa. Tilaajan käyttöön tulevat Etunti palvelut ja asiantuntijapalvelut. Mahdolliset muutokset ja lisätilaukset liitetään sopimukseen erillisellä Palvelusopimuksen päivityksellä.</p>
+
+<p><h3>Sopimuksen syntyminen</h3></p>
+
+<p>Sopimus katsotaan syntyneeksi, kun Tilaaja hyväksyy nämä ehdot tai kun Toimittaja on muutoin todistettavasti hyväksynyt Tilaajan tilauksen.</p>
+
+<p><h3>Palvelun käyttö</h3></p>
+
+<p>Etunti-verkkopalvelun käyttäminen edellyttää käyttäjän tunnistamista tunnistetietojen eli henkilökohtaisen käyttäjätunnuksen ja salasanan avulla. Henkilökohtaiset tunnukset luodaan järjestelmän valvojan tunnuksilla, jotka Tilaaja saa käyttönsä verkkopalvelun käyttöönoton yhteydessä.</p>
+
+<p>Etunti-verkkopalvelua käytetään Internet selaimella. Tilaaja voi käyttää palvelua sekä Suomessa että ulkomailla. Tietoturvateknisistä syitä Palveluntarjoaja voi rajoittaa palvelun käytettävyyttä yksittäisissä maissa. Palveluiden käyttöön ja Palveluntoimittajan velvoitteisiin sovelletaan kuitenkin kaikissa tilanteissa näiden sopimusehtojen lisäksi vain Suomen lakia.</p>
+
+<p>Tilapäiset häiriöt puhelin- ja dataliikenteessä ovat mahdollisia ja ne voivat hetkellisesti estää tai hidastaa verkkopalvelun käytön.</p>
+
+<p>Palveluntarjoaja ei vastaa kolmansien osapuolien palveluiden mahdollisista häiriöistä.</p>
+
+<p><h3>Tilaajan velvoitteet</h3></p>
+
+<p>Tilaaja hankkii Etunti-verkkopalvelun käyttöön soveltuvat puhelimet ohjelmistoineen sekä tarvittavat tietoliikenneyhteydet ja vastaa niiden toimivuudesta, tietoturvasta sekä käyttö- ja ylläpitokustannuksista.</p>
+
+<p>Tilaajakohtaiset käyttäjätunnukset ja salasanat luodaan Tilaajalle verkkopalvelun käyttöönoton yhteydessä. Tilaaja vastaa käyttäjätunnusten ja salasanojen asianmukaisesta käyttämisestä ja salassapidosta.</p>
+
+<p>Tilaaja vastaa hänen tunnistetiedoillaan tehdyistä verkkopalvelun käytöstä. Mikäli Palveluntoimittajalla on syytä epäillä tunnistetietojen väärinkäyttöä tai niiden joutumista vääriin käsiin, on tällä oikeus estää Etunnin verkkopalvelunkäyttö.</p>
+
+<p>Mahdollisesti vääriin käsiin joutuneista salasanoista tai käyttäjätunnuksista on ilmoitettava Palveluntoimittajalle välittömästi. Tilaaja vastaa asiasta mahdollisesti aiheutuvasta vahingosta, kunnes hän on raportoinut asiasta Palveluntoimittajaa ja tälle on jäänyt kohtuullinen aika estää mahdolliset väärinkäytökset.</p>
+
+<p>Tilaaja vastaa Tilaajan asiakastietojen viennistä Etunti-verkkopalvelun tietokantaan. Tilaaja on vastuussa asiakastietojen oikeellisuudesta ja mahdollisten virheiden korjaamisesta. Tilaaja sitoutuu varmistamaan, että tietokantaan vietyjen asiakastietojen perustana on vain olemassa oleva asiakkuus.</p>
+
+<p><h3>Palveluntoimittajanvelvoitteet</h3></p>
+
+<p>Tilaajan yrityskohtaisten tietosisältöjen osalta (esim. asiakastiedot) Palveluntoimittajaa ja tämän henkilökuntaa sitoo salassapitovelvollisuus.</p>
+
+<p>Palveluntoimittajalla ei ole oikeutta luovuttaa asiakastietoja ulkopuolisille tahoille ilman Tilaajan erillistä suostumusta pois lukien tilanteet jossa Suomen laki tätä edellyttää.</p>
+
+<p>Palveluntoimittaja vastaa tietojärjestelmiensä toimivuudesta ja tietoturvan asianmukaisesta järjestämisestä.</p>
+
+<p><h3>Palveluntoimittajan oikeudet</h3></p>
+
+<p>Palveluntoimittajalla on oikeus rajoittaa verkkopalvelujen käyttöaikaa tai keskeyttää tilapäisesti palveluiden toimittaminen tarpeellisten huolto- ja ylläpitotoimenpiteiden suorittamista varten tai muista näihin verrattavista syistä ilmoittamalla siitä asiakkailleen verkkopalvelussa. Ilmoitus pyritään tekemään hyvissä ajoin etukäteen.</p>
+
+<p>Palveluntoimittajalla on oikeus keskeyttää palveluiden toimittaminen sekä jättää sille annettu toimeksianto täyttämättä, mikäli Tilaaja ei noudata palveluja koskevia sopimusehtoja, suorita sopimuksen mukaisia maksuja tai milloin Palveluntoimittajalla on perusteltua syytä epäillä, että palvelua käytetään lainvastaiseen toimintaan tai tavalla, joka saattaa aiheuttaa vahinkoa tai vahingonvaaran Palveluntoimittajalle, Tilaajalle tai sivulliselle.</p>
+
+<p>Palveluntoimittaja päättää Käyttäjälle tarjottavan tuen saatavuudesta ja sen palvelutasosta sekä veloitusperusteesta. Digisten Oy:llä on oikeus tallettaa arkistoida asiakaspalvelupuhelut ja -viestit.</p>
+
+<p>Palveluntoimittajalla on oikeus käyttää Tilaajan nimeä ja Tilaajan kotisivuilla esitettyä graafista yritystunnusta (logoa) Digisten Oy:n markkinointimateriaalissa, esimerkkinä ohjelmistoa käyttävästä yrityksestä.</p>
+
+<p>Palveluntoimittajalla on tähän sopimukseen perustuen oikeus kohdistaa Käyttäjän organisaatiolle lähetettävää Digistenin palveluita koskevaa viestintää.</p>
+
+<p><h3>Hinta ja hinnoitteluperiaatteet</h3></p>
+
+<p>Etunti-verkkopalvelun hinta määräytyy Tilaajan kanssa sovitun palvelukokonaisuuden mukaisesti. Ilmaista Etunti-ohjelmaa on oikeus käyttää 500 kuukausittaiseen työtuntiin saakka. Työtunti tarkoittaa joko suunniteltua tai leimattua työtuntia, riippuen siitä kumpien yhteenlaskettu summa on suurempi. Työtunnit eivät sisällä matkoja eivätkä lounaita ja työtunnit lasketaan yhteen kalenterikuukausittain. </p>
+
+<p>Maksullisen Etunti-palvelun hinta määräytyy palveluun kirjattujen työtuntien kokonaismäärän mukaisesti. Ohjelman eri osien yksikköhinnat on esitetty erillisessä Toimittajan voimassa olevassa hinnastossa. Maksuperusteen jakso on kalenterikuukausi.</p>
+
+<p>Hinnat ja toimituksen sisältö yksilöidään Tilausvahvistuksessa. Mahdolliset lisätilaukset hinnoitellaan Toimittajan kulloinkin voimassa olevan hinnaston mukaisesti.</p>
+
+<p>Palvelun toimittamisen aloituspäivä määritellään Tilausvahvistuksessa. Palvelu laskutetaan kalenteri-kuukausittain, aikaisintaan laskutuskuukautta seuraavan kuukauden ensimmäisenä pankkipäivänä. Laskutus aloitetaan Etunti-verkkopalvelun käyttöönotosta tai viimeistään 3kk:n kuluttua sopimuksen allekirjoittamisesta. Hinnanmuutoksista ilmoitetaan neljä (4) kuukautta ennen uuden hinnan voimaantuloa.</p>
+
+<p>Palvelun toimittaminen</p>
+
+<p>Palvelu on toimitettu, kun Toimittaja ilmoittaa palvelun olevan käytettävissä. Palvelu katsotaan myös toimitetuksi, kun asiakas on saanut järjestelmän käyttöön oikeuttavat tunnukset.</p>
+
+<p><h3>Sopimuksen muuttaminen</h3></p>
+
+<p>Sopimuksen muuttaminen tai siirtäminen on mahdollista vain kummankin sopijaosapuolen hyväksymällä muutossopimuksella.</p>
+
+<p><h3>Palveluntoimittajan vahingonkorvaus-velvollisuus</h3></p>
+
+<p>Palveluntoimittajan vahingonkorvausvastuu palvelussa mahdollisesti ilmenevistä häiriöistä on rajoitettu välittömiin vahinkoihin, jotka ilmenevät ylimääräisinä tai hukkaan menneinä etäyhteyskustannuksina tai niihin verrattavina Asiakkaalle aiheutuneina lisäkustannuksina. Palveluntoimittaja ei siten vastaa Asiakkaalle palvelussa esiintyvien häiriöiden takia mahdollisesti aiheutuvasta taloudellisesta vahingosta kuten esimerkiksi tulon, voiton tai tuoton menetyksestä, muissa sopimussuhteissa aiheutuvista virheistä tai viivästyksistä, kolmannen osapuolen vaatimuksista taikka muusta Palveluntoimittajan kannalta vaikeasti ennakoitavasta vahingosta.</p>
+
+<p>Palveluntoimittaja ei myöskään vastaa vahingosta, joka on aiheutunut Asiakkaan antamien tietojen puutteellisuudesta tai virheellisyydestä.</p>
+
+<p>Palveluntoimittaja ei vastaa vahingosta, joka aiheutuu ylivoimaisesta esteestä, kuten sodasta tai lakosta; Palveluntoimittajasta riippumattomasta häiriöstä tiedon siirrossa, atk-järjestelmissä, sähköisessä viestinnässä, tai sähkön saannissa; tai vastaavasta syystä johtuvasta Palveluntoimittajan toiminnan kohtuuttomasta vaikeutumisesta.</p>
+
+<p>Ylivoimaisen esteen johdosta Palveluntoimittaja on oikeutettu keskeyttämään palvelun toimittaminen toistaiseksi ja jatkamaan palvelua vasta kun este poistuu. Tällöin Palveluntoimittaja on velvollinen ilmoittamaan esteestä Asiakkaalle mahdollisimman pian.</p>
+
+<p>Palveluntoimittaja ei vastaa yhteistyökumppaniensa, kolmansien osapuolten taikka myyjän alustalla palvelujaan tarjoavien osapuolten toiminnasta, palveluista tai tuotteista, eikä niiden asiakkaalle aiheuttamista välittömistä tai välillisistä vahingoista.</p>
+
+<p><h3>Huomautukset, vaatimukset</h3></p>
+
+<p>Mahdolliset palvelua, sopimusta tai toimeksiantoja koskevat huomautukset tai vaatimukset Toimittajalle tulee tehdä kirjallisesti viipymättä, kuitenkin viimeistään 14 kalenteripäivän kuluessa tapahtumapäivästä. Mikäli huomautusta ei tehdä tämän ajan kuluessa, katsotaan Asiakkaan hyväksyneen suoritetut toimenpiteet.</p>
+
+<p><h3>Palveluihin liittyvät immateriaalioikeudet</h3></p>
+
+<p>Omistusoikeus ja kaikki Immateriaalioikeudet Verkkopalveluun, mukaan lukien palveluun lisensioidut sovellukset (”Mobiili”) ovat Toimittajan omaisuutta.</p>
+
+<p>Sopimuksen mukaisten hintojen ja käyttömaksujen suorittamista vastaan Tilaaja saa ei-siirrettävissä olevan oikeuden käyttää palvelua omassa sisäisessä käytössään palvelun suunnitellussa käyttötarkoituksessa. Kohdeympäristöksi on rajattu Tilaajan oma henkilökunta.</p>
+
+<p><h3>Salassapito</h3></p>
+
+<p>Sopijapuolet pitävät toisiltaan saamansa luottamukselliseksi tai liikesalaisuudeksi katsottavan aineiston salassa eivätkä käytä tietoja muihin kuin sopimuksen mukaisiin tarkoituksiin. Salassapitovelvollisuus ei koske tietoa, joka on julkista tai yleisesti saatavilla tai jonka sopijapuoli on saanut laillisesti haltuunsa muuten kuin toiselta sopijaosapuolelta.</p>
+
+<p>Tilaaja ei saa antaa sopimuksen sisällöstä tai hinnoista tietoa ulkopuolisille ilman Toimittajan lupaa.</p>
+
+<p>Mikäli sopijaosapuoli on tahallisesti tai törkeällä huolimattomuudella luovuttanut salassa pidettäviä tietoja kolmannelle osapuolelle ja tästä sopimusrikkomuksesta aiheutuneen vahingon voidaan katsoa olevan olennainen, on sopimuksen rikkoja velvollinen suorittamaan kertakaikkisena sopimussakkona toiselle osapuolelle 10.000 euroa. Sopimussakon määrä ei rajoita loukatun osapuolen oikeutta vaatia vahingonkorvausta, mikäli vahinko ylittää sopimussakon määrän.</p>
+
+<p><h3>Erimielisyydet ja niiden ratkaiseminen</h3></p>
+
+<p>Sopimuksesta mahdollisesti syntyvät erimielisyydet ratkaistaan ensisijaisesti osapuolten välisin neuvotteluin. Mikäli neuvotteluissa ei päästä osapuolia tyydyttävään lopputulokseen, erimielisyydet jätetään ratkaistavaksi vastaajan kotipaikan yleiseen tuomioistuimeen. Mikäli sopijaosapuolet sopivat niin sopivat, asia voidaan jättää välimiesmenettelyn ratkaistavaksi.</p>
+
+<p><h3>Sovellettava laki</h3></p>
+
+<p>Tähän sopimukseen sovelletaan yksinomaan Suomen lakia.</p>
+
+<p><h3>Sopimuksen voimassaolo</h3></p>
+
+<p>Sopimus on voimassa toistaiseksi. Sopimuksen irtisanomisaika on kolme (3) kuukautta.</p>
+
