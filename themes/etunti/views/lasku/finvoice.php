2f4ecedce0d58781b9c201489adce4e04e7f15c6
Elias Luoma <elias.luoma@santelo.fi>
2018-06-13 12:37:41 +0300

    Billnum kommentonti otettu pois. Lähettää nyt ropoon laskunumeron. Testattu dev palvelimella.

--- themes/etunti/views/lasku/finvoice.php
+++ themes/etunti/views/lasku/finvoice.php
@@ -298,7 +298,7 @@ $xml = encodeXml (array(
                 "evoice" => $evoice, # verkkolaskuosoite
                 "evoiceint" => $evoiceint, # välittäjän tunnus
                 "overdueinterest" => $lasku['viivastyskorko'], # korkopros: tyhjä = oletus
-                //"billnum" => $lasku['laskunumero'], # laskun numero
+                "billnum" => $lasku['laskunumero'], # laskun numero
                 "billcode" => "", # tilitysviite tai viesti
                 "ourcode" => $lasku['viitemme'],
                 "yourcode" => $lasku['viitenne'],
