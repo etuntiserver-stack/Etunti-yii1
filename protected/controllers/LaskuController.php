7d6121786edba14bb063cfbcc0d38018158084cb
Roman Sizov <laptopsr@gmail.com>
2018-06-19 07:00:53 +0300

    autolaskutus jatkuu

--- protected/controllers/LaskuController.php
+++ protected/controllers/LaskuController.php
@@ -807,7 +807,7 @@ class LaskuController extends Controller
 		$tp = TuotteetPalvelut::model()->findbypk($id);
 		if(!isset($tp->id))
 		{
-			die 'Asiakasnumerolla: '.$asiakasnumero.' tuote ID: '.$id.' ei ole enää voimassa';
+			die('Asiakasnumerolla: '.$asiakasnumero.' tuote ID: '.$id.' ei ole enää voimassa');
 		}
 
 		if(isset($tp->id))
