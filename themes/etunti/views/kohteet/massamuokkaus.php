f9bc1ca8303a640c4ca5b6cdf11026264d8154c4
Roman Sizov <laptopsr@gmail.com>
2018-08-02 11:13:31 +0300

    massamuokkaus asiakas / historia lokiin

---
+++ themes/etunti/views/kohteet/massamuokkaus.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+        <!-- begin: .tray-center -->
+        <div class="tray-center">
+
+	   <?php     
+		$site = Yii::app()->createController('Site');
+		$site[0]->oikeudet($model->id,'noDelete');
+	   ?>
+	   <h2 class="myBgColors p10"> <i class="glyphicon glyphicon-home"></i> <?php echo Yii::t('main', 'Kohteiden hallinta'); ?>: <?php echo strtoupper(Yii::t('main', 'massamuokkaus')); ?> </h2>
+
+
+            <div class="admin-form">
+              <div class="panel heading-border">
+                <div class="panel-body bg-light">
+                 <div class="row">
+		  <?php echo $this->renderPartial('_massamuokkaus', array('model'=>$model)); ?>
+                 </div>
+                </div>
+              </div>
+            </div>
+
+        <!-- loppu: .tray-center -->
+        </div>
+
