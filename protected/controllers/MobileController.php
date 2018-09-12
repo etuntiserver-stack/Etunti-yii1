32d0b370a26a7f25318589e3b1a4d00958e9c2a9
Roman Sizov <roman@coreseer.com>
2018-09-12 09:44:51 +0300

    ET-1547

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -2246,7 +2246,10 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
 		
 
        		$criteria = new CDbCriteria();
-        	$criteria->order = "tekijan_nimi"; //"SUBSTR(LTRIM(tekijan_nimi), LOCATE(' ',LTRIM(tekijan_nimi)))"
+		// <-- Return order etu ja sukunimella
+		$site = Yii::app()->createController('Site');
+		$criteria = $site[0]->etuSukunimiCriteria($criteria);
+		//     Return order etu ja sukunimella -->
         	$criteria->condition = " aktiivinen=1 ";
 
 		// <-- Tyoryhmat
