d362e1c8a4375da17055a32dd38ba12ea4fbda44
Roman Sizov <laptopsr@gmail.com>
2018-11-06 10:22:42 +0200

    Viestinta edicossa

--- themes/etunti/views/mobile/palkkataulukko.php
+++ themes/etunti/views/mobile/palkkataulukko.php
@@ -212,7 +212,7 @@ $this->breadcrumbs=array(
 
 
   $begin = new DateTime(date("Y-m-d", strtotime($from)));
-  $end = new DateTime(date("Y-m-d", strtotime($to)));
+  $end = new DateTime(date("Y-m-d", strtotime($to." +1 day")));
   $interval = DateInterval::createFromDateString('1 day');
   $period = new DatePeriod($begin, $interval, $end);
 
