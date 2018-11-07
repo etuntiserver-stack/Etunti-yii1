91e5b8f3da4bbcaf8a5e9e8ff398784c9d37cbe3
Roman Sizov <laptopsr@gmail.com>
2018-11-07 07:57:58 +0200

    tv3

--- themes/etunti/views/tyovuoroot/did3.php
+++ themes/etunti/views/tyovuoroot/did3.php
@@ -12,6 +12,29 @@
 	$onkoMennyt = 'mennytPaivat';
 
 	$bod = '';
+	$bod .=  '
+	<div class="latikkolisatiedot_paa">
+		<div class="latikkolisatiedot">
+		 <div class="form-inline">
+			<div class="form-group">
+			   <span class="text-center" id="sum_tunnit_'.$did.'_'.$tid.'" style="text-align:center;opacity:0.6"></span>
+			</div>
+			<div class="kokopaiva form-group">
+			   <span class="valitseKokopaiva link glyphicon glyphicon-th-large" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
+			</div>
+			<div class="plussamerkki form-group">
+			   <span class="plussa link fa fa-plus luominen" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
+			</div>
+			<div class="form-group">
+		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mcut fa fa-exchange link':'forCut').'"" id="forCut_'.$did.'_'.$tid.'" style="margin-right: 5px"></i>
+		 	</div><div class="form-group">
+		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mplus fa fa-copy link':'forCopy').'" id="forCopy_'.$did.'_'.$tid.'"></i> 
+			</div>
+		 </div>
+		</div>
+	</div>
+	';
+
 	$bod .=  '<div style="position:relative" class="latikkoAsetukset '.$onkoMennyt.'" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'">';
 
        	$criteria = new CDbCriteria();
@@ -109,28 +132,6 @@
 		$loppu = $tvVal->loppu;
 	}
 	$bod .=  '</div>';
-	$bod .=  '
-	<div class="latikkolisatiedot_paa">
-		<div class="latikkolisatiedot">
-		 <div class="form-inline">
-			<div class="form-group">
-			   <span class="text-center" id="sum_tunnit_'.$did.'_'.$tid.'" style="text-align:center;opacity:0.6"></span>
-			</div>
-			<div class="kokopaiva form-group">
-			   <span class="valitseKokopaiva link glyphicon glyphicon-th-large" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
-			</div>
-			<div class="plussamerkki form-group">
-			   <span class="plussa link fa fa-plus luominen" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
-			</div>
-			<div class="form-group">
-		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mcut fa fa-exchange link':'forCut').'"" id="forCut_'.$did.'_'.$tid.'" style="margin-right: 5px"></i>
-		 	</div><div class="form-group">
-		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mplus fa fa-copy link':'forCopy').'" id="forCopy_'.$did.'_'.$tid.'"></i> 
-			</div>
-		 </div>
-		</div>
-	</div>
-	';
 
 	if($sum > 0){
 	$bod .= '
