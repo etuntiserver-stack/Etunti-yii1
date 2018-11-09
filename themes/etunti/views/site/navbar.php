b6237923f3a2522eb95ededa3f02b2873dc93f99
Roman Sizov <laptopsr@gmail.com>
2018-11-09 11:41:03 +0200

    tv3

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -319,7 +319,7 @@ if( $curpage == 'tyovuoroot/tv3' )
 
 
 	<!-- Viikonloput -->
-	<?php if($curpage == 'tyovuoroot/index' or $curpage == 'tyovuoroot/tv3') : ?>
+	<?php if($curpage == 'tyovuoroot/index') : ?>
         <li class="p10" data-toggle="tooltip">
               <div class="form-group">
                <div class="form-inline">
