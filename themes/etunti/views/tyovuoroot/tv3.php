8366a27eb22e4dd931021e7389e9030aa1346a96
Roman Sizov <laptopsr@gmail.com>
2018-11-09 12:42:38 +0200

    tv3

--- themes/etunti/views/tyovuoroot/tv3.php
+++ themes/etunti/views/tyovuoroot/tv3.php
@@ -161,7 +161,7 @@
 	    if(date('N', strtotime($date)) == 7)
 	    {
   	    echo '<tr>';
-  		echo '<td class="text-center myBgColors viikkoRivi fixed-column"><b>'.Yii::t('main', 'Viikko').' '.date("W",strtotime($date)).' <i class="fa fa-arrow-up" aria-hidden="true"></i>
+  		echo '<td class="text-center myBgColors viikkoRivi fixed-column"><b>'.date("W",strtotime($date)).' <i class="fa fa-arrow-up" aria-hidden="true"></i>
 </b></td>';
 
 		// VARAUS
