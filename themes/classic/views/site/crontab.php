47af684cfd091f34c51b510bc096088d89b94743
Roman Sizov <laptopsr@gmail.com>
2018-11-13 13:19:00 +0200

    asiakas passiviseksi pvm mukaan

--- themes/classic/views/site/crontab.php
+++ themes/classic/views/site/crontab.php
@@ -632,14 +632,12 @@
 	//    Lasku Netvisor -->
 
 	// <-- Asiakas passiviseksi paivamaaran mukaan
-/*
 	$criteria=new CDbCriteria;
 	$criteria->condition = " 
 		DATE_FORMAT(STR_TO_DATE(lopetuksen_pvm, '%d.%m.%Y'), '%Y-%m-%d') = CURDATE()
 		AND aktiivinen=1
 	";
 	Asiakkaat::model()->updateAll(array('aktiivinen'=>'0'), $criteria);
-*/
 	//     Asiakas passiviseksi paivamaaran mukaan -->
 
 
