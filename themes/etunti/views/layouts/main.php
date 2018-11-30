0afb4b630b3f512181883c54ceef038afcf21ad6
Roman Sizov <laptopsr@gmail.com>
2018-11-30 10:20:08 +0200

    user online IP muutos

--- themes/etunti/views/layouts/main.php
+++ themes/etunti/views/layouts/main.php
@@ -140,7 +140,7 @@ if(isset(Yii::app()->user->nimi))
   } else {
   $online = new UsersOnline();
   $online->time = time();
-  $online->ip = CHttpRequest::getUserHostAddress();;
+  $online->ip = (isset($_SERVER['REMOTE_ADDR']))?$_SERVER['REMOTE_ADDR']:'';
   $online->session = Yii::app()->getSession()->getSessionId();
   $online->user = Yii::app()->user->nimi;
   $online->save();
