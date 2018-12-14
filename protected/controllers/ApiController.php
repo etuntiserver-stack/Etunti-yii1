f5c175a64ee0077fe3371aadd7c733e6aea49bde
Roman Sizov <laptopsr@gmail.com>
2018-12-14 08:48:43 +0200

    erittylyt

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1218,7 +1218,7 @@ public function actionImei($dom)
 		$log_old = $mobupdate->attributes;
                 $mobupdate->loppui = date("d.m.Y H:i:s");
 
-		if( is_array($_POST['tyo_erittelyt']) and count($_POST['tyo_erittelyt']) > 0 ){
+		if( isset($_POST['tyo_erittelyt']) and is_array($_POST['tyo_erittelyt']) and count($_POST['tyo_erittelyt']) > 0 ){
 			$mobupdate->tyo_erittelyt = json_encode($_POST['tyo_erittelyt']);
 		} else {
 			$mobupdate->tyo_erittelyt = '';
