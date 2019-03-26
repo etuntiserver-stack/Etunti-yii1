883b4ac9964b818b10bf065458ffa73b01f7af13
laptopsr <laptopsr@gmail.com>
2019-03-26 14:31:43 +0200

    charts

--- themes/etunti/views/site/management.php
+++ themes/etunti/views/site/management.php
@@ -1,96 +1,96 @@
 <?php
+$months=array(
+	1=>Yii::t('main', 'Tammikuu'),
+	2=>Yii::t('main', 'Helmikuu'),
+	3=>Yii::t('main', 'Maaliskuu'),
+	4=>Yii::t('main', 'Huhtikuu'),
+	5=>Yii::t('main', 'Toukokuu'),
+	6=>Yii::t('main', 'Kesäkuu'),
+	7=>Yii::t('main', 'Heinäkuu'),
+	8=>Yii::t('main', 'Elokuu'),
+	9=>Yii::t('main', 'Syyskuu'),
+	10=>Yii::t('main', 'Lokakuu'),
+	11=>Yii::t('main', 'Marraskuu'),
+	12=>Yii::t('main', 'Joulukuu')
+	);
 
-				$mobile = 0;
-				$start_date = date ("Y-m-d"));
-				$end_date = date ("Y-m-d", strtotime($start_date. " -12 month"));
-	
-	
-				// <-- Ensin katsotaan mobile taulusta toteutuneet
-		       		$criteria = new CDbCriteria();
-		        	$criteria->select = "
-					SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
-					DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit
-				";
-			        $criteria->condition = " 
-					aloitan!='' AND loppui!=''
-					AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN  '$start_date' AND '$end_date'
-					AND status=3
-					AND id NOT IN (SELECT kid FROM sivexkuitti_repaired)
-				";
-				$lu = Mobile::model()->find($criteria);
-	
-	
-		       		$criteria = new CDbCriteria();
-		        	$criteria->select = "
-					SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
-					DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit
-				";
-			        $criteria->condition = " 
-					aloitan!='' AND loppui!=''
-					AND status=3
-					AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN  '$start_date' AND '$end_date'
-				";
-				$tot = Toteutuneet::model()->find($criteria);
-		
-				if(isset($lu->l_tunnit))
-				$mobile += $lu->l_tunnit;
-		
-				if(isset($tot->l_tunnit))
-				$mobile += $tot->l_tunnit;
+	$result = array();
+	$start_date = date ("Y-m-d", strtotime(" -1 year"));
+	$end_date = date ("Y-m-d");
 
+	$criteria = new CDbCriteria();
+       	$criteria->group = " MONTH(DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d')) ";
+       	$criteria->select = "
+		SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit, t.*
+	";
+        $criteria->condition = " 
+		aloitan!='' AND loppui!=''
+		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$start_date."' AND '".$end_date."'
+		AND status=3
+		AND sairaus!=1
+		AND deleted=0
+		AND id NOT IN (SELECT kid FROM sivexkuitti_repaired)
+	";
+	$lu = Mobile::model()->findAll($criteria);
+
+	$criteria = new CDbCriteria();
+       	$criteria->group = " MONTH(DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d')) ";
+       	$criteria->select = "
+		SUM(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')))) as l_tunnit, t.*
+	";
+        $criteria->condition = " 
+		aloitan!='' AND loppui!=''
+		AND status=3
+		AND sairaus!=1
+		AND deleted=0
+		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$start_date."' AND '".$end_date."'
+	";
+	$tot = Toteutuneet::model()->findAll($criteria);
+	$result = array_merge($lu, $tot);
+
+	$data = array();
+	$categories = array();
+	foreach($result as $item){
+		$data[] = round($item->l_tunnit/3600, 2);
+		$categories[] = $months[date("n", strtotime($item->aloitan))];
+	}
+
+//print_r($result);
 ?>
 <script src="https://code.highcharts.com/highcharts.src.js"></script>
 <div id="container"></div>
 <script>
 Highcharts.chart('container', {
-
+    chart: {
+        type: 'line'
+    },
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'Monthly Average Temperature'
     },
-
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+        text: 'Source: WorldClimate.com'
     },
-
+    xAxis: {
+        categories: JSON.parse('<?=json_encode(array_values($categories))?>')
+    },
     yAxis: {
         title: {
-            text: 'Number of Employees'
+            text: 'Temperature (°C)'
         }
     },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-
     plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
+        line: {
+            dataLabels: {
+                enabled: true
             },
-            pointStart: 2010
+            enableMouseTracking: false
         }
     },
-
     series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
-    }
-
+        name: 'Tokyo',
+        data: JSON.parse('<?=json_encode(array_values($data))?>')
+    }]
 });
 </script>
