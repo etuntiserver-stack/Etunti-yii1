c97166977971f117d61481d32bb7919859d0bfce
laptopsr <laptopsr@gmail.com>
2019-04-02 14:39:18 +0300

    tunnit

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2897,7 +2897,7 @@ $(document).ready(function(){
 	";
 	$lu = Mobile::model()->findAll($criteria);
 	foreach($lu as $item){
-		if( date("Hi", strtotime($item->aloitan)) < '0930' or date("Hi", strtotime($item->aloitan)) > '1130' ){ continue; }
+		if( date("Hi", strtotime($item->aloitan)) < date("Hi", strtotime('09:30')) or date("Hi", strtotime($item->aloitan)) > date("Hi", strtotime('11:30')) ){ continue; }
 		echo '<b>'.date("d.m.Y", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->loppui)).'</b> '.$item->kohde_kannasta.', '.$item->tekijan_nimi.'<br>';
 	}
 		exit;
