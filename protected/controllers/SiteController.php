a24eb4c97b384de0090d534a7cf6f5bcd827000a
laptopsr <laptopsr@gmail.com>
2019-04-02 14:49:14 +0300

    tunnit

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2898,10 +2898,10 @@ $(document).ready(function(){
 	$lu = Mobile::model()->findAll($criteria);
 	foreach($lu as $item){
 		if( (strtotime(date("H:i", strtotime($item->aloitan))) < strtotime('11:30')) and (strtotime(date("H:i", strtotime($item->aloitan))) > strtotime('06:30')) ){
-		echo '<b>'.date("d.m.Y", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->loppui)).'</b> '.$item->kohde_kannasta.', '.$item->tekijan_nimi.'  + 3 tuntia aloutusajaksi<br>';
+		echo date("d.m.Y", strtotime($item->aloitan)).' - <b>'.date("H:i", strtotime($item->aloitan)).'</b> - '.date("H:i", strtotime($item->loppui)).' '.$item->kohde_kannasta.', '.$item->tekijan_nimi.'  + 3 tuntia aloutusajaksi<br>';
 		}
 		if( (strtotime(date("H:i", strtotime($item->aloitan))) < strtotime('06:30'))  ){
-		echo '<b>'.date("d.m.Y", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->aloitan)).' - '.date("H:i", strtotime($item->loppui)).'</b> '.$item->kohde_kannasta.', '.$item->tekijan_nimi.'  + 1 tunti aloutusajaksi<br>';
+		echo date("d.m.Y", strtotime($item->aloitan)).' - <b>'.date("H:i", strtotime($item->aloitan)).'</b> - '.date("H:i", strtotime($item->loppui)).' '.$item->kohde_kannasta.', '.$item->tekijan_nimi.'  + 1 tunti aloutusajaksi<br>';
 		}
 	}
 		exit;
