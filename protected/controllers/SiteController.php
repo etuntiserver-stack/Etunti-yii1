17bff734a6cc5e2567b6342dba75211540b53d5b
laptopsr <laptopsr@gmail.com>
2019-04-02 14:28:32 +0300

    tunnit

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2892,18 +2892,8 @@ $(document).ready(function(){
 	Yii::app()->db1->setActive(false);
 	Yii::app()->db1->connectionString = 'mysql:host=localhost;dbname='.$_GET['domain'];
 	$criteria = new CDbCriteria();
-       	$criteria->limit = " 10 ";
-	$criteria->order = " 
-		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
-		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) DESC
-	";
-       	$criteria->select = "
-		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
-		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
-	";
         $criteria->condition = " 
-		aloitan!='' AND loppui!=''
-		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '2019-03-31' AND '2019-04-01'
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '2019-03-31' AND '2019-04-01'
 	";
 	$lu = Mobile::model()->findAll($criteria);
 	foreach($lu as $item){
