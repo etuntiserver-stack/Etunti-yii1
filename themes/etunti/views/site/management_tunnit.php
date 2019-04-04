b05747a77ec19dff774495e4884bc97806b94d02
Roman Sizov <laptopsr@gmail.com>
2019-04-04 16:14:52 +0300

    tunnit management

--- themes/etunti/views/site/management_tunnit.php
+++ themes/etunti/views/site/management_tunnit.php
@@ -1,5 +1,7 @@
 <?php
 if( isset($_GET['haku']) ){
+
+/* Isommat */
 	// <-- Luetut
 	$criteria = new CDbCriteria();
        	$criteria->limit = " 10 ";
@@ -12,9 +14,9 @@ if( isset($_GET['haku']) ){
 		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
 	";
         $criteria->condition = " 
-		aloitan!='' AND loppui!=''
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
 		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
-		AND status=3
+		AND status!=1
 		AND deleted=0
 	";
 	$lu = Mobile::model()->findAll($criteria);
@@ -22,6 +24,7 @@ if( isset($_GET['haku']) ){
 	$i=0;
 	$tl = array();
 	foreach($lu as $item){
+	if(isset($tl[$item->l_tunnit])){ continue; }
 	$tl[$item->l_tunnit] = '<b>Aloitus:</b> '.date("d.m.Y H:i", strtotime($item->aloitan)).'  <b>Lopetus:</b> '.date("d.m.Y H:i", strtotime($item->loppui)).'  <span class="text-danger">Kesto: ('.$this->sprint($item->l_tunnit).')</span> '.$this->etuSukunimi($item->tid);
 	$i++; if($i>10){break;}
 	}
@@ -40,9 +43,9 @@ if( isset($_GET['haku']) ){
 		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
 	";
         $criteria->condition = " 
-		aloitan!='' AND loppui!=''
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
 		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
-		AND status=3
+		AND status!=1
 		AND deleted=0
 		AND id NOT IN (SELECT kid FROM sivexkuitti_repaired)
 	";
@@ -60,9 +63,9 @@ if( isset($_GET['haku']) ){
 		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
 	";
         $criteria->condition = " 
-		aloitan!='' AND loppui!=''
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
 		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
-		AND status=3
+		AND status!=1
 		AND deleted=0
 	";
 	$tot = Toteutuneet::model()->findAll($criteria);
@@ -70,10 +73,90 @@ if( isset($_GET['haku']) ){
 	$i=0;
 	$tt = array();
 	foreach($result as $item){
+	if(isset($tt[$item->l_tunnit])){ continue; }
 	$tt[$item->l_tunnit] = '<b>Aloitus:</b> '.date("d.m.Y H:i", strtotime($item->aloitan)).'  <b>Lopetus:</b> '.date("d.m.Y H:i", strtotime($item->loppui)).'  <span class="text-danger">Kesto: ('.$this->sprint($item->l_tunnit).')</span> '.$this->etuSukunimi($item->tid);
 	$i++; if($i>10){break;}
 	}
 	krsort($tt);
+	//     Hyvaksytyt -->
+
+/* Pisimmat */
+	// <-- Luetut
+	$criteria = new CDbCriteria();
+       	$criteria->limit = " 10 ";
+	$criteria->order = " 
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) ASC
+	";
+       	$criteria->select = "
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
+	";
+        $criteria->condition = " 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
+		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
+		AND status!=1
+		AND deleted=0
+	";
+	$lup = Mobile::model()->findAll($criteria);
+
+	$i=0;
+	$tlp = array();
+	foreach($lup as $item){
+	if(isset($tlp[$item->l_tunnit])){ continue; }
+	$tlp[] = '<b>Aloitus:</b> '.date("d.m.Y H:i", strtotime($item->aloitan)).'  <b>Lopetus:</b> '.date("d.m.Y H:i", strtotime($item->loppui)).'  <span class="text-danger">Kesto: ('.$this->sprint($item->l_tunnit).')</span> '.$this->etuSukunimi($item->tid);
+	$i++; if($i>10){break;}
+	}
+	krsort($tlp);
+	//     Luetut -->
+
+	// <-- Hyvaksytyt
+	$criteria = new CDbCriteria();
+       	$criteria->limit = " 10 ";
+	$criteria->order = " 
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) ASC
+	";
+       	$criteria->select = "
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
+	";
+        $criteria->condition = " 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
+		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
+		AND status!=1
+		AND deleted=0
+		AND id NOT IN (SELECT kid FROM sivexkuitti_repaired)
+	";
+
+	$lup = Mobile::model()->findAll($criteria);
+
+	$criteria = new CDbCriteria();
+       	$criteria->limit = " 10 ";
+	$criteria->order = " 
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) ASC
+	";
+       	$criteria->select = "
+		TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'), 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i'))) as l_tunnit, t.*
+	";
+        $criteria->condition = " 
+		DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < DATE_FORMAT(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i')
+		AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".$from."' AND '".$to."'
+		AND status!=1
+		AND deleted=0
+	";
+	$totp = Toteutuneet::model()->findAll($criteria);
+	$result = array_merge($lup, $totp);
+	$i=0;
+	$ttp = array();
+	foreach($result as $item){
+	if(isset($ttp[$item->l_tunnit])){ continue; }
+	$ttp[] = '<b>Aloitus:</b> '.date("d.m.Y H:i", strtotime($item->aloitan)).'  <b>Lopetus:</b> '.date("d.m.Y H:i", strtotime($item->loppui)).'  <span class="text-danger">Kesto: ('.$this->sprint($item->l_tunnit).')</span> '.$this->etuSukunimi($item->tid);
+	$i++; if($i>10){break;}
+	}
+	krsort($ttp);
 	//     Hyvaksytyt -->
 }
 ?>
@@ -153,6 +236,31 @@ if( isset($_GET['haku']) ){
 	<th>
 	<?php
 	foreach($tt as $k=>$v){
+	echo $v.'<br>';
+	}
+	?>
+	</th>
+	</tr>
+	</table>
+
+	<h2><?=Yii::t('main', '10 pisimmät kestot')?></h2>
+	<table class="table table-bordered">
+	<tr>
+	<th><?=Yii::t('main', 'Luetut')?></th>
+	<th><?=Yii::t('main', 'Hväksytyt')?></th>
+	</tr>
+
+	<tr>
+	<th>
+	<?php
+	foreach($tlp as $k=>$v){
+	echo $v.'<br>';
+	}
+	?>
+	</th>
+	<th>
+	<?php
+	foreach($ttp as $k=>$v){
 	echo $v.'<br>';
 	}
 	?>
