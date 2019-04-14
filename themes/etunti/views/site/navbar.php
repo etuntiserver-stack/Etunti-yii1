94f585130f4c5afc228e7ac8f795bfd4053e48e8
Roman Sizov <laptopsr@gmail.com>
2019-04-14 14:35:05 +0300

    tyosuhteet

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -1277,14 +1277,6 @@ if($site[0]->checkEdicoViestit()){ $bell = '<i class="fa fa-bell text-danger"></
             </a>
             <ul class="nav sub-nav">
               <li>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/trl_asiakkaat">
-                  <span class="fa fa-clock-o"></span> <?php echo Yii::t('main', 'Tarjouslaskenta asiakkaat'); ?></a>
-              </li>
-              <li>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/site/etunnin_asiakkaat">
-                  <span class="fa fa-clock-o"></span> <?php echo Yii::t('main', 'Etunnin domainit'); ?></a>
-              </li>
-              <li>
                 <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/digistenTunnitKk">
                   <span class="fa fa-clock-o"></span> <?php echo Yii::t('main', 'Digisten tunnit kk'); ?></a>
               </li>
