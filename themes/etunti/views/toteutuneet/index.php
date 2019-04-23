7a94cf07c451eb1cddeb40b92a59f1e0c9070256
laptopsr <laptopsr@gmail.com>
2019-04-23 10:22:36 +0300

    arkipyha

--- themes/etunti/views/toteutuneet/index.php
+++ themes/etunti/views/toteutuneet/index.php
@@ -477,7 +477,7 @@ function dateDiff($start, $end) {
 		   <th data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Palkaton sairasloma').'">'.Yii::t('main', 'SPL').'</th>
 		   <th data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Lapsen sairaus').'">'.Yii::t('main', 'LS').'</th>
 		   <th data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Vuosiloma').'">'.Yii::t('main', 'VL').'</th>
-		   <th data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Viikkolomapäivä').'">'.Yii::t('main', 'VKL').'</th>
+		   <!--<th data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Viikkolomapäivä').'">'.Yii::t('main', 'VKL').'</th>-->
 		  </tr>
 		 </thead>
 		  <tr>
@@ -493,7 +493,7 @@ function dateDiff($start, $end) {
 		   <td><span class="allaSPL" total="'.(int)$spl.'">'.$spl.'</span></td>
 		   <td><span class="allaLS" total="'.(int)$ls.'">'.$this->sprint($ls).'</span></td>
 		   <td><span class="allaVL" total="'.(int)$vl.'">'.$vl.'</span></td>
-		   <td><span class="allaVKL" total="'.(int)$vkl.'">'.$this->sprint($vkl).'</span></td>
+		   <!--<td><span class="allaVKL" total="'.(int)$vkl.'">'.$this->sprint($vkl).'</span></td>-->
 		  </tr>';
 		 if($asetukset->netvisor_kaytto == 1 and ($asetukset->netvisor_mita_onkayttossa == 1 or $asetukset->netvisor_mita_onkayttossa == 2)) { 
 		  echo ' 
@@ -574,7 +574,7 @@ function dateDiff($start, $end) {
 		   <th>'.Yii::t('main', 'SPL').'</th>
 		   <th>'.Yii::t('main', 'LS').'</th>
 		   <th>'.Yii::t('main', 'VL').'</th>
-		   <th>'.Yii::t('main', 'VKL').'</th>
+		   <!--<th>'.Yii::t('main', 'VKL').'</th>-->
 		  </tr>
 		 </thead>
 		  <tr>
@@ -594,7 +594,7 @@ function dateDiff($start, $end) {
 		   <td class="SPLWeek_'.date("W",strtotime($date)).'">'.$yhtSPLWeek.'</td>
 		   <td class="LSWeek_'.date("W",strtotime($date)).'">'.$this->sprint($yhtLSWeek).'</td>
 		   <td class="VLWeek_'.date("W",strtotime($date)).'">'.$yhtVLWeek.'</td>
-		   <td class="VKLWeek_'.date("W",strtotime($date)).'">'.$this->sprint($yhtVKLWeek).'</td>
+		   <!--<td class="VKLWeek_'.date("W",strtotime($date)).'">'.$this->sprint($yhtVKLWeek).'</td>-->
 		  </tr>
 		</table>
 	</td></tr>';
@@ -646,7 +646,7 @@ function dateDiff($start, $end) {
 		   <th><?php echo Yii::t('main', 'SPL'); ?></th>
 		   <th><?php echo Yii::t('main', 'LS'); ?></th>
 		   <th><?php echo Yii::t('main', 'VL'); ?></th>
-		   <th><?php echo Yii::t('main', 'VKL'); ?></th>
+		   <!--<th><?php echo Yii::t('main', 'VKL'); ?></th>-->
 		  </tr>
 		 </thead>
 		  <tr>
@@ -666,7 +666,7 @@ function dateDiff($start, $end) {
 		   <td><span class="SPLFoot"><?php echo $yhtSPL; ?></span></td>
 		   <td><span class="LSFoot"><?php echo $this->sprint($yhtLS); ?></span></td>
 		   <td><span class="VLFoot"><?php echo $yhtVL; ?></span></td>
-		   <td><span class="VKLFoot"><?php echo $this->sprint($yhtVKL); ?></span></td>
+		   <!--<td><span class="VKLFoot"><?php echo $this->sprint($yhtVKL); ?></span></td>-->
 		  </tr>
 		</table>
 	</td></tr>
