04ac59dde65599b27c033b5e9b6b226922b31040
Roman Sizov <laptopsr@gmail.com>
2019-04-25 20:27:47 +0300

    kpuhsd

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -6,7 +6,7 @@
 
 //phpinfo();
 
-
+/*
 	$arr = file_get_contents('kphts.txt');
 	foreach(json_decode($arr, true) as $k => $v){
 		$as = Asiakkaat::model()->findByPk($k);
@@ -14,7 +14,7 @@
 			Asiakkaat::model()->updateByPk($as->id, array('ryhma' => $v));
 		}
 	}
-
+*/
 //echo '<pre>';
 //print_r($arr);
 //echo '</pre>';
