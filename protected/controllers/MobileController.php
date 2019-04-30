c823086a054cbe346106ab0cf3f4a2a8918b8364
laptopsr <laptopsr@gmail.com>
2019-04-30 08:43:46 +0300

    admin

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -1491,7 +1491,8 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
 		time DESC ";
 
 	        $criteria->condition = " 
-			deleted=0
+			admin!=1
+			AND deleted=0
 		";
 
 		// <-- Tyoryhmat
