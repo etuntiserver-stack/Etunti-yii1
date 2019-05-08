fce4cef057d7d0e763bdd1e9f6a326c2c3883370
laptopsr <laptopsr@gmail.com>
2019-05-08 09:08:16 +0300

    tid ei nolla

--- protected/controllers/LaskuController.php
+++ protected/controllers/LaskuController.php
@@ -143,7 +143,7 @@ class LaskuController extends Controller
 			  AND sairaus!=1
 			  AND tv_id IS NOT NULL AND tv_id > 0
 			  AND tv_id IN (
-				SELECT id FROM sivex_tvuoro WHERE (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
+				SELECT id FROM sivex_tvuoro WHERE tid!=0 AND (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
 			  )
 			)
 		    )
@@ -161,6 +161,7 @@ class LaskuController extends Controller
 			( SELECT kohde FROM sivex_tvuoro 
 			  WHERE DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') 
 			  BETWEEN '".date("Y-m-d", strtotime($from))."' AND '".date("Y-m-d", strtotime($to))."'
+			  AND tid!=0
 			  AND status='3'
 			  AND peruutettu=0
 			  AND laskutettu=0
@@ -229,7 +230,7 @@ class LaskuController extends Controller
 			AND hyvaksytty!=''
 			AND tv_id IS NOT NULL AND tv_id > 0
 			AND tv_id IN (
-				SELECT id FROM sivex_tvuoro WHERE (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
+				SELECT id FROM sivex_tvuoro WHERE tid!=0 AND (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
 			)
 			AND kohdenID IN (
 				SELECT id FROM sivex_kohdet WHERE asiakas_id='".$id."'
@@ -251,7 +252,7 @@ class LaskuController extends Controller
 			AND hyvaksytty!=''
 			AND tv_id IS NOT NULL AND tv_id > 0
 			AND tv_id IN (
-				SELECT id FROM sivex_tvuoro WHERE (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
+				SELECT id FROM sivex_tvuoro WHERE tid!=0 AND (tuoteID > 0 OR lisa_tuotteet!='') AND laskutettu='0'
 			)
 			AND kohdenID IN (
 				SELECT id FROM sivex_kohdet WHERE asiakas_id='".$id."'
@@ -275,6 +276,7 @@ class LaskuController extends Controller
 			AND kohde IN (
 				SELECT id FROM sivex_kohdet WHERE asiakas_id='".$id."'
 			)
+			AND tid!=0
 			AND status='3'
 			AND peruutettu=0
 			AND laskutettu=0
