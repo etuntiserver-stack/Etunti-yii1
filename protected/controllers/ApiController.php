65cb684988b4e2b04a5e9e5a2964d37911877c7e
Your Name <laptopsr@gmail.com>
2019-05-20 15:15:59 +0300

    korjaus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -903,7 +903,7 @@ public function actionImei($dom)
 		      $tvController = Yii::app()->createController('Tyovuoroot');
 	   	      $tilanteet = $tvController[0]->tilanteet();
 		      if( isset($tilanteet[$val->status]) and $tilanteet[$val->status] != "0" ){
-			      $sel .= '<h3 class="text-center">'. $tilanteet[$val->status].' <i class="fa fa-repeat text-success"></i></h3>';
+			      $sel .= '<h3 class="text-center">'. $tilanteet[$val->status].' '.(($val->toistuva_id != 0)?'<i class="fa fa-repeat text-success"></i>':'').'</h3>';
 		      }
 
 		      $sel .= '<h3 class="text" style="color:'.$color.'">'.$osoite.'</h3><p><b>'.$val->pvm.'</b>, '.Yii::t('main', 'Klo').': '.$alkLop.'</p>';
