3dea54b19662ff1568a3910f0c098128b2de4061
Your Name <laptopsr@gmail.com>
2019-06-10 17:21:25 +0300

    veiko

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -2,7 +2,7 @@
 if( isset($_SERVER['REMOTE_ADDR']) and ($_SERVER['REMOTE_ADDR'] == '::1' or $_SERVER['REMOTE_ADDR'] == '127.0.0.1')){
 	header("Access-Control-Allow-Origin: *");
 }
-
+		header("Access-Control-Allow-Origin: *");
 if(isset($_SERVER['HTTP_HOST'])) {
 
 print_r($_SERVER['HTTP_HOST']);
