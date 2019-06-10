442fd536b024d21fe7ee7d7873523883b327bac0
Your Name <laptopsr@gmail.com>
2019-06-10 17:36:55 +0300

    veiko

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -3,8 +3,13 @@ if( isset($_SERVER['REMOTE_ADDR']) and ($_SERVER['REMOTE_ADDR'] == '::1' or $_SE
 	header("Access-Control-Allow-Origin: *");
 }
 
+if(isset($_SERVER['HTTP_REFERER'])) {
+	$parsed = parse_url($_SERVER['HTTP_REFERER']);
+	if (isset($parsed['host']) && $parsed['host'] == 'mobemu.etunti.fi') {
 		header("Access-Control-Allow-Origin: *");
-
+	}
+	//mail('laptopsr@gmail.com', 'test', json_encode($parsed));
+}
 
 
 //echo $_SERVER['HTTP_X_USERNAME'];
@@ -270,11 +275,6 @@ public function actionLang($dom)
     switch($_GET['model'])
     {
         case 'mob':
-
-if(isset($_SERVER['HTTP_REFERER'])) {
-	$parsed = parse_url($_SERVER['HTTP_REFERER']);
-mail('laptopsr@gmail.com', 'test', json_encode($parsed));
-}
 
 		$lang = array();
 
