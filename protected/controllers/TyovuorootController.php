9d6f4268f67de94cf7e2fd61f46c079104488640
Your Name <laptopsr@gmail.com>
2019-06-12 09:12:55 +0300

    veiko

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -371,7 +371,7 @@ class TyovuorootController extends Controller
 		{
 			$model = Asiakkaat::model()->find(" yhteyshenkilo LIKE '%".Yii::app()->request->getPost('yhteyshenkilo')."' ");
 			if(isset($model->id))
-			$bd = Yii::t('main', 'Asiakas on olemassa');	
+			$bd = Yii::t('main', 'Asiakas löyty tietokannasta');	
 		}
 		echo json_encode($bd);
 		exit;
