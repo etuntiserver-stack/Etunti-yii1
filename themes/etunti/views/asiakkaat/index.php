99125c667d7b154f7242b3c5e0cdbc2e3796edb5
Your Name <laptopsr@gmail.com>
2019-06-12 07:44:57 +0300

    Erikoismerkit pois asiakas hakusta

--- themes/etunti/views/asiakkaat/index.php
+++ themes/etunti/views/asiakkaat/index.php
@@ -511,7 +511,28 @@ $(document).ready(function(){
            }
         });
  });
+ // <-- Huonot symbolit hakussa
+ $("input").keyup(function(){
+	var specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
+	var checkForSpecialChar = function(string){
+	 for(i = 0; i < specialChars.length;i++){
+	   if(string.indexOf(specialChars[i]) > -1){
+	       return true
+	    }
+	 }
+	 return false;
+	}
 
-
+	var str = $(this).val();
+	if(checkForSpecialChar(str)){
+	  alert("Tämä merkki ei sallittu");
+	  $('.haemob').addClass('disabled');
+	  $(this).addClass('bg-danger');
+	} else {
+	  $('.haemob').removeClass('disabled');
+	  $(this).removeClass('bg-danger');
+	}
+ });
+ //     Huonot symbolit hakussa -->
 });
 </script>
