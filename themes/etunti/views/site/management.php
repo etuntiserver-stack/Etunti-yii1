173322537fe152ad5b909117c222516c4d1ba4a3
Your Name <laptopsr@gmail.com>
2019-08-09 09:11:09 +0300

    kaavio

--- themes/etunti/views/site/management.php
+++ themes/etunti/views/site/management.php
@@ -586,8 +586,8 @@ ini_set("max_execution_time", "60");
                         <div class="section">
                           <label class="field select">
 			    <select name="chart_tyyppi" class="gui-input">
-			     <option value="line"><?php echo Yii::t('main', 'Line'); ?></option>
 			     <option value="column" <?=(isset($_GET['chart_tyyppi']) and $_GET['chart_tyyppi'] == 'column')?'selected':''?>><?php echo Yii::t('main', 'Column'); ?></option>
+			     <option value="line" <?=(isset($_GET['chart_tyyppi']) and $_GET['chart_tyyppi'] == 'line')?'selected':''?>><?php echo Yii::t('main', 'Line'); ?></option>
 			     <option value="bar" <?=(isset($_GET['chart_tyyppi']) and $_GET['chart_tyyppi'] == 'bar')?'selected':''?>><?php echo Yii::t('main', 'Bar'); ?></option>
 			     <option value="area" <?=(isset($_GET['chart_tyyppi']) and $_GET['chart_tyyppi'] == 'area')?'selected':''?>><?php echo Yii::t('main', 'Area'); ?></option>
 			    </select>
