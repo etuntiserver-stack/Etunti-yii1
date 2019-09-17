95fd75fe3955eb22cb9960f061360fedd25697d2
Your Name <laptopsr@gmail.com>
2019-09-17 11:00:35 +0300

    korjaus

--- protected/views/mobile/kyhteenveto.php
+++ protected/views/mobile/kyhteenveto.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('memory_limit', '512M');
-ini_set('max_execution_time', 900);
+ini_set('max_execution_time', 1900);
 /* @var $this MobileController */
 /* @var $dataProvider CActiveDataProvider */
 
