3bf090f36c6ceec56e7fa163af49d46066b05724
Your Name <laptopsr@gmail.com>
2019-09-17 13:39:57 +0300

    korjaus

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -2639,7 +2639,6 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
         	$criteria->condition = "
 			id NOT IN (select kid from sivexkuitti_repaired) 
 			AND status='3'
-			AND sairaus!=1
 			AND kohdenID='".$kohdenID."'
 			AND $fromTo
 			AND deleted=0
@@ -2662,7 +2661,6 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
         	$criteria->order = "kohde_kannasta";
         	$criteria->condition = "
 			status='3'
-			AND sairaus!=1
 			AND kohdenID='".$kohdenID."'
 			AND $fromTo
 			AND deleted=0
@@ -2970,7 +2968,6 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
         	$criteria->condition = "
 			id NOT IN (select kid from sivexkuitti_repaired) 
 			AND status='3' 
-			AND sairaus!=1
 			AND kohdenID!=''
 			AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".date('Y-m-d', strtotime($from))."' AND '".date('Y-m-d', strtotime($to))."' 
 			AND deleted=0
@@ -3008,7 +3005,6 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
         	$criteria->group = "kohdenID";
         	$criteria->condition = "
 			status='3'
-			AND sairaus!=1
 			AND kohdenID!=''
 			AND DATE_FORMAT(STR_TO_DATE(aloitan, '%d.%m.%Y'), '%Y-%m-%d') BETWEEN '".date('Y-m-d', strtotime($from))."' AND '".date('Y-m-d', strtotime($to))."' 
 			AND deleted=0
