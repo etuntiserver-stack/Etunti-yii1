d62dd1ce792e7ab348658552cece84a0b494cf3f
Your Name <laptopsr@gmail.com>
2019-09-19 09:16:30 +0300

    korjaus

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -71,11 +71,20 @@ class MobileController extends Controller
         public function init()
         {
 
-	// <-- Oikeudet
-	   $checkOikeus = "tuntienhallinta_4_".Yii::app()->user->adminStatus;
-	   $site = Yii::app()->createController('Site');
-	   $site[0]->checkOikeus($checkOikeus);
-	//  Oikeudet -->
+		if(!isset(Yii::app()->user->adminStatus))
+		{
+			//die('login error');
+		  	echo '<script type="text/javascript">
+				window.location.href=location.protocol + "//" + location.host + "/index.php/site/index";
+			</script>';
+			exit;
+		}
+
+		// <-- Oikeudet
+	   	$checkOikeus = "tuntienhallinta_4_".Yii::app()->user->adminStatus;
+	   	$site = Yii::app()->createController('Site');
+	   	$site[0]->checkOikeus($checkOikeus);
+		//  Oikeudet -->
 
                 if (Yii::app()->controller->isEtuntiAdmin() and !isset(Yii::app()->user->user_theme)) {
                         Yii::app()->theme = 'etunti';
