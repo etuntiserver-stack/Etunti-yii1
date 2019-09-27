818c05d4eed6c009f9c8a864d8a82939c889b809
Your Name <laptopsr@gmail.com>
2019-09-27 14:38:20 +0300

    tv4

--- themes/etunti/views/tyovuoroot/tv4.php
+++ themes/etunti/views/tyovuoroot/tv4.php
@@ -90,11 +90,46 @@
 	$f = date("d.m.Y", strtotime($from));
 	while (strtotime($f) <= strtotime($to)) {
 		//echo '<td>'.$f.'</td>';
-		echo '<td class="laatiko_td"><div class="latikkoAsetukset">';
+		echo '<td class="laatiko_td">';
 		foreach($item as $k=>$p){
-			if( $k == $f){
+
+
+	$did = date("Ymd", strtotime($f));
+	$onkoMennyt = '';
+	if($did < date("Ymd"))
+	$onkoMennyt = 'mennytPaivat';
+
+	$tid = $p[0]['tid'];
+	$pvm = $p[0]['pvm'];
+
+
+			if( $pvm == $f){
+	echo '
+	<div class="latikkolisatiedot_paa">
+		<div class="latikkolisatiedot">
+		 <div class="form-inline">
+			<div class="form-group">
+			   <span class="text-center" id="sum_tunnit_'.$did.'_'.$tid.'" style="text-align:center;opacity:0.6"></span>
+			</div>
+			<div class="kokopaiva form-group">
+			   <span class="valitseKokopaiva link glyphicon glyphicon-th-large" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
+			</div>
+			<div class="plussamerkki form-group">
+			   <span class="plussa link fa fa-plus luominen" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'"></span>
+			</div>
+			<div class="form-group">
+		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mcut fa fa-exchange link':'forCut').'"" id="forCut_'.$did.'_'.$tid.'" style="margin-right: 5px"></i>
+		 	</div><div class="form-group">
+		   	   <i class="'.((isset($_SESSION['muistin']) and count($_SESSION['muistin']) > 0)?'mplus fa fa-copy link':'forCopy').'" id="forCopy_'.$did.'_'.$tid.'"></i> 
+			</div>
+		 </div>
+		</div>
+	</div>
+	';
+
+			echo  '<div style="position:relative" class="latikkoAsetukset '.$onkoMennyt.'" pvm="'.date("d.m.Y",strtotime($pvm)).'" tid="'.$tid.'">';
 				foreach($p as $k1=>$p1){
-					$did = $p1['id'].'_'.date("Ymd", strtotime($p1['pvm'])).'_'.$p1['tid'];
+					$d = $p1['id'].'_'.date("Ymd", strtotime($p1['pvm'])).'_'.$p1['tid'];
 					// <-- Status
 					$status = '';
 					if($p1['status'] == 10){
@@ -122,9 +157,9 @@
 					// Toistuva -->
 
 					echo '
-					<div id="'.$did.'" class="fullRivi" style="color:#333">
+					<div id="'.$d.'" class="fullRivi" style="color:#333">
 					 <div class="pull-left ikoonintila" style="display: none;">
-					  <i class="link tvikooni fa fa-pencil-square-o muistin" for="'.$did.'" data-toggle="tooltip" data-placement="top" title="Valinta kopiontia tai siirtämistä varten"></i> 
+					  <i class="link tvikooni fa fa-pencil-square-o muistin" for="'.$d.'" data-toggle="tooltip" data-placement="top" title="Valinta kopiontia tai siirtämistä varten"></i> 
 					  '.$status.$toistuva.'
 					 </div>
 					 <span class="tv_edit" id="tv_'.$p1['id'].'">
@@ -144,9 +179,11 @@
 					 </span>
 					</div>';
 				}
+
+			echo '</div>';
 			}
 		}
-		echo '</div></td>';
+		echo '</td>';
        	        $f = date ("d.m.Y", strtotime("+1 day", strtotime($f)));
 	}
 	echo '</tr>';
