894888b91b80c8dec6ec9f56760c45a3c78a31eb
Your Name <laptopsr@gmail.com>
2019-10-01 09:27:58 +0300

    tv4

--- themes/etunti/views/tyovuoroot/tv4.php
+++ themes/etunti/views/tyovuoroot/tv4.php
@@ -66,18 +66,44 @@
            echo '<td class="laatiko_td"><div id="'.$did.'_'.$tid.'">';
            echo json_decode(head($did, $tid, $pvm, $onkoMennyt));
            if( isset($tv_arr[$tid][$f]) ){
- 	     $content = $this->renderPartial('//tyovuoroot/did4',array(
-					'tv_arr' => $tv_arr[$tid][$f],
-					'pvm'=>$pvm,
-					'did'=>$did,
-					'tid'=>$tid,
-					'from'=>'tvuoro', 
-					'kohteet_siivous'=>json_encode($kohteet_siivous), 
-					'asiakas'=>$asiakas,
-					'kohde'=>$kohde,
-					'site_0' => $site[0],
-	     ), true);
-	     echo json_decode($content);
+              $bod = '';
+              foreach($tv_arr[$tid][$f] as $tvVal){ 
+		$color = '#888';
+		$bgcol = 'color:#333';
+		// <-- Osoite
+		$osoite = $tvVal['osoite'];
+		// Osoite -->
+
+		if( isset($loppu[0]) and empty($loppu[0]) and isset($loppu[1]) and ($this->num(strtotime($tvVal['alku'])-strtotime($loppu[1])) > 0) ){
+			$valilyonti = strtotime($tvVal['alku'])-strtotime($loppu[1]);
+			$reikatyyppi = 'reika-warning';
+			$bod .= '<div class="reika '.$reikatyyppi.' text-center"><i class="glyphicon glyphicon-time"></i> Aika: '.$this->sprint($valilyonti).'</div>';
+		}
+		$kellot = '<b class="kellot">'.$tvVal['alku'].'-'.$tvVal['loppu'].'&nbsp; </b>';
+	        $muokkaus =  '<i class="link tvikooni fa fa-pencil-square-o muistin" for="'.$tvVal['id'].'_'.$did.'_'.$tid.'" data-toggle="tooltip" data-placement="top" title="'.Yii::t('main', 'Valinta kopiontia tai siirtämistä varten').'"></i>';
+		if(!empty($tvVal['tyoajanmerkinta'])){
+			$expl = explode("/",$tvVal['tyoajanmerkinta']);
+			if(isset($expl[1]) and !empty($expl[1])){
+				$color = $expl[1];
+				$bgcol = 'color:'.$color;
+			}
+		}
+		if(!empty($tvVal['tyoajanlaatu']) and empty($osoite)){
+			$expl1 = explode("/",$tvVal['tyoajanlaatu']);
+			if(isset($expl1[1]) and !empty($expl1[1])){ $color = $expl1[1]; }
+			$osoite = (isset($expl1[0])) ? '<div class="text-center"><b style="color:'.$color.'">'.$expl1[0].'</b></div>' : '';
+			$kellot = '';
+		}
+
+	   	$bod .= '<div id="'.$tvVal['id'].'_'.$did.'_'.$tid.'" class="fullRivi" style="'.$bgcol.'">';
+		$bod .= '<div class="pull-left ikoonintila" style="display:none;margin-right: 5px">'.$muokkaus.' </div>';
+		$bod .= '<span class="tv_edit" id="'.$tvVal['id'].'">';
+		$bod .= $kellot.$osoite;
+	   	$bod .= '</span>';
+	   	$bod .= '</div>';
+		$loppu = array($tvVal['tyoajanlaatu'], $tvVal['loppu']);
+              }
+	     echo $bod;
            }
            echo '</div></div></td>';
 	$f = date ("d.m.Y", strtotime("+1 day", strtotime($f)));
