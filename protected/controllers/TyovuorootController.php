baa3aefd6162cd10161a73759ef2fff9489283e5
Your Name <laptopsr@gmail.com>
2019-10-02 15:55:59 +0300

    tv4

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -1991,7 +1991,7 @@ class TyovuorootController extends Controller
 
 		if(!isset(Yii::app()->session['from']) or !isset(Yii::app()->session['to'])){
 			Yii::app()->session['from'] = date("Y-m-d");
-			Yii::app()->session['to'] = date("Y-m-d", strtotime(Yii::app()->session['from'].' Friday this week'));
+			Yii::app()->session['to'] = date("Y-m-d", strtotime(Yii::app()->session['from'].' Friday next week'));
 		}
 
 		// <-- Order tyontekijat
