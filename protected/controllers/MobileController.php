617aaca36e9a81712d02db12b9c8fccca67aeb4c
Arttu Huurinainen <arttu.huurinainen@santelo.fi>
2019-10-07 20:04:14 +0300

    Clean and re-arrange iltatunnit query

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -1927,35 +1927,41 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
 	{
 		// <-- ILTA
 		$ilta_criteria = "
-        	SUM(CASE 
-	            WHEN 
-			TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) >= TIME('18:00') 
-			&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('18:00') 
-			&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) <= TIME('23:00') 
-			   THEN TIME_TO_SEC(TIMEDIFF(TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')), TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))))
-	            WHEN 
-			TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) < TIME('18:00') 
-			&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('18:00') 
-			&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) <= TIME('23:00') 
-			   THEN TIME_TO_SEC(TIMEDIFF(TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')), TIME('18:00')))
-	            WHEN 
-			TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) >= TIME('18:00') 
-			&& 
-			(
-			  TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('23:00')
-			  || DATE(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) != DATE(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))
-			)
-			   THEN TIME_TO_SEC(TIMEDIFF(TIME('23:00'), TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))))
-	            WHEN 
-			TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) < TIME('18:00') 
-			&& 
-			(
-			  TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('23:00')
-			  || DATE(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) != DATE(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))
-			)
-			   THEN TIME_TO_SEC(TIMEDIFF(TIME('23:00'), TIME('18:00')))
-	            ELSE 0
-	        END) AS l_tunnit
+			SUM(CASE
+			WHEN
+				TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) >= TIME('18:00')
+			THEN
+				CASE
+				WHEN
+					TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('18:00')
+					&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) <= TIME('23:00')
+				THEN
+					TIME_TO_SEC(TIMEDIFF(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s'), STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')))
+				WHEN
+					TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('23:00')
+					|| DATE(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) != DATE(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))
+				THEN
+					TIME_TO_SEC(TIMEDIFF(TIME('23:00'), STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')))
+				ELSE 0
+				END
+			WHEN
+				TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) < TIME('18:00')
+			THEN
+				CASE
+				WHEN
+					TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('18:00')
+					&& TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) < TIME('23:00')
+				THEN
+					TIME_TO_SEC(TIMEDIFF(TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')), TIME('18:00')))
+				WHEN
+					TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) > TIME('23:00')
+					|| DATE(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) != DATE(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s'))
+				THEN
+					TIME_TO_SEC(TIMEDIFF(TIME('23:00'), TIME('18:00')))
+				ELSE 0
+				END
+			ELSE 0
+			END) AS l_tunnit
 		";
 		//     ILTA -->
 
