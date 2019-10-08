b5e8f2544270495d60f4c4bfe7eba53026048d37
Arttu Huurinainen <arttu.huurinainen@santelo.fi>
2019-10-08 12:41:54 +0300

    Add case 3 (sunnuntai)

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -2044,6 +2044,24 @@ time <= date_sub(NOW(), interval 3 hour) AND status IN (1,2,10) AND loppui='' DE
 						END) AS l_tunnit";
 					break;
 				case 3:
+					$criteria->select = "tid, SUM(CASE
+						WHEN
+							DAYOFWEEK(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')) = 1
+						THEN CASE
+							WHEN
+								DAYOFWEEK(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) = 1
+							THEN
+								TIME_TO_SEC(TIMEDIFF(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s'), STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')))
+							ELSE
+								TIME_TO_SEC(TIMEDIFF('23:59:00', TIME(STR_TO_DATE(aloitan, '%d.%m.%Y %H:%i:%s')))) + 60
+							END
+						WHEN
+							DAYOFWEEK(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')) = 1
+						THEN
+							TIME_TO_SEC(TIMEDIFF(TIME(STR_TO_DATE(loppui, '%d.%m.%Y %H:%i:%s')), '00:01')) + 60
+						ELSE
+							0
+						END) AS l_tunnit";
 					break;
 			}
 
