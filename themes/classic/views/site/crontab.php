1129740fed242f6543a963e253c6957dedd6f66b
Your Name <laptopsr@gmail.com>
2019-10-17 08:42:51 +0300

    crontab

--- themes/classic/views/site/crontab.php
+++ themes/classic/views/site/crontab.php
@@ -518,6 +518,5 @@ foreach ($list as $d) {
 	Asiakkaat::model()->updateAll(array('aktiivinen' => '0'), $criteria);
 	//     Asiakas passiviseksi paivamaaran mukaan -->
 
-	echo "end loop \n";
 	unset($_SESSION['domain']);
 }
