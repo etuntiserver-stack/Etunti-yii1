00f706e362e6ceccd5aeb7204f2b4a952b4e83d4
Your Name <laptopsr@gmail.com>
2019-10-24 10:54:24 +0300

    henri

--- themes/etunti/views/tyosopimukset/index.php
+++ themes/etunti/views/tyosopimukset/index.php
@@ -182,7 +182,7 @@
 			 	echo '<tr><td>';
 
 				// <-- file_safe_opener
-				$filepath = Yii::getPathOfAlias('application').'/../'.$file;
+				$filepath = Yii::getAlias('@app').'/'.$file;
 echo $filepath;
 				echo CHtml::link(end($explNimi),
 					array('/site/file_safe_opener', 'filepath' => $filepath, 'ext' => 'docx'),
