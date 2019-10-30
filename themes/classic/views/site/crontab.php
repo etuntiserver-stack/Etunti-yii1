8b9d8637a08ce745eae6b6385f19144fe1d80dff
Your Name <laptopsr@gmail.com>
2019-10-30 09:45:40 +0200

    crontab

--- themes/classic/views/site/crontab.php
+++ themes/classic/views/site/crontab.php
@@ -43,6 +43,7 @@ foreach ($list as $d) {
 	$domainit = Domainit::model()->findByPk($d->id);
 
 	// <-- Tuntien Autohyvaksyminen
+/*
 	if ($asetukset->app_hyvaksynnan_peruste == 2) {
 		$date_yday = date('Y-m-d', strtotime('-1 day'));
 		$criteria = new CDbCriteria();
@@ -60,6 +61,7 @@ foreach ($list as $d) {
 			}
 		}
 	}
+*/
 	//    Tuntien Autohyvaksyminen -->
 
 	// <-- maksullinen versio
@@ -103,6 +105,7 @@ foreach ($list as $d) {
 	//     maksullinen versio -->
 
 	// <-- Ilmoitus määräajan ylittäneistä kohteista
+/*
 	$criteria = new CDbCriteria;
 	$criteria->condition = " 
 		DATE_FORMAT(STR_TO_DATE(CONCAT(pvm,loppu), '%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i') < (NOW() - INTERVAL $aikavali_halytys MINUTE)
@@ -177,9 +180,11 @@ foreach ($list as $d) {
 			//     Sahkopostin lahetys -->
 		}
 	}
+*/
 	// Ilmoitus määräajan ylittäneistä kohteista -->
 
 	// <-- ilmoitus_myohastyneista_kohteesta
+/*
 	$criteria = new CDbCriteria;
 	$criteria->condition = " 
 		DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') = CURDATE()
@@ -262,6 +267,7 @@ foreach ($list as $d) {
 			//     Sahkopostin lahetys -->
 		}
 	}
+*/
 	// ilmoitus_myohastyneista_kohteesta -->
 
 	// <-- merkkipaivailmoitukset
