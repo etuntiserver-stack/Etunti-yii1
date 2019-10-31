6ca896ca251f72d65bae414dc0205be5e60232f3
Your Name <laptopsr@gmail.com>
2019-10-31 13:23:00 +0200

    palvelukota

--- themes/etunti/views/toteutuneet/index.php
+++ themes/etunti/views/toteutuneet/index.php
@@ -457,6 +457,8 @@ exit;
     echo '</td>';
 
     echo '<td id="'.$did.'_'.$tid.'" ilman_lounastaukot="'.$ilman_lounastaukot.'" ilman_matkat="'.$ilman_matkat.'">';
+print_r($toteutuneet_laatikot[$date]);
+exit;
     if(isset($toteutuneet_laatikot[$date])){
     	echo '<div class="small">';
 	   foreach($toteutuneet_laatikot[$date] as $item)
