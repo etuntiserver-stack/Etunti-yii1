c189a038967c813aea9901b1492b0a813cceb7aa
Your Name <laptopsr@gmail.com>
2019-10-31 13:38:03 +0200

    palvelukota

--- protected/controllers/ToteutuneetController.php
+++ protected/controllers/ToteutuneetController.php
@@ -691,6 +691,8 @@ $xml = '
 		foreach($get as $k=>$v){
 		      $laatikot[date("d.m.Y",strtotime($k))][] = $this->renderPartial('al',array('attributes'=>$v), true);
 		}
+echo '<textarea class="form-control">'.print_r($laatikot).'</textarea>';
+exit;
 	        return json_encode($laatikot);
 
 	}
--- themes/etunti/views/toteutuneet/index.php
+++ themes/etunti/views/toteutuneet/index.php
@@ -325,8 +325,7 @@ $dateDiff = dateDiff($from, $to);
 
   $luetut_laatikot = json_decode($this->LuetutPvmTidBetween($from,$to,$tid), true);
   $toteutuneet_laatikot = json_decode($this->TotPvmTidBetween($from,$to,$tid), true);
-echo '<textarea class="form-control">'.print_r($this->TotPvmTidBetween($from,$to,$tid)).'</textarea>';
-exit;
+
   $vuosilomachecker	= $this->vuosilomaCheckerBetween($from, $to, $tid);
   $hyvaksymmattomat_t	= $this->hyvaksyttamatTunnitBetween($from, $to, $tid);
 
@@ -458,7 +457,6 @@ exit;
     echo '</td>';
 
     echo '<td id="'.$did.'_'.$tid.'" ilman_lounastaukot="'.$ilman_lounastaukot.'" ilman_matkat="'.$ilman_matkat.'">';
-
     if(isset($toteutuneet_laatikot[$date])){
     	echo '<div class="small">';
 	   foreach($toteutuneet_laatikot[$date] as $item)
