e7eb4a6d184deb8dc77f4a56420c987f29a0c9de
Your Name <laptopsr@gmail.com>
2019-11-01 20:29:30 +0200

    Henry

--- themes/etunti/views/lasku/luolaskut.php
+++ themes/etunti/views/lasku/luolaskut.php
@@ -273,7 +273,7 @@ $iban				= $asetukset->iban;
 			if( isset($mob['alv']) ){ $alv = $mob['alv']; }
 			if( isset($r['yksikko']) ){ $yksikko = $r['yksikko']; }
 			if( isset($mob['yksikko']) ){ $yksikko = $mob['yksikko']; }
-			if( isset($mob['freetext']) ){ $freetext = $mob['freetext']; }
+			//if( isset($mob['freetext']) ){ $freetext = $mob['freetext']; }
 
 			// <-- TV
 			if( isset($tyovuoroot['tuoteID']) ){ $tp_id = $tyovuoroot['tuoteID']; }
@@ -284,11 +284,11 @@ $iban				= $asetukset->iban;
 			// <-- MOB
 			if( isset($mob_tunnit['aloitan']) and isset($mob_tunnit['kohde_kannasta']) ){
 				if(isset($_GET['viestikenta']) and in_array('pvm', $_GET['viestikenta'])){
-					$freetext .= date("d.m.Y", strtotime($mob_tunnit['aloitan']));
+					//$freetext .= date("d.m.Y", strtotime($mob_tunnit['aloitan']));
 				}
 				if(isset($_GET['viestikenta']) and in_array('osoite', $_GET['viestikenta'])){
 					if(!empty($freetext)){ $freetext .= ', '; }
-					$freetext .= $mob_tunnit['kohde_kannasta'];
+					//$freetext .= $mob_tunnit['kohde_kannasta'];
 				}
 			}
 			// <-- TV
