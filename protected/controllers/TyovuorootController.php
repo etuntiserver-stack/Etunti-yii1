1b3b4e0721f84ac3a24788d35f3ce232b3382edd
Your Name <laptopsr@gmail.com>
2019-11-05 11:04:08 +0200

    autolaskutus

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -2308,7 +2308,7 @@ class TyovuorootController extends Controller
 				if(isset($expl1[1]) and !empty($expl1[1])){ $color = $expl1[1]; }
 				$arvo['osoite'] = (isset($expl1[0])) ? '<b class="tv_edit" id="'.$arvo['id'].'" style="color:'.$color.'">'.$expl1[0].'</b>' : '';
 			} else {
-				$arvo['osoite'] = '<span class="tv_edit" id="'.$arvo['id'].'" style="'.$bgcol.'">'.$status[$arvo['status']].''.$arvo['alku'].'-'.$arvo['loppu'].'<br> '.$osoite.'</span>';
+				$arvo['osoite'] = '<span class="tv_edit" id="'.$arvo['id'].'" style="'.$bgcol.'">'.((isset($status[$arvo['status']]))?$status[$arvo['status']]:'').''.$arvo['alku'].'-'.$arvo['loppu'].'<br> '.$osoite.'</span>';
 			}
 			$tv_arr[$arvo->tid][$arvo->pvm][] = $arvo['osoite'];
 		}
