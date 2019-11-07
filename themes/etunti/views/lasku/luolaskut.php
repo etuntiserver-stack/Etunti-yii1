154fe57e917eda50b8b696b90148cfd2f1f56fc1
Your Name <laptopsr@gmail.com>
2019-11-07 09:09:35 +0200

    henri

--- themes/etunti/views/lasku/luolaskut.php
+++ themes/etunti/views/lasku/luolaskut.php
@@ -311,7 +311,7 @@ $iban				= $asetukset->iban;
 			$yht 		= 0;
 			if(!is_numeric($hinta) or !is_numeric($kpl) or !is_numeric($alv)){
 				echo 'Hinta: '.$hinta.', ALV: '.$alv.'<br>'; 
-				echo '<div class="alert alert-danger"><h1>Hinta tai ALV ei saa olla teksti muodossa tai pilkulla.</h1></div>';
+				echo '<div class="alert bg-danger"><h1>Hinta tai ALV ei saa olla teksti muodossa tai pilkulla.</h1></div>';
 				break;
 			}
 			if( $alvsis == 0 ){
