ba755bb5ca47d7db6030dc5dadc071023ad10b85
Roman Sizov <laptopsr@gmail.com>
2019-11-18 14:34:17 +0200

    korjaus

--- themes/etunti/views/toistuvatTyovuorot/korjaus.php
+++ themes/etunti/views/toistuvatTyovuorot/korjaus.php
@@ -1,18 +1,13 @@
 <?php
 // AIzaSyBR8t3Xq2pvDlRvNBdwUFBYFGrUAuMWblI
-$k = Kohteet::model()->findbypk(10);
-		$asetuksetForAll = AsetuksetForAll::model()->findByPk(1);
-		$kartta = '';
-		if(isset($asetuksetForAll->googlemaps_apikey) and !empty($asetuksetForAll->googlemaps_apikey)){
-			$full_addr = $k->osoite.' '.$k->pnumero.' '.$k->kaupunki;
-			$json_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($full_addr).'&language=fi&sensor=false&key='.$asetuksetForAll->googlemaps_apikey;
-			$json = file_get_contents($json_url);
-			$obj = json_decode($json);
-			if( isset($obj->results[0]->geometry->location->lat) ){
-				$kartta = '<p><a href="geo:'.$obj->results[0]->geometry->location->lat.",".$obj->results[0]->geometry->location->lng.'">'.Yii::t('main', 'Näytä kartalla').'</a></p>';
-			}
-		}
-echo $json;
+$_POST['email'] = 'laptopsr@gmail.com';
+$_POST['salasana'] = 'KristinA1';
+
+$adm_salasana = password_hash($_POST['salasana'], PASSWORD_BCRYPT);
+if (password_verify('KristinA1', $adm_salasana))
+echo 'true';
+
+
 exit;
 /*
 $site = Yii::app()->createController('Site');
