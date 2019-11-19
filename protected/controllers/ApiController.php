db4a78444183746f9f6ff663068574dc29f313ea
Roman Sizov <laptopsr@gmail.com>
2019-11-19 12:59:52 +0200

    korjaus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -746,8 +746,8 @@ public function actionImei($dom)
 			$body = '<h2>'.Yii::t('app', 'Henkilökortti').'</h2>';
 			$filepath = dirname(Yii::app()->getBasePath())."/img/tekijat/".$dom."/".$ttekija->id.".jpg";
 			$body .= '<div class="well">';
+			$body .= '<p><img src="'.$asetukset->logon_polkku.'" height="50px"></p>';
 			$body .= '<div class="row"><div class="col-xs-8">';
-			$body .= '<img src="'.$asetukset->logon_polkku.'" height="50px">';
 			$body .= '<h3>'.$firma->yritys.'</h3>';
 			$body .= '<p>Y-tunnus: '.$firma->y_tunnus.'</p>';
 			$body .= $ttekija->tekijan_nimi.' '.$ttekija->sukunimi;
