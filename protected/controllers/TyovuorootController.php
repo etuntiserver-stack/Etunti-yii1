889921c9685e7503093d778b0889f8d2c9d4dbe7
Roman Sizov <laptopsr@gmail.com>
2020-01-30 11:17:51 +0200

    toistuva jatkuu

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -523,7 +523,7 @@ class TyovuorootController extends Controller
 		$status = "status='".implode("' OR status='", $status)."'";
 
 		// Tässä on uusi muoto työvuorojen saamisestä, jossa on arrayissa kaikki yhteensä ( tavalliset ja Viertualiset )
-		// Mä en voi anta tv_arr funktiolle $criteria->select joka saisimme tässä funktiossa, koska se vaikuttaa pelka Tavalliselle työvuoroille
+		// Mä en voi anta tv_arr funktiolle $criteria->select joka saisimme vanhassa funktiossa, koska se vaikuttaa pelka Tavalliselle työvuoroille
 		// tv_arr funktio on tehty kahdesta osasta, tavalliset ja Virtualiset
 		// Jos laita $criteria->select toistuville, niin tulos olisi pelkä 1 rivistä ja EI koko ketjusta. Tämä on tärkeä asia mistä olen taistelemassa
 		// Tärkeä!  tv_arr funktio on käytössä nyt monessa paikassa
