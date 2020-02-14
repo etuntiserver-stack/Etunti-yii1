fb3ede931d7c5d082142be926c7e53ea3f962b4f
Roman Sizov <laptopsr@gmail.com>
2020-02-14 11:16:58 +0200

    toistuva jatkuu

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -1305,15 +1305,12 @@ class TyovuorootController extends Controller
 			id NOT IN(select toistuva_id from sivex_tvuoro where toistuva_id!=0)
 		";
 		$ts = ToistuvatTyovuorot::model()->find($criteria);
-		if( isset($ts->id) ){
-			$tvr = ToistuvatTyovuorot::model()->findAll($criteria);
-			foreach($tvr as $item){
-				ToistuvatTyovuorot::model()->deletebypk($item->id);
+			if( isset($ts->id) ){
+				ToistuvatTyovuorot::model()->deleteAll($criteria);
+				echo 'STAGE 1 - korjattu '.count($tvr).' kpl<br>';
+				echo CHtml::link('<h4>Seuraava</h4>', array('beta', 'mode' => $mode, 'stage' => 2));
+				exit;
 			}
-			echo 'STAGE 1 - korjattu '.count($tvr).' kpl<br>';
-			echo CHtml::link('<h4>Seuraava</h4>', array('beta', 'mode' => $mode, 'stage' => 2));
-			exit;
-		}
 		}
 		//     CLEAR puhdista turhat  ketjut -->
 		if( $stage == 2 ){
