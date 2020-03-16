ea144aaac83819bb95a36e1de080cfa88ab82aec
Roman Sizov <laptopsr@gmail.com>
2020-03-16 08:18:31 +0200

    korjaus

--- protected/controllers/AsiakkaatController.php
+++ protected/controllers/AsiakkaatController.php
@@ -912,7 +912,7 @@ Yritys '.$yr.'
 	$context = stream_context_create($optsPOST);
 	
 	$response = file_get_contents($url, false, $context);
-	$result = new SimpleXMLElement($response);
+	$result = new \SimpleXMLElement($response);
 	
 
 		//if($result->ResponseStatus->Status == 'OK')
