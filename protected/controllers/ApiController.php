a8168913f65745258d37470b8a315593eab53809
Roman Sizov <laptopsr@gmail.com>
2020-03-26 21:56:22 +0200

    korjaus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -305,8 +305,6 @@ public function actionAsetukset($dom)
     switch($_GET['model'])
     {
         case 'mob':
-print_r($_POST);
-exit;
 	if(isset($_POST['tid']) ){
 		$ttekija = Tyontekijat::model()->findByPk($_POST['tid']);
 	}
