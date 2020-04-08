63166d8cee4637103100129c00f5a8f5282590e7
Roman Sizov <laptopsr@gmail.com>
2020-04-08 11:42:04 +0300

    korjaus

--- protected/config/main.php
+++ protected/config/main.php
@@ -15,7 +15,6 @@ $is_production = in_array($server_name, ['app.etunti.fi', 'etunti.com']);
 $get_domain = trim(strtolower($_GET['dom'] ?? $_GET['domain'] ?? ''));
 
 // <-- Redirect Domain; app.etunti.fi|etunti.com => apps.etunti.fi
-/*
 if ($is_production) {
     if ($get_domain == 'demo') {
         header("Access-Control-Allow-Origin: *");
@@ -43,7 +42,6 @@ if ($is_production) {
         exit;
     }
 }
-*/
 //    Redirect Domain -->
 
 if (
