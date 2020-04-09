a71fb4f1ef0c7b1ba4f464d9013aa4165ced7a60
Roman Sizov <laptopsr@gmail.com>
2020-04-09 08:37:47 +0300

    management avataan

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -1322,7 +1322,6 @@ if($site[0]->checkEdicoViestit()){ $bell = '<i class="fa fa-bell text-danger"></
           </li>
 	<?php endif; ?>
 
-	<?php if( Yii::app()->user->domain == 'sivex' or Yii::app()->user->domain == 'demo' ): ?>
           <li>
             <a class="accordion-toggle" href="#">
               <span class="fa fa-line-chart"></span>
@@ -1347,7 +1346,6 @@ if($site[0]->checkEdicoViestit()){ $bell = '<i class="fa fa-bell text-danger"></
 	<?php */ ?>
             </ul>
           </li>
-	<?php endif; ?>
 
 	<?php if(in_array('999',$tas)) : ?>
           <li>
