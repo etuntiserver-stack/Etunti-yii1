742b9312daf286306117718329f932d2c2dfaa29
Roman Sizov <laptopsr@gmail.com>
2020-04-23 10:35:38 +0300

    toistuva jatkuu

--- themes/etunti/views/tyovuoroot/vko.php
+++ themes/etunti/views/tyovuoroot/vko.php
@@ -182,9 +182,5 @@ if( isset($_SESSION['skrollaus']) )
         </div>
 </div>
 
-<?php
-echo 'TERVE Veiko';
-exit;
-?>
 <!-- Täytetään tauluu -->
 <?php echo $this->tv_arrJava($from, $to, $haku_criteria, $haku_tids, 'vko'); ?>
