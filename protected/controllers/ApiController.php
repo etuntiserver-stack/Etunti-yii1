6a490ef27c6d388e85688ca2aefa4bfdad2dd388
Roman Sizov <laptopsr@gmail.com>
2020-05-05 20:21:25 +0300

    toistuva jatkuu

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1441,6 +1441,9 @@ public function actionImei($dom)
 			$mobupdate->tyo_erittelyt = '';
 		}
 
+		$this->_sendResponse(200, CJSON::encode($_POST));
+		exit;
+
 		$vanhaViesti = '';
 		if($mobupdate->viesti != '')
 		$vanhaViesti = $mobupdate->viesti."\n";
