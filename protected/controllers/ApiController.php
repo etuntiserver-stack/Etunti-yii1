17b359ca3d7e43bba86b85a54c758d492aa8b581
Roman Sizov <laptopsr@gmail.com>
2020-05-05 20:43:02 +0300

    korjaus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1445,8 +1445,8 @@ public function actionImei($dom)
 		$log_old = $mobupdate->attributes;
                 $mobupdate->loppui = date("d.m.Y H:i:s");
 
-		if( isset($_POST['tyo_erittelyt']) and is_array($_POST['tyo_erittelyt']) and count($_POST['tyo_erittelyt']) > 0 ){
-			$mobupdate->tyo_erittelyt = json_encode($_POST['tyo_erittelyt']);
+		if( isset($_POST['tyo_erittelyt']) and !is_array($_POST['tyo_erittelyt']) and !empty($_POST['tyo_erittelyt']) ){
+			$mobupdate->tyo_erittelyt = $_POST['tyo_erittelyt'];
 		} else {
 			$mobupdate->tyo_erittelyt = '';
 		}
