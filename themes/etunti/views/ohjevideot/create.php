9a29d78f8cc913f88d504c82c17972f109e6f104
Roman Sizov <laptopsr@gmail.com>
2020-05-08 10:55:16 +0300

    toistuva jatkuu

---
+++ themes/etunti/views/ohjevideot/create.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+        <!-- begin: .tray-center -->
+        <div class="tray-center">
+
+	   <h2 class="myBgColors p10"> <i class="fa fa-file-video-o"></i> <?php echo Yii::t('main', 'Luo video'); ?> </h2>
+
+
+            <div class="admin-form">
+              <div class="panel heading-border">
+                <div class="panel-body bg-light">
+                 <div class="row">
+		  <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+                 </div>
+                </div>
+              </div>
+            </div>
+
+        <!-- loppu: .tray-center -->
+        </div>
+
