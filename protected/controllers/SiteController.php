a3da0c71364b8e61619375252ef9f531bb4d92ca
Roman Sizov <laptopsr@gmail.com>
2020-05-11 10:32:03 +0300

    viestit

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2111,7 +2111,7 @@ $(document).ready(function(){
 		$criteria = new CDbCriteria();
         	$criteria->condition = "
 			status=3 AND tekija='toimisto'
-			AND DATE(STR_TO_DATE(time, '%Y-%m-%d')) = CURDATE()
+			AND DATE(time) = CURDATE()
 		";
 	  	$v = Viestinta::model()->findAll($criteria);
 	  	$viestit = count($v);
