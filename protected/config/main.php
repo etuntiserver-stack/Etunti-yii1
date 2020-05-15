0ff2a24204aa6a0524faf35ec7f89eab2c1d8d62
Roman Sizov <laptopsr@gmail.com>
2020-05-15 13:48:18 +0300

    bro_workers siirto

--- protected/config/main.php
+++ protected/config/main.php
@@ -16,7 +16,7 @@ $actual_link	= (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
 $path 		= parse_url($actual_link);
 $oinlinevaraus 	= ( isset($path['path']) and strpos($path['path'], "onlinevaraus") !== false )? true: false;
 
-$siirto_domainit = ['demo','sivex'];
+$siirto_domainit = ['demo', 'sivex', 'bro_workers'];
 
 // <-- Redirect Domain; app.etunti.fi|etunti.com => apps.etunti.fi
 if ($is_production and empty($_FILES) and !$oinlinevaraus) {
