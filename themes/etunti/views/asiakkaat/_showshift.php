57dda094796822a366efc02b6e3e88090e6ef42a
Roman Sizov <laptopsr@gmail.com>
2020-05-22 14:27:52 +0300

    showshitf

--- themes/etunti/views/asiakkaat/_showshift.php
+++ themes/etunti/views/asiakkaat/_showshift.php
@@ -3,15 +3,15 @@
 	$k=Kohteet::model()->findbypk($data->kohde);
 	if(isset($k->id)) $osoite = $k->osoite; else $osoite = '';
 
-	$tt=Tyontekijat::model()->findbypk($data->tid);
+	$tt=Tyontekijat::model()->findbypk($this_tid);
 	if(isset($tt->id)) $tekijan_nimi = $this->etuSukunimi($tt->id); else $tekijan_nimi = '';
 
 
-  	$path = Yii::app()->basePath.'/../img/tekijat/'.Yii::app()->user->domain.'/'. $data->tid.'.jpg';
+  	$path = Yii::app()->basePath.'/../img/tekijat/'.Yii::app()->user->domain.'/'. $this_tid.'.jpg';
   	if (file_exists($path) and isset($_POST['tulosta'])){
-		$img =  '<img src="img/tekijat/'.Yii::app()->user->domain.'/'. $data->tid.'.jpg" height="30">';
+		$img =  '<img src="img/tekijat/'.Yii::app()->user->domain.'/'. $this_tid.'.jpg" height="30">';
   	} elseif (file_exists($path) and !isset($_POST['tulosta'])){
-		$img =  '<img src="'.Yii::app()->request->baseUrl.'/img/tekijat/'.Yii::app()->user->domain.'/'. $data->tid.'.jpg" height="60">
+		$img =  '<img src="'.Yii::app()->request->baseUrl.'/img/tekijat/'.Yii::app()->user->domain.'/'. $this_tid.'.jpg" height="60">
 			<p>'.$tekijan_nimi.'</p>';
 	} else {
 		$img =  $tekijan_nimi;
@@ -22,7 +22,7 @@
 	echo '<tr class="'.(($data->peruutettu != 0)? 'text-danger':'').'">';
 	echo '<td>
 	'.CHtml::link('<i class="fa fa-pencil-square-o" aria-hidden="true" style="font-size: 110%"></i>', 
-				array('/tyovuoroot/beta?mode=vko&year='.date("Y", strtotime($data->pvm)).'&week='.date("W", strtotime($data->pvm)).'&tv_id='.$this_id), 
+				array('/tyovuoroot/beta?mode=vko&year='.date("Y", strtotime($this_pvm)).'&week='.date("W", strtotime($this_pvm)).'&tv_id='.$this_id), 
 				array(
 					'class'=>'btn btn-primary myBgColors', 
 					'style'=>'color:white', 
@@ -33,7 +33,7 @@
 				)
 			).
 	'</td>';
-	echo '<td class="col1">'.$data->pvm.'</td>';
+	echo '<td class="col1">'.$this_pvm.'</td>';
 	echo '<td class="col2">'.$data->alku.'-'.$data->loppu.'</td>';
 	echo '<td class="col1">'.$this->sprint($kesto).'</td>';
 	echo '<td class="col3">'.$osoite.'</td>';
