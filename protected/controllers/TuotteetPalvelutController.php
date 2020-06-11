45de04b00aa59666e616208596e065caa81d0b28
Roman Sizov <laptopsr@gmail.com>
2020-06-11 10:23:57 +0300

    Henrin pyynto

--- protected/controllers/TuotteetPalvelutController.php
+++ protected/controllers/TuotteetPalvelutController.php
@@ -46,24 +46,9 @@ class TuotteetPalvelutController extends Controller
 	}
 
 	public function isEtuntiAdmin() {
-
-		$tas = '';
-		if(isset(Yii::app()->user->adminPaketti))
-		$tas = explode(",",Yii::app()->user->adminPaketti);
-
-		if(isset(Yii::app()->user->adminID) and in_array('3',$tas))
-		{
-		   $m = Administrators::model()->findbypk(Yii::app()->user->adminID);
-	       	   if($m->id == Yii::app()->user->adminID)
-		   {
+		$m = Administrators::model()->findbypk(Yii::app()->user->adminID);
+		if($m->id == Yii::app()->user->adminID)
 			return true;
-		   } else {
-			$this->redirect(array('/site/otakaytoon', 'tila' => 'lasku'));
-		   }		
-
-		} else {
-			$this->redirect(array('/site/otakaytoon', 'tila' => 'lasku'));
-		}
 	}
 
         public function init()
