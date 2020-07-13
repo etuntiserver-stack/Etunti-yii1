0404081e9f7f0bde3f07ce65108020d96d2c56d5
dxo <2art@pm.me>
2020-07-13 18:19:31 +0300

    Remove test variables from debug session and prep for testing (probably fixed)

--- protected/controllers/TyovuorootController.php
+++ protected/controllers/TyovuorootController.php
@@ -3355,11 +3355,6 @@ class TyovuorootController extends Controller
 		//     Luodaan yksittainen toistuvasta. -->
 
     // <-- Toistuva Alkamispaiva siirto.
-    if ($toistuva) {
-      $test = $post['pfromNew'];
-      $test2 = strtotime($post['pfromNew']);
-      $test3 = strtotime($laatikko_pvm);
-    }
 		if( 
 			// $toistuva and isset($edellinen_model['id']) and isset($post['is_toistuva'])
 			// and strtotime($edellinen_model['pfrom']) != strtotime($laatikko_pvm) 
