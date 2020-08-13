60024ee6267c70d24a1e472caa7a25c7f69d7fa2
dxo <2art@pm.me>
2020-08-14 01:14:50 +0300

    Disable YII_DEBUG while in production enviroment

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/YII/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+// check if we're in development (local) environment.
+$development_environment = in_array($_SERVER['REMOTE_ADDR'] ?? null, ['::1', '127.0.0.1'])
+                        && strpos($_SERVER['HTTP_REFERER'] ?? null, "api/mob") === false;
 
-// remove the following lines when in production mode
-/*
-if( 
-	($_SERVER['REMOTE_ADDR'] == '::1' 
-	or $_SERVER['REMOTE_ADDR'] == '127.0.0.1' 
-	)
-	and (isset($_SERVER['HTTP_REFERER']) and strpos($_SERVER['HTTP_REFERER'], "api/mob") === false )
-)
-{
-	defined('YII_DEBUG') or define('YII_DEBUG',true);
-} else {
-	defined('YII_DEBUG') or define('YII_DEBUG',false);
-}
-*/
-	defined('YII_DEBUG') or define('YII_DEBUG',true);
-	//defined('YII_DEBUG') or define('YII_DEBUG',false);
+// enable debug mode in development environments.
+defined('YII_DEBUG') or define('YII_DEBUG', $development_environment);
 
-// specify how many levels of call stack should be shown in each log message
+// how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+// run application - change the following paths if necessary
+$yii=dirname(__FILE__).'/YII/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 require_once($yii);
 Yii::createWebApplication($config)->run();
