ed9ac463eaea20ae1c53daf4fd51bab29961f856
Roman Sizov <laptopsr@gmail.com>
2020-08-20 18:29:18 +0300

    verotusprosentti

--- themes/etunti/views/asetukset/_form.php
+++ themes/etunti/views/asetukset/_form.php
@@ -662,6 +662,13 @@ $('.ryhmat').multiselect({
 	<div class="section fill mb5">
 		<?php $redirect = Yii::createComponent('Procountor')->getRedirectUri(); ?>
 		<p><?php echo CHtml::link('Kirjaudu Procountoriin', "https://api.procountor.com/login?response_type=code&client_id=etuntiClient&redirect_uri=$redirect&state=" .strtolower(Yii::app()->user->domain), ['class' => 'btn btn-lg btn-primary myBgColors']); ?></p>
+		<?php 
+			/* Testi tunnukset */ 
+			//	etunti.test
+			//	Elias2011!
+
+			//<p><?php echo CHtml::link('Kirjaudu Procountoriin TEST', "https://api-test.procountor.com/login?response_type=code&client_id=etuntiTestClient&redirect_uri=$redirect&state=" .strtolower(Yii::app()->user->domain), ['class' => 'btn btn-lg btn-primary myBgColors']); ?></p>
+		?>
 		<!-- https://api-test.procountor.com/login?response_type=code&client_id=etuntiTestClient&redirect_uri=$redirect&state=" . strtolower(Yii::app()->user->domain) -->
 	</div>
 	<?php
