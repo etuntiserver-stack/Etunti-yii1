3b579f8ab75102261ea058a437df088487b9a403
Roman Sizov <roman@coreseer.com>
2020-09-23 14:20:24 +0300

    pilkut palkkataulukkoon

--- themes/etunti/views/mobile/_palkkataulukko.php
+++ themes/etunti/views/mobile/_palkkataulukko.php
@@ -52,21 +52,21 @@
 
 	<td class="tulostus_tekija col1"><?=$data->$tt_order_1.' '.$data->$tt_order_2?></td>
 	<td class="col2"><?php if($tp != 0) echo $tp; ?></td>
-	<td class="col3" style="vnd.ms-excel.numberformat:0.0;"><?php if($this->num($matkatunnit) != 0) echo $this->num($matkatunnit); ?></td>
-	<td class="col4" style="vnd.ms-excel.numberformat:0.0;"><?php if($tyotunnit != 0) echo $this->num($tyotunnit); ?></td>
-	<td class="col5" style="vnd.ms-excel.numberformat:0.0;"><?php echo $sum; ?></td>
+	<td class="col3"><?php if($this->num($matkatunnit) != 0) echo number_format($this->num($matkatunnit), 2, ',', ''); ?></td>
+	<td class="col4"><?php if($tyotunnit != 0) echo number_format($this->num($tyotunnit), 2, ',', ''); ?></td>
+	<td class="col5"><?php echo number_format($sum, 2, ',', ''); ?></td>
 	<td class="col6"><?php echo $iltatunnit_tyotjamatkat.$matkaIlta; ?></td>
-	<td class="col7" style="vnd.ms-excel.numberformat:0.0;"><?php echo $this->num($iltatunnit_ja_iltamatka); ?></td>
-	<td class="col8" style="vnd.ms-excel.numberformat:0.0;"><?php echo $this->num($loun); ?></td>
-	<td class="col8" style="vnd.ms-excel.numberformat:0.0;"><?=$this->num($yotunnit)?></td>
-	<td class="col9" style="vnd.ms-excel.numberformat:0.0;"><?php if($sutunnit != 0) echo $sutunnit; ?></td>
-	<td class="col10" style="vnd.ms-excel.numberformat:0.0;"><?php echo $this->num($pyhat); ?></td>
-	<td class="col10" style="vnd.ms-excel.numberformat:0.0;"><?php echo $this->num($el); ?></td>
-	<td class="col10" style="vnd.ms-excel.numberformat:0.0;"><?php if($sl > 0) echo $sl; ?></td>
-	<td class="col11" style="vnd.ms-excel.numberformat:0.0;"><?php if($spl > 0) echo $spl; ?></td>
-	<td class="col12" style="vnd.ms-excel.numberformat:0.0;"><?php if($ls > 0) echo $ls; ?></td>
-	<td class="col12" style="vnd.ms-excel.numberformat:0.0;"><?php if($vl > 0) echo $vl; ?></td>
-	<td class="col12" style="vnd.ms-excel.numberformat:0.0;"><?php if($vkl > 0) echo $vkl; ?></td>
+	<td class="col7"><?php echo number_format($this->num($iltatunnit_ja_iltamatka), 2, ',', ''); ?></td>
+	<td class="col8"><?php echo number_format($this->num($loun), 2, ',', ''); ?></td>
+	<td class="col8"><?=number_format($this->num($yotunnit), 2, ',', '')?></td>
+	<td class="col9"><?php if($sutunnit != 0) echo number_format($sutunnit, 2, ',', ''); ?></td>
+	<td class="col10"><?php echo number_format($this->num($pyhat), 2, ',', ''); ?></td>
+	<td class="col10"><?php echo number_format($this->num($el), 2, ',', ''); ?></td>
+	<td class="col10"><?php if($sl > 0) echo number_format($sl, 2, ',', ''); ?></td>
+	<td class="col11"><?php if($spl > 0) echo number_format($spl, 2, ',', ''); ?></td>
+	<td class="col12"><?php if($ls > 0) echo number_format($ls, 2, ',', ''); ?></td>
+	<td class="col12"><?php if($vl > 0) echo number_format($vl, 2, ',', ''); ?></td>
+	<td class="col12"><?php if($vkl > 0) echo number_format($vkl, 2, ',', ''); ?></td>
 	<td class="col13"><?php echo $korv; ?></td>
 	<td class="col14"><?php echo $lisatt; ?></td>
 	<td class="col15"><?php echo $ennakko; ?></td>
