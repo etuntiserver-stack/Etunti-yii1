a78f20d2bfe24300bf7408838c5f024d5ef541c7
Roman Sizov <roman@coreseer.com>
2020-10-01 13:36:56 +0300

    etaisyys real

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1685,7 +1685,6 @@ public function actionImei($dom)
 			if(isset($tvuoro->id)){
 				// GPS sijainti
 				$asetuksetForAll = AsetuksetForAll::model()->findByPk(1);
-$mobinsert->app_aloitus_destination_checker = $my_location;
 				if(
 					!empty($my_location) and isset($tvuoro->kohteet->gps_sijainti) 
 					and isset($asetuksetForAll->googlemaps_apikey) and !empty($asetuksetForAll->googlemaps_apikey)
@@ -1697,6 +1696,8 @@ $mobinsert->app_aloitus_destination_checker = $my_location;
 		        		$data = json_decode($json);
 				        if (isset($data->status) and $data->status == "OK")
 					        $mobinsert->app_aloitus_destination_checker = $json;
+					else
+					        $mobinsert->app_aloitus_destination_checker = 'ERROR: '. $json;
 				}
 
 				// <-- Timer
