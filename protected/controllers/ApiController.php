0423b233b319ff3c4bc484a7d56df2667283f20a
Roman Sizov <roman@coreseer.com>
2020-10-01 15:01:00 +0300

    etaisyys real

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1554,6 +1554,7 @@ public function actionImei($dom)
 
 		// <-- GPS checker
 		if($mobupdate->tv_id > 0){
+$mobinsert->app_lopetus_destination_checker = $my_location;
 			$tvuoro = Tyovuoroot::model()->findByPk($mobupdate->tv_id);
 			if(isset($tvuoro->id)){
 				$dist = $this->DestinationChecker($my_location, $tvuoro, $asetuksetForAll);
