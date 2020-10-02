c914db39d649c71d7aa6f59522135e8ebdb25943
Roman Sizov <roman@coreseer.com>
2020-10-02 09:02:43 +0300

    korjaus

--- themes/etunti/views/mobile/index_a.php
+++ themes/etunti/views/mobile/index_a.php
@@ -5,7 +5,7 @@
   <th><?php echo Yii::t('main', 'T'); ?></th>
   <th><?php echo Yii::t('main', 'Versio / GPS'); ?></th>
   <th><?php echo Yii::t('main', 'Päivä'); ?></th>
-  <th data-toggle="tooltip" title="Tästä näkyy aloitus ja lopetus etäisyydet kilometri tarkuudella suunnitteluista työvuoroista.."><?php echo Yii::t('main', '<i class="fa fa-2x fa-question-circle"></i>'); ?></th>
+  <th class="text-center" data-toggle="tooltip" title="Tästä näkyy aloitus ja lopetus etäisyydet kilometri tarkuudella suunnitteluista työvuoroista.."><?php echo Yii::t('main', '<i class="fa  fa-question-circle"></i>'); ?></th>
   <th><?php echo Yii::t('main', 'Työntekijä'); ?></th>
   <th><?php echo Yii::t('main', 'Er.'); ?></th>
 
