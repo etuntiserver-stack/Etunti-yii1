3e6dd39e1a1131caa23767783b962a8791427618
Roman Sizov <roman@coreseer.com>
2020-10-02 09:11:22 +0300

    korjaus

--- themes/etunti/views/mobile/_view.php
+++ themes/etunti/views/mobile/_view.php
@@ -165,7 +165,10 @@ if( isset($sivu) and $sivu == 'laskutettu' )
 	<td class="text-center">
 		<?php echo $karttaA." ".$karttaL; ?>
 		<?php if($data->tv_id > 0) : ?>
-		<br><?=(!empty($data->app_aloitus_destination_checker))? $data->app_aloitus_destination_checker : '' ?><?=(!empty($data->app_lopetus_destination_checker))? '| ' .$data->app_lopetus_destination_checker : ''?>
+		<table class="table table-bordered">
+		<td><?=(empty($data->app_aloitus_destination_checker))? '0' : $data->app_aloitus_destination_checker?></td>
+		<td><?=(empty($data->app_lopetus_destination_checker))? '0' : $data->app_lopetus_destination_checker?></td>
+		</table>
 		<?php endif; ?>
 	</td>
 
