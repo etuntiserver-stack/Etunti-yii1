7e2f792e8076bb1785f5f19bc022f7807658e1db
Roman Sizov <roman@coreseer.com>
2020-10-02 09:56:04 +0300

    korjaus

--- themes/etunti/views/mobile/_view.php
+++ themes/etunti/views/mobile/_view.php
@@ -163,7 +163,10 @@ if( isset($sivu) and $sivu == 'laskutettu' )
 	<td class="text-center"><?=$versio?><br>GPS:<?=$data->gps_enabled?><br><?=$data->app_platform?></td>
 	<td><b><?php echo CHtml::encode(date("d.m.Y",strtotime($data->aloitan))); ?></b></td>
 	<td class="text-center">
-		<div data-toggle="tooltip" data-placement="top" title="Näytä karttalla"><?= $karttaA." ".$karttaL ?></div>
+		<div><?= 
+			'<span data-toggle="tooltip" data-placement="top" title="Aloitus karttalla">'.$karttaA.'</span>
+			<span data-toggle="tooltip" data-placement="top" title="Lopetus karttalla">'.$karttaL 
+		?></div>
 		<?php 
 		if(
 			$data->tv_id > 0 
