d77ec95cb73005e98ad51cfd09b32f10934ef3d4
Roman Sizov <roman@coreseer.com>
2020-10-06 14:14:54 +0300

    test

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -244,6 +244,10 @@ public function actionPaivita_tiedot($dom)
 		{
 			$token = $_POST['token'];
 			Tyontekijat::model()->updatebypk($ttekija->id, array('gcm_reg_id'=>$token));
+
+			if(isset($_POST['app_platform']))
+				Tyontekijat::model()->updatebypk($ttekija->id, array('app_platform' => $_POST['app_platform']));
+
         		$this->_sendResponse(200, CJSON::encode('token updated'));
 		} else {
         		$this->_sendResponse(200, CJSON::encode('tyontekija error'));
