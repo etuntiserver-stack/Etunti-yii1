c3172c499a9747380a74b12f05e8672c72f2aa92
Roman Sizov <roman@coreseer.com>
2020-10-06 17:06:04 +0300

    sovellus

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -718,6 +718,10 @@ public function actionImei($dom)
 				}
 				//     Version checker -->
 
+				if( $dom == 'demo' ){
+					$ilmoitus_kaikkille .=  '<a class="btn btn-warning btn-block" href="https://play.google.com/store/apps/details?id=fi.etunti.local&gl=FI">'.Yii::t('app', 'Päivitä sovellus').'</a>'
+				}
+
 				$return = [
 					"tid" => $ttekija->id,
 					"ilmoitus_kaikkille" => $ilmoitus_kaikkille,
