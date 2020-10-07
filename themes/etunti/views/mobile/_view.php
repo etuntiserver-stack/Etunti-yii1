602644f48b302f7066db36e90f72754de770132c
Roman Sizov <roman@coreseer.com>
2020-10-07 09:24:46 +0300

    tunnit sivu

--- themes/etunti/views/mobile/_view.php
+++ themes/etunti/views/mobile/_view.php
@@ -172,6 +172,13 @@ if( isset($sivu) and $sivu == 'laskutettu' )
 	</td>
 	<td><b><?php echo CHtml::encode(date("d.m",strtotime($data->aloitan))); ?></b></td>
 	<td class="text-center">
+
+		<div class="row">
+		<table class="table" width="100%">
+		<tr>
+		<td style="padding: 3px 5px" width="50%"><span data-toggle="tooltip" data-placement="top" title="Aloitus karttalla"><?=$karttaA?></span></td>
+		<td style="padding: 3px 5px" width="50%"><span data-toggle="tooltip" data-placement="top" title="Lopetus karttalla"><?=$karttaL?></span></td>
+		</tr>
 		<?php 
 		if(
 			$data->tv_id > 0 
@@ -191,19 +198,16 @@ if( isset($sivu) and $sivu == 'laskutettu' )
 			if($data->app_lopetus_destination_checker > 3 and $data->app_lopetus_destination_checker < 100)
 				$varoitus_lp = 'text-danger';
 
-		echo '<div class="row">
-			<table class="table" width="100%">
+		echo '<tr>
 			<td class="'.$varoitus_al.'" style="padding: 3px 5px" width="50%">
-				<span data-toggle="tooltip" data-placement="top" title="Aloitus karttalla">'.$karttaA.'</span>
 				<b data-toggle="tooltip" title="Aloitus kilometri määrä">'.((empty($data->app_aloitus_destination_checker) or $data->app_aloitus_destination_checker == 0)? '0.00' : $data->app_aloitus_destination_checker).'</b>
 			</td>
 			<td class="'.$varoitus_lp.'" style="padding: 3px 5px" width="50%">
-				<span data-toggle="tooltip" data-placement="top" title="Lopetus karttalla">'.$karttaL.'</span>
 				<b data-toggle="tooltip" title="Lopetus kilometri määrä">'.((empty($data->app_lopetus_destination_checker) or $data->app_lopetus_destination_checker == 0)? '0.00' : $data->app_lopetus_destination_checker).'</b>
-			</td>
-			</table></div>';
+			</td></tr>';
 		}
 		?>
+		</table></div>
 	</td>
 
 	<td>
