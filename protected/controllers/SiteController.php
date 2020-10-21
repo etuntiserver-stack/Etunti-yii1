a578480039ec6be8455e29160a925c7d5e0cddb8
Roman Sizov <laptopsr@gmail.com>
2020-10-21 15:51:06 +0300

    etunti

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -486,7 +486,7 @@ class SiteController extends Controller
 		if($mob_result < $tyovuorot_result)
 			$result = $tyovuorot_result;
 
-		return $this->num($mob_result);
+		return $this->num($result);
 	}
 
 	public function actionTyot_tanaan()
