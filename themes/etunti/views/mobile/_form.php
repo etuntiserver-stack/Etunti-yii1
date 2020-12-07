a345811a83b24263cda8ef0ded61a670a5d357a3
Roman Sizov <laptopsr@gmail.com>
2020-12-07 09:02:48 +0200

    laskurivi

--- themes/etunti/views/mobile/_form.php
+++ themes/etunti/views/mobile/_form.php
@@ -220,7 +220,7 @@ if(isset($model->tietoja))
 		</div>
 		<div class="section">
 			<?php echo $form->labelEx($model,'laskurivi_id'); ?>
-			<?php echo $form->textField($model,'laskurivi_id',array('class'=>'form-control', 'readonly' => 'yes')); ?>
+			<?php echo $form->textField($model,'laskurivi_id',array('class'=>'form-control')); ?>
 			<?php echo $form->error($model,'laskurivi_id'); ?>
 		</div>
 	</div>
