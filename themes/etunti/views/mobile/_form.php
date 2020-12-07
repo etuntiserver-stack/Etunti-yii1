069d3730d4e891d2a28f8f66f0b8af4adfa02223
Roman Sizov <laptopsr@gmail.com>
2020-12-07 09:01:55 +0200

    laskurivi

--- themes/etunti/views/mobile/_form.php
+++ themes/etunti/views/mobile/_form.php
@@ -55,26 +55,26 @@ if(isset($model->tietoja))
                     <p class="lead">
                         <strong><?php echo $model->tekijan_nimi; ?></strong>
                     </p>
+		            <ul class="list-group list-group-flush text-center">
+		                <li class="list-group-item minheight">
+		                    <?php echo $form->labelEx($model,'kohde_kannasta'); ?>
+		                    <strong><?php echo $perusTiedot['0']; ?></strong>
+		                </li>
+		                <li class="list-group-item minheight">
+		                    <?php echo $form->labelEx($model,'aloitan'); ?>
+		                    <strong><?php echo $perusTiedot['1']; ?></strong>
+		                </li>
+		                <li class="list-group-item minheight">
+		                    <?php echo $form->labelEx($model,'loppui'); ?>
+		                    <strong><?php echo $perusTiedot['2']; ?></strong>
+		                </li>
+		                <li class="list-group-item"  style="height:172px">
+		                    <?php echo $form->labelEx($model,'viesti'); ?>
+							<br>
+		                    <strong><?php echo str_replace("\n","<br>",$model->viesti); ?></strong>
+		                </li>
+		            </ul>
                 </div>
-                <ul class="list-group list-group-flush text-center">
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'kohde_kannasta'); ?>
-                        <strong><?php echo $perusTiedot['0']; ?></strong>
-                    </li>
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'aloitan'); ?>
-                        <strong><?php echo $perusTiedot['1']; ?></strong>
-                    </li>
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'loppui'); ?>
-                        <strong><?php echo $perusTiedot['2']; ?></strong>
-                    </li>
-                    <li class="list-group-item"  style="height:172px">
-                        <?php echo $form->labelEx($model,'viesti'); ?>
-			<br>
-                        <strong><?php echo str_replace("\n","<br>",$model->viesti); ?></strong>
-                    </li>
-                </ul>
             </div>
         </div>
 
@@ -87,26 +87,26 @@ if(isset($model->tietoja))
                     <p class="lead">
                         <strong><?php echo $model->tekijan_nimi; ?></strong>
                     </p>
+					<ul class="list-group list-group-flush text-center">
+						<li class="list-group-item minheight">
+							<?php echo $form->labelEx($model,'kohde_kannasta'); ?>
+							<strong><?php echo $kohde_kannasta; ?></strong>
+						</li>
+						<li class="list-group-item minheight">
+							<?php echo $form->labelEx($model,'aloitan'); ?>
+							<strong><?php echo $aloitan; ?></strong>
+						</li>
+						<li class="list-group-item minheight">
+							<?php echo $form->labelEx($model,'loppui'); ?>
+							<strong><?php echo $loppui; ?></strong>
+						</li>
+						<li class="list-group-item"  style="height:172px">
+							<?php echo $form->labelEx($model,'viesti'); ?>
+							<br>
+							<strong><?php echo str_replace("\n","<br>",$model->viesti); ?></strong>
+						</li>
+					</ul>
                 </div>
-                <ul class="list-group list-group-flush text-center">
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'kohde_kannasta'); ?>
-                        <strong><?php echo $kohde_kannasta; ?></strong>
-                    </li>
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'aloitan'); ?>
-                        <strong><?php echo $aloitan; ?></strong>
-                    </li>
-                    <li class="list-group-item minheight">
-                        <?php echo $form->labelEx($model,'loppui'); ?>
-                        <strong><?php echo $loppui; ?></strong>
-                    </li>
-                    <li class="list-group-item"  style="height:172px">
-                        <?php echo $form->labelEx($model,'viesti'); ?>
-			<br>
-                        <strong><?php echo str_replace("\n","<br>",$model->viesti); ?></strong>
-                    </li>
-                </ul>
             </div>
         </div>
 
@@ -130,115 +130,109 @@ if(isset($model->tietoja))
 
 
 
-  <?php if(Yii::app()->user->adminStatus == 1) : ?>
-  <div class="row">
-    <div class="col-sm-4">
+<?php if(Yii::app()->user->adminStatus == 1) : ?>
+<div class="row">
+	<div class="col-sm-4">
+		<div class="section">
+			<?php echo $form->labelEx($model,'gps_enabled'); ?>
+			<?php echo $form->textField($model,'gps_enabled',array('class'=>'form-control')); ?>
+			<?php echo $form->error($model,'gps_enabled'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'app_aloitus_destination_checker'); ?>
+			<?php echo $form->textField($model,'app_aloitus_destination_checker',array('class'=>'form-control')); ?>
+			<?php echo $form->error($model,'app_aloitus_destination_checker'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'app_lopetus_destination_checker'); ?>
+			<?php echo $form->textField($model,'app_lopetus_destination_checker',array('class'=>'form-control')); ?>
+			<?php echo $form->error($model,'app_lopetus_destination_checker'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'app_platform'); ?>
+			<?php echo $form->textField($model,'app_platform',array('class'=>'form-control')); ?>
+			<?php echo $form->error($model,'app_platform'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'tid'); ?>
+			<?php
+				$site = Yii::app()->createController('Site');
+				$list = $site[0]->tyontekiatArrayList(1); 
+				echo $form->dropDownList($model, 'tid', $list, array('class'=>'form-control')); ?>
+			<?php echo $form->hiddenField($model,'tekijan_nimi',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+			<?php echo $form->error($model,'tid'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'kohdenID'); ?>
+			<?php echo $form->dropDownList($model, 'kohdenID', CHtml::listData(Kohteet::model()->findAll(array('order'=>'osoite')), 'id', 'osoite'), array('empty'=>'Valitse', 'class'=>'form-control')); ?>
+			<?php echo $form->hiddenField($model,'kohde_kannasta',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+			<?php echo $form->error($model,'kohdenID'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'status'); ?>
+			<?php 
+				  $list = array(3=>Yii::t('main','TYÖ'),2=>Yii::t('main','MATKA'),10=>Yii::t('main','LOUNASTAUKO'));
+				  //array_unshift($list, $list[$s->status]);
+				  echo $form->dropDownList($model,'status', 
+				 	$list, 
+					array('class'=>'form-control'));
+			?>
+			<?php echo $form->error($model,'status'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'aloitan'); ?>
+			<?php echo $form->textField($model,'aloitan',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+			<?php echo $form->error($model,'aloitan'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'loppui'); ?>
+			<?php echo $form->textField($model,'loppui',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
+			<?php echo $form->error($model,'loppui'); ?>
+		</div>
 
-	<div class="section">
-		<?php echo $form->labelEx($model,'gps_enabled'); ?>
-		<?php echo $form->textField($model,'gps_enabled',array('class'=>'form-control')); ?>
-		<?php echo $form->error($model,'gps_enabled'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'app_aloitus_destination_checker'); ?>
-		<?php echo $form->textField($model,'app_aloitus_destination_checker',array('class'=>'form-control')); ?>
-		<?php echo $form->error($model,'app_aloitus_destination_checker'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'app_lopetus_destination_checker'); ?>
-		<?php echo $form->textField($model,'app_lopetus_destination_checker',array('class'=>'form-control')); ?>
-		<?php echo $form->error($model,'app_lopetus_destination_checker'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'app_platform'); ?>
-		<?php echo $form->textField($model,'app_platform',array('class'=>'form-control')); ?>
-		<?php echo $form->error($model,'app_platform'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'tid'); ?>
-		<?php
-			$site = Yii::app()->createController('Site');
-			$list = $site[0]->tyontekiatArrayList(1); 
-			echo $form->dropDownList($model, 'tid', $list, array('class'=>'form-control')); ?>
-		<?php echo $form->hiddenField($model,'tekijan_nimi',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'tid'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'kohdenID'); ?>
-		<?php echo $form->dropDownList($model, 'kohdenID', CHtml::listData(Kohteet::model()->findAll(array('order'=>'osoite')), 'id', 'osoite'), array('empty'=>'Valitse', 'class'=>'form-control')); ?>
-		<?php echo $form->hiddenField($model,'kohde_kannasta',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'kohdenID'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'status'); ?>
-		<?php 
-		      $list = array(3=>Yii::t('main','TYÖ'),2=>Yii::t('main','MATKA'),10=>Yii::t('main','LOUNASTAUKO'));
-		      //array_unshift($list, $list[$s->status]);
-		      echo $form->dropDownList($model,'status', 
-			 	$list, 
-				array('class'=>'form-control'));
-		?>
-		<?php echo $form->error($model,'status'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'aloitan'); ?>
-		<?php echo $form->textField($model,'aloitan',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'aloitan'); ?>
-	</div>
-	<div class="section">
-		<?php echo $form->labelEx($model,'loppui'); ?>
-		<?php echo $form->textField($model,'loppui',array('size'=>20,'maxlength'=>20,'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'loppui'); ?>
-	</div>
+		<?php if( isset($model->id) ): ?>
+		<div class="section">
+			<?php echo $form->labelEx($model,'tv_id'); ?>
+			<?php
+		   		$criteria = new CDbCriteria();
+		   		$criteria->condition = " 
+				DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') 
+				BETWEEN '".date("Y-m-d", strtotime($model->aloitan))."' AND '".date("Y-m-d", strtotime($model->aloitan))."'
+			";
+			?>
+			<?php echo $form->dropDownList($model, 'tv_id', CHtml::listData(Tyovuoroot::model()->findAll($criteria), 'id', 'osoiteAndAika'), array('empty'=>'Valitse', 'class'=>'form-control')); ?>
+			<?php echo $form->error($model,'tv_id'); ?>
+		</div>
+		<?php endif; ?>
 
-	<?php if( isset($model->id) ): ?>
-	<div class="section">
-		<?php echo $form->labelEx($model,'tv_id'); ?>
-		<?php
-       		$criteria = new CDbCriteria();
-       		$criteria->condition = " 
-			DATE_FORMAT(STR_TO_DATE(pvm, '%d.%m.%Y'), '%Y-%m-%d') 
-			BETWEEN '".date("Y-m-d", strtotime($model->aloitan))."' AND '".date("Y-m-d", strtotime($model->aloitan))."'
-		";
-		?>
-		<?php echo $form->dropDownList($model, 'tv_id', CHtml::listData(Tyovuoroot::model()->findAll($criteria), 'id', 'osoiteAndAika'), array('empty'=>'Valitse', 'class'=>'form-control')); ?>
-		<?php echo $form->error($model,'tv_id'); ?>
 	</div>
-	<?php endif; ?>
-
-	<br>
-	<legend>Laskutus tilanne</legend>
-	<div class="section">
-		<?php echo $form->labelEx($model,'laskutettu'); ?>
-		<?php 
-		      $list = array(0 => Yii::t('main','Ei laskutettu'), 1 => Yii::t('main','Laskutettu'));
-		      echo $form->dropDownList($model,'laskutettu', 
-			 	$list, 
-				array('class'=>'form-control'));
-		?>
-		<?php echo $form->error($model,'laskutettu'); ?>
+	<div class="col-sm-4">
+		<legend>Laskutus tilanne</legend>
+		<div class="section">
+			<?php echo $form->labelEx($model,'laskutettu'); ?>
+			<?php 
+				  $list = array(0 => Yii::t('main','Ei laskutettu'), 1 => Yii::t('main','Laskutettu'));
+				  echo $form->dropDownList($model,'laskutettu', 
+				 	$list, 
+					array('class'=>'form-control'));
+			?>
+			<?php echo $form->error($model,'laskutettu'); ?>
+		</div>
+		<div class="section">
+			<?php echo $form->labelEx($model,'laskurivi_id'); ?>
+			<?php echo $form->textField($model,'laskurivi_id',array('class'=>'form-control', 'readonly' => 'yes')); ?>
+			<?php echo $form->error($model,'laskurivi_id'); ?>
+		</div>
 	</div>
-	
-	<div class="section">
-		<?php echo $form->labelEx($model,'laskurivi_id'); ?>
-		<?php echo $form->textField($model,'laskurivi_id',array('class'=>'form-control', 'readonly' => 'yes')); ?>
-		<?php echo $form->error($model,'laskurivi_id'); ?>
-	</div>
+</div>
 <br>
-
-	<div class="section">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Luo' : 'Tallenna', array('class'=>'btn btn-primary')); ?>
-	</div>
-    </div>
-  </div>
-  <?php endif; ?>
-
-
-
-
+<div class="section">
+	<?php echo CHtml::submitButton($model->isNewRecord ? 'Luo' : 'Tallenna', array('class'=>'btn btn-primary')); ?>
+</div>
+<?php endif; ?>
 <?php $this->endWidget(); ?>
 
-</div><!-- form -->
+</div><!--form-->
 
 
 
