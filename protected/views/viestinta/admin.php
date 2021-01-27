bb1459f510b4b6043b9bf4512934927a19a528b8
Roman Sizov <laptopsr@gmail.com>
2021-01-27 13:01:08 +0200

    Users varten muutos

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -172,7 +172,7 @@ if(isset($_GET['users_siirto']))
 		$all_users[strtolower($d->domain)] = $merge;
 	}
 	
-	$ongelmat = [];
+	//$ongelmat = [];
 	
 	if(count($ongelmat) == 0)
 	{
@@ -204,14 +204,14 @@ if(isset($_GET['users_siirto']))
 			$profiles[] = $arr['Profile'];
 		}
 
-		
+		/*
 		// Poistetaan ensin kaikki
 		Yii::app()->db->createCommand()->delete('user');
 		Yii::app()->db->createCommand()->delete('profile');
 		$builder 	= Yii::app()->db->schema->commandBuilder;
 		$builder->createMultipleInsertCommand('user', $users)->execute();
 		$builder->createMultipleInsertCommand('profile', $profiles)->execute();
-		
+		*/
 		
 		echo 'Yhteensä '.$yhteensa.'<br>';
 		echo 'tehty_hash: '.$tehty;
