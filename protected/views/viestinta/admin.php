e18058f7bc267b55bd398fa82c13756d3b208e01
Roman Sizov <laptopsr@gmail.com>
2021-03-02 17:26:39 +0200

    siirto

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -188,22 +188,19 @@ if(isset($_GET['users_siirto']))
 				{
 					foreach($all_users[$sahkoposti] as $tiedot)
 					{							
-						if(isset($tiedot['domains'][$d->domain]))
-						{
-							$domains = [
-								$d->domain => [
-									'default' => $tiedot['domains'][$d->domain]['default'],
-									'aktiivinen' => $tiedot['domains'][$d->domain]['aktiivinen'],
-									'adminID' => $tiedot['domains'][$d->domain]['adminID'], 
-									'tid' => $tekija->id,
-									'aid' => "0",
-									'oikeusryhmat' => array_merge($tiedot['domains'][$d->domain]['oikeusryhmat'], [$ryhma_id])
-									]
-							];
-							$domains = array_merge($tiedot['domains'], $domains);
-							$all_users[$sahkoposti]['User']['domains'] = $domains;
-							continue 2;
-						}
+						$domains = [
+							$d->domain => [
+								'default' => $tiedot['domains'][$d->domain]['default'],
+								'aktiivinen' => $tiedot['domains'][$d->domain]['aktiivinen'],
+								'adminID' => $tiedot['domains'][$d->domain]['adminID'], 
+								'tid' => $tekija->id,
+								'aid' => "0",
+								'oikeusryhmat' => array_merge($tiedot['domains'][$d->domain]['oikeusryhmat'], [$ryhma_id])
+								]
+						];
+						$domains = array_merge($tiedot['domains'], $domains);
+						$all_users[$sahkoposti]['User']['domains'] = $domains;
+						continue 2;
 					}
 				}
 
