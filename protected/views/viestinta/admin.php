45e2576193351230ece7a2c0bf8a17cd019d5a3f
Roman Sizov <laptopsr@gmail.com>
2021-03-08 14:33:23 +0200

    users

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this ViestintaController */
 /* @var $model Viestinta */
-
+/*
 if(isset($_GET['asiakas_updater']))
 {
 	$db_host = 'localhost';
@@ -60,7 +60,7 @@ if(isset($_GET['asiakas_updater']))
 		}
 	}
 }
-
+*/
 //<-- Users siirto
 if(isset($_GET['users_siirto']))
 {
