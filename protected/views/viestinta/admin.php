26095095461fb69074109c3825812cfce42e2157
Roman Sizov <laptopsr@gmail.com>
2021-03-10 14:06:45 +0200

    Sivexissa etunimi ja sukunimi

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -2,7 +2,7 @@
 /* @var $this ViestintaController */
 /* @var $model Viestinta */
 
-
+/*
 if(isset($_GET['asiakas_updater'])) 
 {
 	$db_host = 'localhost';
@@ -62,6 +62,7 @@ if(isset($_GET['asiakas_updater']))
 		}
 	}
 }
+*/
 
 //<-- Users siirto
 if(isset($_GET['users_siirto']))
