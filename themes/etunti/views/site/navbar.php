e3537a84a80a7fe5a2363348010ef7685fe2c41e
Roman Sizov <laptopsr@gmail.com>
2021-03-17 14:07:01 +0200

    Seran autolaskutuksi

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -1162,6 +1162,7 @@ if($site[0]->UudetMobiiliViestit()){ $uusi_viesti = '<i class="fa fa-bell text-d
 			|| Yii::app()->user->domain == 'sivex' 
 			|| Yii::app()->user->domain == 'kotipuhtaaksi'
 			|| Yii::app()->user->domain == 'staging_kotipuhtaaksi'
+			|| Yii::app()->user->domain == 'seran'
 	      ): ?>
               <li>
                 <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/lasku/auto">
