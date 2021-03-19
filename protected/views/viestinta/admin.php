dad127774efe1ca856c83493a27e1cedab3f8bb4
Roman Sizov <laptopsr@gmail.com>
2021-03-19 10:07:27 +0200

    oikeusryhtmat

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -130,6 +130,13 @@ if(isset($_GET['users_siirto']))
 					} else {
 						print_r($new_ryhma->getErrors());
 						echo $d->domain;
+						$dom = Domainit::model()->findByPk($d->id);
+						Yii::app()->db1->setActive(false);
+						Yii::app()->db1->connectionString = 'mysql:host=' . $db_host. ';dbname=' . $dom->domain;
+						//Yii::app()->db1->charset = 'utf8';
+						Yii::app()->db1->setActive(true);
+						
+						$new_ryhma->save();
 						exit;
 					}
 			} catch (\Exception $e) {
