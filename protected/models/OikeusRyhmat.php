472baa3a30fe1028ac1612f5a378d398d51f1c1a
Roman Sizov <laptopsr@gmail.com>
2021-03-22 12:09:22 +0200

    for delete

--- protected/models/OikeusRyhmat.php
+++ protected/models/OikeusRyhmat.php
@@ -26,7 +26,7 @@ class OikeusRyhmat extends DB2ActiveRecord
 
 		$table_structure = array(
 			'nimike' => 'varchar(255) DEFAULT NULL',
-			//'for_delete' => 'varchar(10) DEFAULT \'true\''
+			'for_delete' => 'varchar(10) DEFAULT \'true\''
 		);
 
 		foreach($table_structure as $key=>$value)
