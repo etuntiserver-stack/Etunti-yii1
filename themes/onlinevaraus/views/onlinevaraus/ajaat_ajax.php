72a721c612cf2ae612c9ba752b7d94073ff32a41
Roman Sizov <laptopsr@gmail.com>
2021-04-07 12:01:55 +0300

    Onlinevaraus valokuvat

--- themes/onlinevaraus/views/onlinevaraus/ajaat_ajax.php
+++ themes/onlinevaraus/views/onlinevaraus/ajaat_ajax.php
@@ -13,29 +13,35 @@
 		$filepath = dirname(Yii::app()->getBasePath())."/img/tekijat/".Yii::app()->user->domain."/".$vapaaTid.".jpg";
 		if (file_exists($filepath))
 		{
-		$url = Yii::app()->request->baseUrl."img/tekijat/".Yii::app()->user->domain."/".$vapaaTid.".jpg";
-		$size = getimagesize($url);
-		if( $size[0] > 640 )
-		{
-			// <-- Image resize
-			//header('Content-Type: image/jpeg');
-			$width = 640;
-			$image = imagecreatefromjpeg($url);
-			$orig_width = imagesx($image);
-			$orig_height = imagesy($image);
-			$height = (($orig_height * $width) / $orig_width);
-			$new_image = imagecreatetruecolor($width, $height);
-			imagecopyresized($new_image, $image,
-				0, 0, 0, 0,
-				$width, $height,
-				$orig_width, $orig_height);
-		
-			imagejpeg($new_image, $url);
-			//	  Image resize -->
-		}
-		$imageData = base64_encode(file_get_contents($filepath));
-		$src = 'data: '.mime_content_type($filepath).';base64,'.$imageData;
+			$url = Yii::app()->request->baseUrl."img/tekijat/".Yii::app()->user->domain."/".$vapaaTid.".jpg";
+			/*
+			$size = getimagesize($url);
+			if( $size[0] > 640 )
+			{
+				// <-- Image resize
+				//header('Content-Type: image/jpeg');
+				$width = 640;
+				$image = imagecreatefromjpeg($url);
+				$orig_width = imagesx($image);
+				$orig_height = imagesy($image);
+				$height = (($orig_height * $width) / $orig_width);
+				$new_image = imagecreatetruecolor($width, $height);
+				imagecopyresized($new_image, $image,
+					0, 0, 0, 0,
+					$width, $height,
+					$orig_width, $orig_height);
+
+				imagejpeg($new_image, $url);
+				//	  Image resize -->
+			}
+			
+			$imageData = base64_encode(file_get_contents($filepath));
+			$src = 'data: '.mime_content_type($filepath).';base64,'.$imageData;
 			$kuva = '<img src="'.$src.'" class="img-thumbnail">';
+			*/
+			
+			$kuva = '<img src="'.$url.'" class="img-thumbnail">';
+			
 		} else {
 			$kuva = '<img src="../../lib/img/noname.jpg" class="img-thumbnail">';
 		}
@@ -46,11 +52,11 @@
 	function tr($vapaaTid, $pvm, $sta, $sto, $kuva)
 	{
 		$return = '
-	<tr class="link ajaanClick" tid="'.$vapaaTid.'" pvm="'.date("d.m.Y",strtotime($pvm)).'" alku="'.$sta.'" loppu="'.$sto.'">
-	<td class="col-xs-2">'.$kuva.'</td>
-	<td>'.$sta.' - '.$sto.'</td>
-	</tr>';
-	return $return;
+		<tr class="link ajaanClick" tid="'.$vapaaTid.'" pvm="'.date("d.m.Y",strtotime($pvm)).'" alku="'.$sta.'" loppu="'.$sto.'">
+		<td class="col-xs-2">'.$kuva.'</td>
+		<td>'.$sta.' - '.$sto.'</td>
+		</tr>';
+		return $return;
 	}
 
 	$date		= $_POST['pvm'];
@@ -61,7 +67,7 @@
 	$body .= '<table class="table table-hover">';
 	foreach($tekijat as $key=>$value)
 	{
-	$body .= tr($value[0], $value[1], $value[2], $value[3], kuva($value[0]));
+		$body .= tr($value[0], $value[1], $value[2], $value[3], kuva($value[0]));
 	}
 	$body .= '</table>';
 	$body .= '</div>';
