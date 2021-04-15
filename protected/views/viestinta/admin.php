b9b862e7373b8e4914df33be5b08ac46eadad91b
Roman Sizov <laptopsr@gmail.com>
2021-04-15 10:06:31 +0300

    sarake puutuu

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -338,7 +338,12 @@ if(isset($_GET['users_siirto']))
 		$asiakkaat 			= Asiakkaat::model()->findAll("sahkoposti!='' AND salasana!=''");
 		$yhteensa			+= count($asiakkaat);
 		
-		Yii::app()->db1->createCommand()->addColumn('asiakkaat', 'extra_contacts', 'text DEFAULT NULL');
+		$tb_name 	= 'asiakkaat';
+		$column 	= 'extra_contacts';
+		$structure 	= 'text DEFAULT NULL';
+		$table 		= Yii::app()->db1->schema->getTable($tb_name);
+		if (!isset($table->columns[$column]))
+			Yii::app()->db1->createCommand()->addColumn($tb_name, $column, $structure);
 				
 		$del = OikeusRyhmat::model()->find("nimike='eDico'");
 		if($del !== null) $del->delete();
