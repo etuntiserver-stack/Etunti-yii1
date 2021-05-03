5a0c6e0717d2e0c839d090c1568135dbb8db01f1
Simo <simo@kotipuhtaaksi.fi>
2021-05-03 16:57:09 +0300

    avaimet sivu asiakkaan nimi

--- themes/etunti/views/avaimet/_view.php
+++ themes/etunti/views/avaimet/_view.php
@@ -28,10 +28,7 @@
 		<?php
 		if(isset($data->kohteet->id)){
 			$as = Asiakkaat::model()->findByPk($data->kohteet->asiakas_id);
-			if(isset($as->id) and $as->tyyppi == 'yritys')
-				echo $as->yrityksen_nimi;
-			if(isset($as->id) and $as->tyyppi == 'henkilo')
-				echo $as->yhteyshenkilo;
+			echo $as->Fullname;
 		}
 		?>
 	</td>
