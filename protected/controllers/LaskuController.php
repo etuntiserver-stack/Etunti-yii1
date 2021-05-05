175f03f6c511532255ba7f3833e4fb5f6a934ee6
Roman Sizov <laptopsr@gmail.com>
2021-05-05 10:16:36 +0300

    step 12

--- protected/controllers/LaskuController.php
+++ protected/controllers/LaskuController.php
@@ -2306,7 +2306,7 @@ exit;
 		
 		$body .= '
 			<br>
-			<p><button type="submit" class="btn-block btn btn-info laskutetuksi" asiakas_id="'.$asiakas_id.'" from="'.$from.'" to="'.$to.'" etunti_tunniste="'.$etunti_tunniste.'" style="display:none">Luo uusi lasku</button></p>
+			<p><button type="submit" class="btn-block btn btn-info laskutetuksi" asiakas_id="'.$asiakas_id.'" from="'.$from.'" to="'.$to.'" etunti_tunniste="'.$etunti_tunniste.'" style="display:none" data-toggle="tooltip" data-placement="top" title="Hei, Tämän painamalla oikeasti luodaan heti uusi hyväksymätön laskun ruksilla määritettyltä riviltä">Luo uusi lasku</button></p>
 		';
 		
 		echo json_encode($body);
