90e199b1ddabf8db746ccf31a38c2ed00053a2c9
Simo <simo@kotipuhtaaksi.fi>
2021-05-10 11:49:48 +0300

    disabled gallery for now

--- themes/etunti/views/site/gallery.php
+++ themes/etunti/views/site/gallery.php
@@ -1,5 +1,9 @@
 <?php
 // path to image folder
+/*
+you can't reference images directly or you'll get 403 forbidden
+and encoding them like this seems to be way too much data at once, and loading="lazy" probably doesn't
+work with encoded images.
 $img_path = Yii::app()->basePath."/../tiedostot/asiakkaat/" . Yii::app()->user->domain . "/";
 foreach(glob($img_path.'*.{jpg,JPG,jpeg,JPEG,png,PNG}',GLOB_BRACE) as $file){
     $imageFile = Yii::app()->basePath."/../tiedostot/asiakkaat/" . Yii::app()->user->domain . "/" . basename($file);
@@ -9,4 +13,6 @@ foreach(glob($img_path.'*.{jpg,JPG,jpeg,JPEG,png,PNG}',GLOB_BRACE) as $file){
         <img loading="lazy" src="'.$img.'" />
     ';
 }
+*/
+echo "WIP";
 ?>
