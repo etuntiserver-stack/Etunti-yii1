1d437459f068e55a6dd3e4d5955193208f163b6f
Roman Sizov <laptopsr@gmail.com>
2021-05-14 10:11:26 +0300

    crontab

--- themes/classic/views/site/crontab.php
+++ themes/classic/views/site/crontab.php
@@ -53,6 +53,7 @@ foreach ($list as $d) {
 	$dh 		= DigistenHinnasto::model()->findByPk(1);
 	$domainit 	= Domainit::model()->findByPk($d->id);
 
+	/*
 	// Sarake lisäys
 	$tb_name 	= 'asiakkaat';
 	$column 	= 'tuote';
@@ -60,7 +61,7 @@ foreach ($list as $d) {
 	$table 		= Yii::app()->db1->schema->getTable($tb_name);
 	if (!isset($table->columns[$column]))
 		Yii::app()->db1->createCommand()->addColumn($tb_name, $column, $structure);
-		
+	*/
 			
 	// <-- Tuntien Autohyvaksyminen
 	if ($asetukset->app_hyvaksynnan_peruste == 2) {
