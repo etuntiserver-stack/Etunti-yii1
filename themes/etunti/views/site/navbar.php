0b75033c23d44beac36e1f86efb3c281f243a9ae
Elias Luoma <elias.luoma@santelo.fi>
2021-06-16 18:21:29 +0300

    Removed FreshDesk from menu

--- themes/etunti/views/site/navbar.php
+++ themes/etunti/views/site/navbar.php
@@ -1414,27 +1414,6 @@ if($site[0]->checkEdicoViestit()){ $bell = '<i class="fa fa-bell text-danger"></
               </li>
             </ul>
           </li>
-
-
-          <!-- Freshdesk menu item. -->
-          <?php $freshdesk = Yii::createComponent('Freshdesk'); ?>
-          <?php if ($freshdesk && !$freshdesk->isDisabled()): ?>
-          <li>
-            <a class="accordion-toggle crm" href="#">
-              <span class="fa fa-users"></span>
-              <span class="sidebar-title"><?php echo Yii::t('main', 'Asiakaspalvelu'); ?></span>
-              <span class="caret"></span>
-            </a>
-            <ul class="nav sub-nav">
-              <li>
-                <a href="<?php echo Yii::app()->request->baseUrl; ?>/index.php/asiakkaat/freshdesk">
-                  <span class="fa fa-money"></span> <?php echo Yii::t('main', 'Freshdesk Tukipyynnöt'); ?></a>
-              </li>
-            </ul>
-          </li>
-          <?php endif; ?>
-
-
           <li>
             <a class="accordion-toggle" href="#">
               <span class="sidebar-title"><?php echo strtoupper(Yii::app()->user->domain); ?></span>
