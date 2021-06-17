b79354e76f036c0b9623360d4df0b1cfdf23396e
Simo <simo@kotipuhtaaksi.fi>
2021-06-17 11:52:30 +0300

    re-enabled worker integromat integration

--- protected/controllers/TyontekijatController.php
+++ protected/controllers/TyontekijatController.php
@@ -1106,8 +1106,6 @@ $xml = '
 	 */
 	public function integromatUpsert(Tyontekijat $worker)
 	{
-		// temporarily disabled
-		return;
 		// get and parse workers name
 		$apiController = Yii::app()->createController("Api")[0];
 		$firstName = $apiController->parseName($worker->tekijan_nimi);
