0c117414755c4a61897563967dd77b4e24328ea3
Roman Sizov <laptopsr@gmail.com>
2021-07-23 09:48:32 +0300

    tuote ei pakkollinen kohdessa

--- protected/models/Kohteet.php
+++ protected/models/Kohteet.php
@@ -147,7 +147,7 @@ class Kohteet extends DB2ActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('asiakas_id, etu_suku_nimet, osoite, tuote_h', 'required'),
+			array('asiakas_id, etu_suku_nimet, osoite', 'required'),
 			array('asiakas_id, aktiivinen, maksuehto_paiva, avaimen_sijainti, tarvittavien_tyontekijoiden_maara, uusi_tilaus, hinnasto_id, alv, tyoryhma, tyonkuvaus_tiedostot_mobiilissa, kustannuspaikka_nro, tuote_h, tuote_kk, tuote_kpl', 'numerical', 'integerOnly'=>true),
 			array('tag_id, kaupunki, toimipaikka, kohteen_neliot', 'length', 'max'=>20),
 			array('gps_sijainti, osoite, katuosoite, kenella_on_avain, puh_nro, hinta_sis_alv, hinta_tyyppi, laskurivi_tyyppi', 'length', 'max'=>50),
