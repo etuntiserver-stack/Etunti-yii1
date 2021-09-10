9861bcfd141c23030b7f02a6907d260e487b561c
Roman Sizov <laptopsr@gmail.com>
2021-09-10 09:56:09 +0300

    etunimet

--- protected/views/viestinta/admin.php
+++ protected/views/viestinta/admin.php
@@ -2,9 +2,9 @@
 /* @var $this ViestintaController */
 /* @var $model Viestinta */
 
-/*
+
 if( Yii::app()->user->domain == 'kotimaan_huolenpitopalvelut_oy' ){
-   $a = Asiakkaat::model()->findAll("time='0000-00-00 00:00:00' AND etunimi!='' AND sukunimi!=''");
+   $a = Asiakkaat::model()->findAll("id>200 AND id<=260 AND etunimi!='' AND sukunimi!=''");
    echo '<h3>Yhteensä: '.count($a).'</h3>';
    echo '<table><tr><th>Etunimi</th><th>Sukunimi</th></tr>';
    foreach($a as $item)
@@ -15,7 +15,7 @@ if( Yii::app()->user->domain == 'kotimaan_huolenpitopalvelut_oy' ){
    }
    echo '</table>';
 }
-*/
+
 
 /*
 if( Yii::app()->user->domain == 'sivex' )
