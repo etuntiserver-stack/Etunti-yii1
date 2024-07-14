5b61a54d083538ea76a2bd8f13506e8bca55463e
Roman <laptopsr@gmail.com>
2024-07-14 13:22:48 +0300

    lasku 25.5

--- protected/controllers/LaskuController.php
+++ protected/controllers/LaskuController.php
@@ -1489,7 +1489,7 @@ exit;
 		if($row)
 		$body .= '<option value="'.$row.'">'.$row.'</option>';
 		$body .= '<option value="25.5">25.5</option>';
-		$body .= '<option value="24">24</option>';
+		$body .= '<option value="24" selected>24</option>';
 		$body .= '<option value="14">14</option>';
 		$body .= '<option value="10">10</option>';
 		$body .= '<option value="0">0</option>';
